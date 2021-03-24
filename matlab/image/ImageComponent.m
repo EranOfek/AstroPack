35b8e04c1ed2b549e6b738ba0ed7a1f44867dbd3
Chen Tishler <chen@tishler.dev>
2021-03-24 16:15:25 +0200

    templates, drafts

--- matlab/image/ImageComponent.m
+++ matlab/image/ImageComponent.m
@@ -1,5 +1,19 @@
 
 classdef ImageComponent < Component
+    
+    
+    properties (Hidden, SetAccess = public)
+        Data                                    % e.g., Image matrix
+        Scale {mustBeNumeric(Scale)} = [];      %
+        ScaleMethod = 'lanczos3';               %
+        
+        %DataProp cell = {'Data'};              % a cell of properties on which the fun_* methods will be applied
+        Virt VirtImage                          % Actual image data
+        
+        % Storage 
+    end
+
+    
     properties (Hidden, SetAccess = public)
   
     end
