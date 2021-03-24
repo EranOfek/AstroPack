b0ec7ac9fc34432490451047211f4d5424c2a4ec
Eran Ofek <ofek.eran@gmail.com>
2021-03-24 21:33:37 +0200

    AstroCatalog setters/getters/funs

--- matlab/image/AstroCatalog.m
+++ matlab/image/AstroCatalog.m
@@ -1,5 +1,5 @@
-% BaseImage handle class - all images inherits from this class
-% Package: @BaseImage
+% AstroCatalog handle class
+% Package: @AstroCatalog
 % Description: 
 % Tested : Matlab R2018a
 % Author : Eran O. Ofek (Mar 2021)
@@ -8,53 +8,110 @@
 % Reliable: 2
 %--------------------------------------------------------------------------
 
-classdef AstroImage < ImageComponent
+classdef AstroCatalog < handle %ImageComponent
     % Component should contain:
     % UserData
     % Config
     
-    properties (Dependent) % Access image data directly        
-        Image 
-        Mask 
-        Back 
-        Var
-        Cat
-    end
-    
     properties (SetAccess = public)
-        Header AstroHeader
-        PSF AstroPSF
-        CatData AstroCatalog
-        WCS AstroWCS
-        
-        % Data
-        ImageData(1,1) BaseImage
-        MaskData(1,1) BaseImage
-        BackData(1,1) BaseImage
-        VarData(1,1) BaseImage
+        Catalog                                                = [];
+        ColCell cell                                           = {};
+        ColUnits cell                                          = {};
+        SortByCol                                              = [];
+        IsSorted(1,1) logical                                  = false;
     end
     
     methods % Constructor
        
-        function Obj = AstroImage
-            
+        function Obj = AstroCatalog
+            %
             
         end
 
     end
  
+%     methods % subsref
+%         function varargout = subsref(Obj,S)
+%             % FOR SMALL TABLES THIS SLOWS DOWN PERFORMNCES BY FACTOR OF 4!
+%             % tic; for I=1:1:1000, aa=AC.Catalog; end; toc
+
+%             if numel(S)>1 && strcmp(S(end-1).subs,'CatCol')
+%                 % identified a CatColumn requesy
+%                 if strcmp(S(end).type,'.')
+%                     ColInd = colname2ind(Obj, S(end).subs);
+%                     varargout{1} = Obj.Catalog(:,ColInd);
+%                     if istable(varargout{1})
+%                         varargout{1} = table2array(varargout{1});
+%                     end
+%                 end
+%             else
+%                 if nargout==0
+%                     Nargout = 1;
+%                 else
+%                     Nargout = nargout;
+%                 end
+%                 [varargout{1:Nargout}] = builtin('subsref',Obj,S);
+%             end
+%             
+%         end
+%     end
 
  
     methods % Setters/Getters
-        function Obj = set.Image(Obj, Data)
-            % setter for Image - store image in ImageData property
-            Obj.ImageData.Data = Data;
+        function set.Catalog(Obj, Data)
+            % setter for catalog - set also column names and units if table
+            % and available
+            
+            if istable(Data)
+                Obj.Catalog = Data;
+                
+            elseif isnumeric(Data)
+                Obj.Catalog = Data;
+            elseif iscell(Data)
+                Obj.Catalog = cell2table(Data);
+            else
+                error('Catalog must be numeric, cell, or table');
+            end
+            Obj.IsSorted = false;
         end
         
-        function Data = get.Image(Obj)
+        function Result = get.Catalog(Obj)
             % getter for Image - get image from ImageData property
-            Data = Obj.ImageData.Data;
+            Result = Obj.Catalog;
         end        
+        
+        function set.ColCell(Obj, CellColName)
+            % setter for ColCell - input is either a cell or a string array
+            % if the catalog is in table format it will also set its
+            % Properties.VariableNames (but only if the size is consistent)
+            Obj.ColCell = CellColName;
+            if istable(Obj.Catalog)
+                % set the column names also in the table
+                if numel(CellColName)==size(Obj.Catalog,2)
+                    Obj.Catalog.Properties.VariableNames = CellColName;
+                end
+            end
+        end
+        
+        function CellColName = get.ColCell(Obj)
+            % getter for ColCell
+           
+            CellColName = Obj.ColCell;
+            if isempty(CellColName) && ~isempty(Obj.Catalog) && istable(Obj.Catalog)
+                % get ColCell from table properties
+                CellColName = Obj.Catalog.Properties.VariableNames;
+            end
+                
+            
+        end
+            
+        function set.SortByCol(Obj,Val)
+            % Setter for SortByCol, also set IsSorted to false
+            
+            Obj.SortByCol = Val;
+            Obj.IsSorted  = false;
+        end
+        
     end
     
     methods (Static)  % static methods
@@ -62,6 +119,151 @@ classdef AstroImage < ImageComponent
     end
     
     methods % functionality
+        function ColInd = colname2ind(Obj, ColName, FillValue)
+            % Convert column names to column indices
+            % Input  : - A single element AstroCatalog object.
+            %          - A column name, a cell array of column names or an
+            %            array of column indices.
+            %          - If the colun name is char or cell, and it doesn't
+            %            exist, this is the fill value for the column
+            %            index. If empty, will fail. Default is NaN.
+            % Output : - A vector of column indices corresponding to the column names.
+            % Author : Eran Ofek (Mar 2021)
+            % Example: colname2ind(AC, {'Var1','aa'})
+            %          colname2ind(AC, [1 2])
+            
+            arguments
+                Obj(1,1)
+                ColName
+                FillValue      = NaN;
+            end
+           
+            if isnumeric(ColName)
+                % assumes columns are already column index
+                ColInd = ColName;
+            elseif ischar(ColName)
+                ColInd = find(strcmp(Obj.ColCell, ColName));
+                Tmp = find(strcmp(Obj.ColCell, ColName));
+                if isempty(Tmp)
+                    if isempty(FillValue)
+                        error('Column %s not found',ColName);
+                    else
+                        ColInd = FillValue;
+                    end
+                else
+                    ColInd = Tmp;
+                end
+                
+            elseif iscell(ColName) || isstring(ColName)
+                Ncol   = numel(ColName);
+                ColInd = nan(1,Ncol);
+                for Icol=1:1:Ncol
+                    Tmp = find(strcmp(Obj.ColCell, ColName{Icol}));
+                    if isempty(Tmp)
+                        if isempty(FillValue)
+                            error('Column %s not found',ColName{Icol});
+                        else
+                            ColInd(Icol) = FillValue;
+                        end
+                    else
+                        ColInd(Icol) = Tmp;
+                    end
+                end
+            end
+            
+        end
+        
+        function ColName = colind2name(Obj, ColInd)
+            % Return column names corresponding to column indices
+            % Input  : - A single element AstroCatlog object
+            %          - A vector of column indices.
+            % Output : - A cell array of column names corresponding to the
+            %            column indices.
+            % Example: colind2name(AC,[2 1])
+           
+            ColName = Obj.ColCell(ColInd);
+        end
+        
+        function Result = getCol(Obj,Columns,OutputIsTable,UpdateAstroCatalog)
+            % Get a catalog columns by index or names
+            % Input  : - A single element AstroCatalog object.
+            %          - A vector of column indices, or a column name, or a
+            %            cell array of column names.
+            %          - If true will convert the output to table,
+            %            otherwise will attempt to convert to a matrix.
+            %            Default is false.
+            %          - If true than in addition to returning the sub
+            %            catalog, will store this sub catalog in the
+            %            Astrocatalog object.
+            %            Default is false.
+            % Output : - A matrix or a table containing the selected
+            %            columns.
+            % Author : Eran Ofek (Mar 2021)
+            % Example: AC.getCol({'Var1','Var3'},true)
+            %          AC.getCol([1 2])
+            %          AC.getCol({'Var1','Var3'},true,true)
+            
+            arguments
+                Obj(1,1)
+                Columns
+                OutputIsTable(1,1) logical         = false;
+                UpdateAstroCatalog(1,1) logical    = false;
+            end
+                
+            
+            ColInd = colname2ind(Obj, Columns, []);
+            if istable(Obj.Catalog)
+                if OutputIsTable
+                    Result = Obj.Catalog(:,ColInd);
+                else
+                    Result = table2array(Obj.Catalog(:,ColInd));
+                end
+            else
+                if OutputIsTable
+                    Result = array2table(Obj.Catalog(:,ColInd));
+                    Result.Properties.VariableNames = Obj.ColCell;
+                    Result.Properties.VariableUnits = Obj.UnitCell;
+                else
+                    Result = Obj.Catalog(:,ColInd);
+                end
+            end
+            
+            if UpdateAstroCatalog
+                Obj.Catalog = Result;
+            end
+            
+        end
+        
+        function Obj = sortrows(Obj,SortByColumn)
+            % Sort AstroCatalog objects by some column names/indices
+            % Input  : - An AstroCatalog object (multiple elements is possible).
+            %          - A column/s names or indices by which to sort the
+            %            catalogs. If empty, will attempt to use the
+            %            SortByCol property. Default is empty.
+            % Output : - An object in which the catalogs are sorted.
+            % Author : Eran Ofek (Mar 2021)
+            % Example: AC=AstroCatalog; AC.Catalog = rand(100,3); AC=sortrows(AC,2);
+            
+            arguments
+                Obj
+                SortByColumn       = [];
+            end
+           
+            SortByColumn           = colname2ind(Obj,SortByColumn);
+            
+            Nobj = numel(Obj);
+            for Iobj=1:1:Nobj
+                if ~Obj(Iobj).IsSorted
+                    if isempty(SortByColumn)
+                        SortByColumn = Obj(Iobj).SortByCol;
+                    end
+                    Obj(Iobj).Catalog   = sortrows(Obj(Iobj).Catalog, SortByColumn);
+                    Obj(Iobj).SortByCol = SortByColumn;
+                    Obj(Iobj).IsSorted  = true;
+                end
+            end
+        end
+        
         function Result = fun_unary(Obj, OperatorOperatorArgs, OutType, DataProp, DataPropOut)
             %
            
