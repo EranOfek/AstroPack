43efe4e2fdc903bcd4ef8b6e7fe3d554a8a93bd6
Eran Ofek <ofek.eran@gmail.com>
2021-03-29 18:46:37 +0300

    fun_unary_withVariance treat symbolic derivatives

--- matlab/image/+imUtil/+image/fun_unary_withVariance.m
+++ matlab/image/+imUtil/+image/fun_unary_withVariance.m
@@ -1,4 +1,4 @@
-function [Result,ResultVar,FlagBad]=fun_unary_withVariance(Operator, Mat, Var, OpArg)
+function [Result,ResultVar,FlagBad,FunH]=fun_unary_withVariance(Operator, Mat, Var, OpArg)
 % Applay an operator on an array and its variance.
 % Input  : - Operator handle (e.g., @sin).
 %          - An array in which to apply the operator.
@@ -9,8 +9,13 @@ function [Result,ResultVar,FlagBad]=fun_unary_withVariance(Operator, Mat, Var, O
 %          - The result of applying the operator to the variance.
 %          - A matrix of logicals indicating if a resulted matrix value is
 %            NaN or Inf, or the resulted Variance is NaN.
+%          - The function handle for the derivative function (only if was
+%            found symbolically).
 % Author : Eran Ofek (Mar 2021)
-% Example: [Result,ResultVar,Flag]=imUtil.image.fun_unary_withVariance(@sin, randn(5,5), rand(5,5).*0.01)
+% Example: [Result,ResultVar,Flag,FunH]=imUtil.image.fun_unary_withVariance(@sin, randn(5,5), rand(5,5).*0.01)
+%          [Result,ResultVar,Flag,FunH]=imUtil.image.fun_unary_withVariance(@mean, randn(2,2), rand(2,2).*0.01)
+%          [Result,ResultVar,Flag,FunH]=imUtil.image.fun_unary_withVariance(@mean, randn(2,2), rand(2,2).*0.01,{'all'})
+%          [Result,ResultVar,Flag,FunH]=imUtil.image.fun_unary_withVariance(@tanh, randn(2,2), rand(2,2).*0.01);
 
     arguments
         Operator 
@@ -18,6 +23,8 @@ function [Result,ResultVar,FlagBad]=fun_unary_withVariance(Operator, Mat, Var, O
         Var          {mustBeNumeric(Var)} = [];
         OpArg cell                        = {};
     end
+    
+    FunH      = [];
     
     Result    = Operator(Mat, OpArg{:});
     if isempty(Var)
@@ -50,12 +57,16 @@ function [Result,ResultVar,FlagBad]=fun_unary_withVariance(Operator, Mat, Var, O
                 end
             
             otherwise
-                error('Unknown unary operator option');
+                % Unknown unary operator option
                 % attempt to use symbolic math
-                % FFU
-                % syms x
-                % eval(sprintf('diff(%s(x))', func2str(Operator)
-                % use subs
+                syms x;
+                SymFun = eval(sprintf('diff(%s(x))', func2str(Operator)));
+                if SymFun==1
+                    error('Function derivative could not be found symbolically');
+                end
+                FunH   = matlabFunction(SymFun);
+                ResultVar = Var.*FunH(Mat);
+                warning('The variance was propagated using symbolic math - For speed consider adding this function to the list of built in functions');
         end
         FlagBad = isinf(Mat) | isnan(Mat) | isnan(Var);
     end
