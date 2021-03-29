60524bdf6fee0a2640e742ef64e6df24cd2e4053
Chen Tishler <chen@tishler.dev>
2021-03-29 12:56:06 +0300

    Algo

---
+++ matlab/image/CatAlgo.m
@@ -0,0 +1,31 @@
+% AstroCatalog handle class
+% Package: @AstroCatalog
+% Description: 
+% Tested : Matlab R2018a
+% Author : Eran O. Ofek (Mar 2021)
+% Dependencies: @convert, @celestial
+% Example : 
+% Reliable: 2
+%--------------------------------------------------------------------------
+
+classdef CatAlgo < BaseAlgo %Component or Base
+    
+    methods % Constructor
+       
+        function Obj = CatAlgo()
+        end  
+               
+    end
+    
+
+    methods (Static)  % Unit-Test
+        function Result = unitTest()
+ 
+            Result = true;
+        end
+    end
+    
+
+end
+
+            
