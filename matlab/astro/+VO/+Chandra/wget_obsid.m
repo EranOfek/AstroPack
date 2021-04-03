86575991f5f818552d1f591f42d76a60bdd5b089
Eran Ofek <ofek.eran@gmail.com>
2021-04-03 17:30:10 +0300

    astro/+VO with links corrections

---
+++ matlab/astro/+VO/+Chandra/wget_obsid.m
@@ -0,0 +1,180 @@
+function URL=wget_obsid(ObsID,varargin)
+% Get all the files associated with a Chandra ObsID
+% Package: VO.Chandra
+% Description: Get all the files associated with a Chandra ObsID
+%              The Chandra observations catalog is stored in
+%              cats.X.ChandraObs.
+%              Use VO.Chandra.build_obsid_cat to build catalog.
+% Input  : - ObsID
+%          * Arbitrary number of pairs of arguments: ...,keyword,value,...
+%            where keyword are one of the followings:
+%            'Download' - Indicating what to download:
+%                         'all' - all data (default).
+%                         'primary' - only data in primary directory.
+%                         'secondary' - only data in secondary directory.
+%            'Output'   - Output data in a 'flat' structure' or
+%                         'dir' (directory) structure.
+%                         'none' - do not copy.
+%                         Default is 'dir'.
+%            'Ungzip'   - ungzip data {true|false}. Default is true.
+%            'CopyTo'   - Directory in which to cd before copying data.
+%            'ReGet'    - Get files again if 'primary' dir exist 
+%                         in directory {true|false}. Default is false.
+%            'Extra'    - Extra parameters for wget.
+%            'MaxGet'   - Maximum number of files for parallel get
+%                         (see www.pwget.m). Default is 10.
+% Output : null
+% Tested : Matlab R2014a
+%     By : Eran O. Ofek                    Jan 2015
+%    URL : http://weizmann.ac.il/home/eofek/matlab/
+% Example: VO.Chandra.wget_obsid(366)
+% Reliable: 2
+%--------------------------------------------------------------------------
+
+%DefV.ChandraCat    = [];
+DefV.Download      = 'all';   % {'all'|'primary'|'evt'}
+DefV.Output        = 'dir_full';   % {'dir_full'|'dir_obsid'|'flat'}
+DefV.Ungzip        = true;
+DefV.CopyTo        = [];
+DefV.UnGzip        = true;
+DefV.ReGet         = false;
+DefV.Extra         = '';
+DefV.MaxGet        = 10;
+DefV.BaseURL       = 'https://cxc.cfa.harvard.edu/cdaftp/science/';
+InPar = InArg.populate_keyval(DefV,varargin,mfilename);
+
+
+% if (isempty(InPar.ChandraCat))
+%     Cat = VO.Chandra.build_obsid_cat('GetInfo',false,'Verbose',false);
+% else
+%     if (ischar(InPar.ChandraCat))
+%         Cat = load2(InPar.ChandraCat);
+%     else
+%         Cat = InPar.ChandraCat;
+%     end
+% end
+
+Cat = cats.X.ChandraObs;
+
+% Search ObsID
+%Iobs = find([Cat.ObsID]==ObsID);
+Iobs  = find(Cat.Cat.ObsID==ObsID);
+%URL  = Cat(Iobs).URL;
+URL   = Cat.Cat.URL{Iobs};
+
+
+PWD = pwd;
+if (InPar.CopyTo)
+    cd(InPar.CopyTo);
+end
+
+% check if directory is populated
+if (exist('primary','dir')>0 && ~InPar.ReGet)
+    % do not get files again
+else
+
+    % get file names
+    [List,IsDir,FileName]=www.r_files_url(URL);
+    switch lower(InPar.Download)
+        case 'evt'
+            Flag = ~tools.cell.isempty_cell(regexp(List,'_evt2.fits','match'));
+            List = List(Flag);
+        case 'primary'
+            Flag1 = ~tools.cell.isempty_cell(regexp(List,'/primary/','match'));
+            Flag2 = ~tools.cell.isempty_cell(regexp(List,'/oif.fits','match'));
+            Flag  = Flag1 | Flag2;
+            List = List(Flag);
+        case 'all'
+            % keep List as is
+        otherwise
+            error('Unknown Download option');
+    end
+
+    Nl = numel(List);
+    ObsIDstr = sprintf('%d',ObsID);
+    
+    % download
+    switch lower(InPar.Output)
+        case 'none'
+            % do nothing
+            
+        case 'flat'
+            www.pwget({List.URL},InPar.Extra,InPar.MaxGet);        
+            if (InPar.Ungzip)
+                system('gzip -d *.gz');
+            end
+        case {'dir_obsid','dir_full'}
+            
+            switch lower(InPar.Output)
+                case 'dir_obsid'
+                    if exist(ObsIDstr,'dir')==0
+                        mkdir(ObsIDstr);
+                        cd(ObsIDstr);
+                        DirExist = false;
+                    else
+                        DirExist = true;
+                    end
+                    
+                case 'dir_full'
+                    ListTmp = regexprep(List,InPar.BaseURL,'');
+                    SplitTmp = regexp(ListTmp,'/','split');
+                    DirAO = SplitTmp{1}{1};
+                    DirCat = SplitTmp{1}{2};
+                    
+                    mkdir(DirAO);
+                    cd(DirAO);
+                    mkdir(DirCat);
+                    cd(DirCat);
+                    if exist(ObsIDstr,'dir')==0
+                        mkdir(ObsIDstr);
+                        cd(ObsIDstr);
+                        DirExist = false;
+                    else
+                        DirExist = true;
+                    end    
+                    
+                otherwise
+                    error('Impossible error');
+            end
+            
+            
+            if ~DirExist
+                % only if ObsIDstr dir doesn't exist
+                MatchP = sprintf('%sao\\d\\d/cat\\d/\\d.{1,5}/primary/*',InPar.BaseURL);
+                MatchS = sprintf('%sao\\d\\d/cat\\d/\\d.{1,5}/secondary/*',InPar.BaseURL);
+                Fp     = ~tools.cell.isempty_cell(regexp(List,MatchP,'match'));
+                Fs     = ~tools.cell.isempty_cell(regexp(List,MatchS,'match'));
+                Ff     = ~Fp & ~Fs;
+                if (sum(Ff)>0)
+                    www.pwget(List(Ff),InPar.Extra,InPar.MaxGet);
+                    if InPar.UnGzip
+                        system('gzip -d *.gz');
+                    end
+                end
+                if (sum(Fp)>0)
+                    mkdir('primary');
+                    cd('primary');
+                    www.pwget(List(Fp),InPar.Extra,InPar.MaxGet);
+                    if InPar.UnGzip
+                        system('gzip -d *.gz');
+                    end
+                    cd ..
+                end
+                if (sum(Fs)>0)
+                    mkdir('secondary');
+                    cd('secondary');
+                    www.pwget(List(Fs),InPar.Extra,InPar.MaxGet);
+                    if InPar.UnGzip
+                        system('gzip -d *.gz');
+                    end
+                    cd ..
+                end
+
+            end
+            
+       otherwise
+            error('Unknown Output option');
+    end
+
+end  
+cd(PWD);                        
---
+++ matlab/astro/+VO/+search/search_sortedlat_multiNearest.m
@@ -0,0 +1,81 @@
+function [IndTable,CatFlagNearest,CatFlagAll]=search_sortedlat_multiNearest(Cat,Long,Lat,Radius,DistFun)
+% Search a single long/lat in a catalog sorted by latitude
+% Package: VO.search
+% Description: A low level function for a single cone search
+%              in a [Long, Lat] array.
+% Input  : - An array of [Long, Lat] in radians, sorted by Lat.
+%            The program doesnot verify that the array is sorted.
+%          - Longitude [radians] to search.
+%          - Latitude [radians] to search.
+%          - Radius [radians] to search.
+%          - A function handle for calculating distances Fun(X1,Y1,X2,Y2).
+%            Default is @celestial.coo.sphere_dist_fast.
+% Output : - A three column matrix with, one line per line in Long,Lat.
+%            Columns are [Index of nearest source, within search radius, in Cat,
+%            Distance, Total number of matches within radius].
+%          - A vector of logical (length as Cat), which indicate the object
+%            in Cat that were identified as the nearest object to a source in
+%            Long, Lat.
+%          - The same as the previous output, but for all the sources
+%            within the search radius.
+%     By : Eran O. Ofek                    Feb 2017
+%    URL : http://weizmann.ac.il/home/eofek/matlab/
+% Example: Cat=sortrows(rand(10000,2),2);
+%          Ind=VO.search.search_sortedlat_multi(Cat,0.5,0.5,0.01)
+% Reliable: 2
+
+arguments
+    Cat
+    Long
+    Lat
+    Radius
+    DistFun function_handle      = @celestial.coo.sphere_dist_fast;
+end
+
+
+Col.Lon = 1;
+Col.Lat = 2;
+
+% somewhat slower version:
+% Ilow  = tools.find.bin_sear(Cat(:,Col.Lat),Lat-Radius);
+% Ihigh = tools.find.bin_sear(Cat(:,Col.Lat),Lat+Radius);
+
+Lat   = Lat(:).';  % convert Lat to a row vector
+Nlat  = numel(Lat); % number of latitudes to search
+Ilat  = [(1:1:Nlat).', (1:1:Nlat).'+Nlat];
+
+Ncat  = size(Cat,1);
+Inear = tools.find.mfind_bin(Cat(:,Col.Lat),[Lat-Radius, Lat+Radius]);
+
+% Inear(Ilat) is a two column matrix [low, high] index for each latitud
+% search
+Ilowhigh = double(Inear(Ilat));
+Ilow     = Ilowhigh(:,1);
+Ihigh    = min(Ncat,Ilowhigh(:,2)+1); % add 1 because of the way mfind_bin works
+
+IndTable = [nan(Nlat,2), zeros(Nlat,1)]; % [Index, Dist, Nmatch]
+%tools.struct.struct_def({'Ind','Nmatch','Dist'},Nlat,1);
+
+CatFlagNearest  = false(Ncat,1);
+CatFlagAll      = false(Ncat,1);
+
+
+for I=1:1:Nlat
+    %Dist  = celestial.coo.sphere_dist_fast(Long(I),Lat(I), Cat(Ilow(I):Ihigh(I),Col.Lon), Cat(Ilow(I):Ihigh(I),Col.Lat));
+    Dist  = DistFun(Long(I),Lat(I), Cat(Ilow(I):Ihigh(I),Col.Lon), Cat(Ilow(I):Ihigh(I),Col.Lat));
+    FlagDist = Dist <= Radius;
+    
+    IndI  = Ilow(I)-1+find(FlagDist);
+    DistI = Dist(FlagDist);
+    if ~isempty(DistI)
+        [MinDist, MinInd] = min(DistI);
+        IndTable(I,1) = IndI(MinInd);   % VERIFY THIS???
+        IndTable(I,2) = MinDist;
+        IndTable(I,3) = numel(IndI);
+        
+        CatFlagNearest(IndTable(I,1)) = true;
+        CatFlagAll(IndI)              = true;
+        
+    end
+    
+end
