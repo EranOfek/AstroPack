21a9887743f7ab5c0de148a4e9ee22011005b7b0
Chen Tishler <chen@tishler.dev>
2021-04-18 14:24:00 +0300

    virt

--- matlab/image/VirtImageManager.m
+++ matlab/image/VirtImageManager.m
@@ -1,12 +1,17 @@
 % Virtual Image Manager
 %--------------------------------------------------------------------------
 
-
 classdef VirtImageManager < Component
     % Properties
     properties (SetAccess = public)
         VirtPath = '';      % Default path to storge virtual image files
-        ImageList = []      % containers.Map - List of VirtImage objects
+        
+        % Map object is a data structure that allows you to retrieve values 
+        % using a corresponding key. Keys can be real numbers or character 
+        % vectors. As a result, they provide more flexibility for data 
+        % access than array indices, which must be positive integers. 
+        % Values can be scalar or nonscalar arrays.
+        Map = []            % containers.Map - List of VirtImage objects
     end
     
     %-------------------------------------------------------- 
@@ -14,7 +19,7 @@ classdef VirtImageManager < Component
         
         function Obj = VirtImageManager()
             Obj.msgLog(LogLevel.Debug, 'VirtImageManager created');
-            Obj.ImageList = containers.Map();
+            Obj.Map = containers.Map();
             
         end
         
@@ -22,6 +27,7 @@ classdef VirtImageManager < Component
         function delete(Obj)
             % Destructor
             Obj.msgLog(LogLevel.Debug, 'VirtImageManager deleted');
+            release();
         end
     end
     
@@ -29,11 +35,41 @@ classdef VirtImageManager < Component
     methods    
         function registerImage(Obj, Image)
             Obj.msgLog(LogLevel.Debug, 'registerImage: %s', Image.Uuid);
+            
+            Key = Obj.getImageKey(Image);
+            if ~Obj.Map.isKey(Key)
+                Obj.Map(Image.Uuid) = Image;
+            else
+                Obj.msgLog(ObjLevel.Warning, 'registerImage: Image already exists in map: %s', Key);
+            end
         end
         
         
         function unregisterImage(Obj, Image)
             Obj.msgLog(LogLevel.Debug, 'unregisterImage: %s', Image.Uuid);
+            
+            Key = Obj.getImageKey(Image);
+            if Obj.Map.isKey(Key)
+                Obj.Map.remove(Image.Uuid)
+            else
+                Obj.msgLog(ObjLevel.Warning, 'unregisterImage: Image does not exist in map: %s', Key);
+            end            
+            
+        end
+        
+        
+        function Result = getImagKey(Obj, Image)
+            Result = Image.Uuid;
+        end
+        
+        
+        function release(Obj)
+            for Key=Obj.Map.keys
+                Obj.Map.remove(Key);
+            end
+            
+            % Make sure that everything was removed
+            assert(Obj.Map.Count == 0);
         end
         
     end
@@ -57,7 +93,7 @@ classdef VirtImageManager < Component
 
             Manager = VirtImageManager.getSingle();
             
-            
+            % See tests in VirtImage.m
             
             io.msgLog(LogLevel.Test, 'VirtImageManager test passed');
             Result = true;
---
+++ matlab/util/+tools/+os/iswindows.m
@@ -0,0 +1,6 @@
+function Result = iswindows
+
+Result = ~isunix && ~ismac;
+
+end
+
