e2beb677946b60f89e3332e06e9fa8203ec9dbf6
Chen Tishler <chen@tishler.dev>
2021-05-09 13:28:36 +0300

    utils

---
+++ lazarus/pipeline_utils/About.pas
@@ -0,0 +1,27 @@
+unit About;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs;
+
+type
+  TAboutForm = class(TForm)
+  private
+
+  public
+
+  end;
+
+var
+  AboutForm: TAboutForm;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
+
---
+++ lazarus/pipeline_utils/Main.pas
@@ -0,0 +1,48 @@
+unit Main;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, Menus,
+  About;
+
+type
+
+  { TMainForm }
+
+  TMainForm = class(TForm)
+    MainMenu: TMainMenu;
+    MIXlsxToSql: TMenuItem;
+    MIExit: TMenuItem;
+    MIDatabase: TMenuItem;
+    MIAbout: TMenuItem;
+    MIFile: TMenuItem;
+    MIHelp: TMenuItem;
+    Panel1: TPanel;
+    Panel2: TPanel;
+    procedure MIHelpClick(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  MainForm: TMainForm;
+
+implementation
+
+{$R *.lfm}
+
+{ TMainForm }
+
+procedure TMainForm.MIHelpClick(Sender: TObject);
+begin
+
+end;
+
+end.
+
+
