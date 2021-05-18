e9fcd0bf34d3944af830ee35f6a8dd87df83fc71
Chen Tishler <chen@controlbit.com>
2021-05-18 18:11:57 +0300

    managers base

--- python/managers/lsyncd.py
+++ python/managers/lsyncd.py
@@ -3,6 +3,21 @@
 # Top-Level manager, above Postgres, lsyncd, cloud (TBD)
 
 
+class LsyncdManager(ManagerBase):
+
+    # Constructor
+    def __init__(self):
+        self.interface_name = ''
+
+
+    # Destructor
+    def __del__(self):
+        # Deleted
+        pass
+
+
+
+
 
 
 
