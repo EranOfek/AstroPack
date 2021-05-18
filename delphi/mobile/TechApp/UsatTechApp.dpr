c418790f2d95abe0995c22055575fad64c72cd8e
Chen Tishler <chen@controlbit.com>
2021-05-18 17:57:15 +0300

    apps

---
+++ delphi/mobile/TechApp/UsatTechApp.dpr
@@ -0,0 +1,14 @@
+program UsatTechApp;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  HeaderFooterTemplate in 'HeaderFooterTemplate.pas' {HeaderFooterForm};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(THeaderFooterForm, HeaderFooterForm);
+  Application.Run;
+end.
