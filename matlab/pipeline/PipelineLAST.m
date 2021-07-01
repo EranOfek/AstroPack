968395de991989155c0d585456d5b86961b0b8b6
Chen Tishler <chen@controlbit.com>
2021-07-01 14:55:34 +0300

    pipeline frame

---
+++ matlab/pipeline/PipelineLAST.m
@@ -0,0 +1,45 @@
+
+%--------------------------------------------------------------------------
+
+
+classdef PipelineLAST < PipelineComponent
+    % Properties
+    properties (SetAccess = public)
+        
+        filename        
+        configPath = "";
+        data
+        lines
+        userData
+        
+        inputImagePath
+        inputImageExt
+    end
+    
+    %-------------------------------------------------------- 
+    methods
+        % Constructor    
+        function Obj = PipelineLAST()
+            Obj.setName('PipelineLAST');
+            Obj.needUuid();
+            Obj.msgLog(LogLevel.Debug, 'created: %s', Obj.Uuid);                        
+        end
+     
+
+    end
+
+    
+    % Unit test
+    methods(Static)
+        function Result = unitTest()
+            io.msgStyle(LogLevel.Test, '@start', 'PipelineLAST test started')
+            
+            
+            io.msgStyle(LogLevel.Test, '@passed', 'PipelineLAST test passed')                       
+            Result = true;
+        end
+    end    
+        
+end
+
+
