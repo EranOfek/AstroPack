650fe1bf18f3b4070d4e8053a6b591d8d10d4ad3
Eran Ofek <ofek.eran@gmail.com>
2021-08-09 20:32:25 +0300

    astrometrySubImages started

--- matlab/image/+imProc/+astrometry/astrometrySubImages.m
+++ matlab/image/+imProc/+astrometry/astrometrySubImages.m
@@ -26,6 +26,8 @@ function Result = astrometrySubImages(Obj, Args)
     
     % Center of sub images
     [SubCenterX, SubCenterY] = imUtil.ccdsec.center_ccdsec(Args.CCDSEC);
+    SubCenterX = SubCenterX(:);
+    SubCenterY = SubCenterY(:);
     if isempty(Args.CenterXY)
         % calculate CenterXY of full image from Args.CCDSEC
         Args.CenterXY = [0.5.*(min(Args.CCDSEC(:,1) + max(Args.CCDSEC(:,2))),...
@@ -37,28 +39,48 @@ function Result = astrometrySubImages(Obj, Args)
     [~,SI]  = sort(DistSub);
     
     EnoughCoreSolutions = false;
+    Sucess              = false(size(Obj));  % sucessful solution
     for Iobj=1:1:Nobj
         % for each sub image
         
         % select sub image index, after sorting by distance of sub image
         % from full image
-        Iim = SI(Iobj);
+        
         
         if ~EnoughCoreSolutions
             % run astrometryCore
+            Iim = SI(Iobj);
             
+            
+            % check qulity of solution
+            Sucess(Iim) = 
             
             if Iobj>Args.MinNumberCoreSolutions
                 EnoughCoreSolutions = true;
             end
+            
         else
             % run astrometryRefine
-            
-            % select nearest solution
+            % find a sub image which have nearby sucessful solution
+            % matrix of distances : rows - sucssful; lines - ~sucessful
+            DistSubMat   = sqrt((SubCenterX(Sucess) - SubCenterX(~Sucess).').^2 + (SubCenterY(Sucess) - SubCenterY(~Sucess).').^2);
+            [~,IndMin]   = min(DistSubMat,[],'all','linear');
+            [MinI,MinJ]  = imUtil.image.ind2sub_fast(size(DistSubMat), IndMin);
+            FS           = find(Sucess);
+            FNS          = find(~Sucess);
+            IndSucess    = FS(MinI); 
+            IndNotSucess = FNS(MinJ);
+            % Index of image to solve
+            Iim  = IndNotSucess;
+            % index of image from which to take the WCS solution
+            Iref = IndSucess;
             
             % shift solution to current CCDSEC
             
             % call astrometryRefine
+            
+            % check qulity of solution
+            Sucess(Iim) = 
             
         end
     
