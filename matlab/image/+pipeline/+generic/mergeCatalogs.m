7bb8840109c99781453cb8a826a6451c5fb3e9b5
Eran Ofek <ofek.eran@gmail.com>
2021-09-01 23:32:25 +0300

    pipeline.generic.mergeCatalogs progress

--- matlab/image/+pipeline/+generic/mergeCatalogs.m
+++ matlab/image/+pipeline/+generic/mergeCatalogs.m
@@ -6,17 +6,21 @@ function Result = mergeCatalogs(Obj, Args)
         Args.CooType                 = 'sphere';
         Args.allSourcesArgs cell     = {};
         Args.matchArgs cell          = {};
+        Args.MatchedColums           = {'RA','Dec','PSF_MAG','PSF_MAGERR','APER_MAG_1_','APER_MAGERR_1_','APER_MAG_2_','APER_MAGERR_2_'});
     end
     
     % find all unique sources
     AllSources = imProc.match.allSources(Obj, 'CooType',Args.CooType, Args.allSourcesArgs{:});
     
     % Match catalogs by RA/Dec or X/Y
-    [MatchedObj, UnMatchedObj, TruelyUnMatchedObj] = imProc.match.match(Obj, AllSources, 'CooType',Args.CooType, Args.matchArgs{:})
+    [MatchedObj, UnMatchedObj, TruelyUnMatchedObj] = imProc.match.match(Obj, AllSources, 'CooType',Args.CooType, Args.matchArgs{:});
     
     % Generate a matched sources matrix
+    MatchedS = MatchedSources;
+    MatchedS.addMatrix(MatchedObj, Args.MatchedColums);
     
     % fit PM
+    
     
     % flag PM
     
