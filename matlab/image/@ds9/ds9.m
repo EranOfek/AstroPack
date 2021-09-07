84fedf846f5fc330e14d64aceb359f08dce04050
Chen Tishler <chen@controlbit.com>
2021-09-07 21:52:32 +0300

    class folder

--- matlab/image/@ds9/ds9.m
+++ matlab/image/@ds9/ds9.m
@@ -3265,17 +3265,9 @@ classdef ds9 < handle
 
     
     methods (Static) % Unit-Test
-        function Result = unitTest()
+        Result = unitTest()
             % unitTest for ds9
-            io.msgStyle(LogLevel.Test, '@start', 'ds9 test started');
-           
-            if ~isunix && ~ismac
-                io.msgStyle(LogLevel.Test, 'red', 'ds9 - Windows is not supported yet !!!');
-            end
-            
-            io.msgStyle(LogLevel.Test, '@passed', 'ds9 test passed');
-            Result = true;
-        end
+
     end
     
     
---
+++ matlab/image/@ds9/unitTest.m
@@ -0,0 +1,11 @@
+function Result = unitTest()
+	% unitTest for ds9
+	io.msgStyle(LogLevel.Test, '@start', 'ds9 test started');
+   
+	if ~isunix && ~ismac
+		io.msgStyle(LogLevel.Test, 'red', 'ds9 - Windows is not supported yet !!!');
+	end
+	
+	io.msgStyle(LogLevel.Test, '@passed', 'ds9 test passed');
+	Result = true;
+end
