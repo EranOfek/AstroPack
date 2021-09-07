bc7d06cc1d9344f2a380761161c9a53e78746617
Chen Tishler <chen@controlbit.com>
2021-09-07 18:13:49 +0300

    ut

--- matlab/base/@Component/Component.m
+++ matlab/base/@Component/Component.m
@@ -342,41 +342,7 @@ classdef Component < Base
 
 
     methods(Static) % Unit test
-        function Result = unitTest()
+        Result = unitTest()
             % unitTest for Component class
-            io.msgLog(LogLevel.Test, 'Component test started');
-
-            % Create instances
-            a = Component;
-            a.msgLog(LogLevel.Test, 'a created');
-            b = Component;
-            b.msgLog(LogLevel.Test, 'b created');
-            c = Component;
-            c.msgLog(LogLevel.Test, 'c created');
-
-            % Make sure that we get different Uuids
-            io.msgLog(LogLevel.Test, 'Testing Uuid');
-            a.needUuid();
-            b.needUuid();
-            assert(~all(a.Uuid ~= b.Uuid));
-
-            % Make sure that we get different MapKeys
-            io.msgLog(LogLevel.Test, 'Testing MapKey');
-            a.needMapKey();
-            b.needMapKey();
-            assert(~all(a.MapKey ~= b.MapKey));
-
-            % Generate Uuid and MapKey for arrays
-            c(1) = Component;
-            c(2) = Component;
-            c.msgLog(LogLevel.Test, 'Msg');
-            u = c.needUuid();
-            disp(u);
-            k = c.needMapKey();
-            disp(k);
-
-            io.msgStyle(LogLevel.Test, '@passed', 'Component test passed');
-            Result = true;
-        end
     end
 end
