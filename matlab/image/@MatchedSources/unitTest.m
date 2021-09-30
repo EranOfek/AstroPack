eaa8a9f228d8fdad2fdcb30710a8eb6092ef1e6e
EranOfek <ofek.eran@gmail.com>
2021-09-30 18:50:11 +0300

    unitTest fixes

--- matlab/image/@MatchedSources/unitTest.m
+++ matlab/image/@MatchedSources/unitTest.m
@@ -51,11 +51,9 @@ function Result = unitTest()
 	AC.Catalog  = [1 0; 1 2; 1 1; 2 -1; 2 0; 2.01 0];
 	AC.ColNames = {'RA','Dec'};
 	AC.ColUnits = {'rad','rad'};
-	AC.getCooTypeAuto
 	AC2 = AstroCatalog;
 	AC2.Catalog  = [1 2; 1 1; 2.001 0; 3 -1; 3 0];
 	AC2.ColNames = {'RA','Dec'}; AC2.ColUnits = {'rad','rad'};
-	AC2.getCooTypeAuto
 	[MC,UM,TUM] = imProc.match.match(AC,AC2,'Radius',0.01,'RadiusUnits','rad');
 	[MC,UM,TUM] = imProc.match.match([AC;AC2; AC; AC2],AC2,'Radius',0.01,'RadiusUnits','rad');
 	% run addMatrix with AstroCatalog input
