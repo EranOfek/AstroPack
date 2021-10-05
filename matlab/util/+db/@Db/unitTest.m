e28789961c6a0fe3c6e4d27db373bb0de8d3ac11
Chen Tishler <chen@tishler.dev>
2021-10-05 16:15:07 +0300

    draft

--- matlab/util/+db/@Db/unitTest.m
+++ matlab/util/+db/@Db/unitTest.m
@@ -3,9 +3,6 @@ function Result = unitTest()
     
     io.msgStyle(LogLevel.Test, '@start', 'Db test started\n');
 
-    Glb = Db;
-
-
     % Done
     io.msgStyle(LogLevel.Test, '@passed', 'Db test passed')
     Result = true;
