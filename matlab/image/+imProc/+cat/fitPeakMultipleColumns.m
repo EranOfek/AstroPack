20d62475156e2f389c59ae33412e440bd4f2b39c
EranOfek <ofek.eran@gmail.com>
2021-10-06 18:09:39 +0300

    fitPeakMultipleColumns

--- matlab/image/+imProc/+cat/fitPeakMultipleColumns.m
+++ matlab/image/+imProc/+cat/fitPeakMultipleColumns.m
@@ -1,11 +1,13 @@
 function [FitRes, Result] = fitPeakMultipleColumns(Obj, Args)
     %
+    % Example: X=rand(100,3);
+    %          [FitRes, Result] = imProc.cat.fitPeakMultipleColumns(X, 'Pos',[1 2 3])
    
     arguments
         Obj                                       % matrix | AstroImage | AstroCatalog
         Args.Cols                            = {'SN_1','SN_2','SN_3'};
         Args.Pos                             = [];                           % must have the same length as Cols
-        
+        Args.FlagBad logical                 = true; % modify MaxFitPos in cases there is no max solution, or solution is out of range
         Args.InsertBestVal logical           = true;
         Args.InsertBestInd logical           = true;
         Args.InsertMaxFitVal logical         = true;
@@ -13,7 +15,7 @@ function [FitRes, Result] = fitPeakMultipleColumns(Obj, Args)
         Args.CreateNewObj logical            = false;
     end
     
-    if numel(Args.Col)~=numel(Args.Pos)
+    if numel(Args.Cols)~=numel(Args.Pos)
         error('Number of Cols must be equal to number of Pos');
     end
     
@@ -28,6 +30,7 @@ function [FitRes, Result] = fitPeakMultipleColumns(Obj, Args)
         Nobj = numel(Obj);
     end
         
+    Result = Obj;
     for Iobj=1:1:Nobj
         % for each image/catalog
         if isa(Obj, 'AstroCatalog')
@@ -52,9 +55,46 @@ function [FitRes, Result] = fitPeakMultipleColumns(Obj, Args)
         Data = Data.';  % column per source
         
         Par = H\Data;
-        X_ext        = -Par(2)./(2.*Par(3));
-        SecDerivSign = sign(Par(3)); 
-        if X_ext
+        X_ext           = (-Par(2,:)./(2.*Par(3,:))).';
+        SecDerivSign    = sign(Par(3,:)).'; 
+        Flag.IsMax      = SecDerivSign<0;
+        Flag.IsBelowMin = X_ext<Args.Pos(1);
+        Flag.IsAboveMax = X_ext>Args.Pos(end);
         
-    
+        MaxFitPos          = X_ext;
+        MaxFitVal          = Par(1) + X_ext.*(Par(2) + X_ext.*Par(3));   % second order polynomial
+        [BestVal, BestInd] = max(Data,[],1);
+        
+        FitRes.MaxFitPos = MaxFitPos;
+        FitRes.MaxFitVal = MaxFitVal;
+        FitRes.BestVal   = BestVal.';
+        FitRes.BestInd   = BestInd.';
+        FitRes.Flag      = Flag;
+        
+        if Args.FlagBad
+            % modify MaxFitPos in cases there is no max solution, or solution
+            % is out of range
+            FitRes.MaxFitPos(Flag.IsBelowMin) = Args.Pos(1);
+            FitRes.MaxFitPos(Flag.IsAboveMax) = Args.Pos(end);
+            FitRes.MaxFitPos(~Flag.IsMax)     = NaN;
+        end
+        
+        % insert columns to output table
+        % only if not a matrix
+        if ~isnumeric(Obj)
+            if  Args.InsertBestVal
+                
+            end
+            if Args.InsertBestInd
+                
+            end
+            if Args.InsertMaxFitVal
+                
+            end
+            if Args.InsertMaxFitPos
+                
+            end
+        end
+    end
+        
 end
