66161aedd82be11250f09e99b17152ca36bd6120
EranOfek <ofek.eran@gmail.com>
2021-10-07 21:45:26 +0300

    classifySources

--- matlab/image/+imProc/+sources/classifySources.m
+++ matlab/image/+imProc/+sources/classifySources.m
@@ -1,17 +1,24 @@
-function classifySources(Obj, Args)
+function [Result, Flag] = classifySources(Obj, Args)
     %
    
     arguments
         Obj
-        Args.ColName_SharpSN     = 'SN_1';
-        Args.ColNamsSN           = 'SN_%d';   % or cell array of column names
+        Args.ColNamsSN           = 'SN_%d';   % or cell array of column names, sharp is always in 1
         Args.SigmaPSF            = [];
         
-        Args.ColNameBack         = 'BACK_ANNULUS';
-        Args.ColNameStd          = 'STD_ANNULUS';
+        Args.ThresholdSN         = 5;
+        Args.BackratioLimit      = 1.5;
+        Args.VarRatioLimit       = 1.5;
+        
+        Args.ColNameFlux         = {'FLUX_CONV_2','FLUX_CONV_3'};
+        Args.ColNameBackAnn      = 'BACK_ANNULUS';
+        Args.ColNameStdAnn       = 'STD_ANNULUS';
+        Args.ColNameBackIm       = 'BACK_IM';
+        Args.ColNameVarIm        = 'VAR_IM';
         
         Args.ColNameMom2         = {'X2','Y2','XY'};
         
+        Args.CreateNewObj logical = false;
     end
     
     NsigmaPSF  = numel(Args.SigmaPSF);
@@ -30,14 +37,64 @@ function classifySources(Obj, Args)
     
     Nobj = numel(Obj);
     
+    
+    Result = Obj;
     for Iobj=1:1:Nobj
-    
-        getCol(Obj, Args.ColNameBack);
-        getCol(Obj, Args.ColNameStd);
-        getCol(Obj, ColNamsSN);
-        getCol(Obj, Args.ColNameMom2);
-    end
+        if isa(Obj, 'AstroImage')
+            if Args.CreateNewObj
+                Cat = Obj(Iobj).CatData.copy;
+            else
+                Cat = Obj(Iobj).CatData;
+            end
+        elseif isa(Obj, 'AstroCatalog')
+            if Args.CreateNewObj
+                Cat = Obj(Iobj).copy;
+            else
+                Cat = Obj(Iobj);
+            end
+        else
+            error('First input argument must be an AstroImage or AstroCatalog object');
+        end
         
-    
+        BackAnn = getCol(Obj, Args.ColNameBackAnn);
+        StdAnn  = getCol(Obj, Args.ColNameStdAnn);
+        BackIm  = getCol(Obj, Args.ColNameBackIm);
+        VarIm   = getCol(Obj, Args.ColNameVarIm);
+        SN      = getCol(Obj, ColNamsSN);
+        Mom2    = getCol(Obj, Args.ColNameMom2);
+        Flux    = getCol(Obj, Args.ColNameFlux);
+        
+        SN_FluxAnn = Flux./StdAnn;
+        Flag.BadSN = all(SN_FluxAnn < Args.ThresholdSN,2);
+        Flag.CR    = SN(:,1) > SN(:,2);
+        
+        [~,MaxI] = max(SN,[],2);  % index of matche filter that maximize S/N
+        ModeI    = mode(MaxI);
+        
+        if ModeI>(NsigmaPSF+1)
+            % the typical sources has the largest match filter size
+            % can't find extended sources
+            Flag.Extended = false([size(SN,1) 1]);
+        else
+            Flag.Extended = SN(:,ModeI+2) > SN(:,ModeI);
+        end
+        
+        Flag.AnomBack = (BackAnn./BackIm) > Args.BackRatioLimit;
+        Flag.AnomVar  = (StdAnn.^2./VarIm) > Args.VarRatioLimit;
+        
+        % remove bad sources
+        
+        
+        % add columns with flags
+        
+        
+        % save in Result
+        if isa(Obj, 'AstroImage')
+            Result(Iobj).CatData = Cat;
+        else
+            Result(Iobj)         = Cat;
+        end
+        
+    end    
     
 end
