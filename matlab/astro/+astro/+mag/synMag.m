a31e2fe12dcab1374a454b4471c70f8af6455da8
EranOfek <ofek.eran@gmail.com>
2021-10-10 19:41:50 +0300

    synMag

--- matlab/astro/+astro/+mag/synMag.m
+++ matlab/astro/+astro/+mag/synMag.m
@@ -1,5 +1,7 @@
 function Mag = synMag(Spec, Trans, System, Args)
-    %
+    % Calculate the synthetic magnitude of a spectrum and transmission
+    %   in the Vega, AB, or STMAG systems.
+    % Input  : - 
     % Example: Trans = [6000 0; 6001 1; 7000 1; 7001 0];
     %          W=(3000:1:1e4)'; [~,~,Fl] = astro.spec.black_body(5700,W);
     %          Spec  = [W, Fl.*(constant.SunR.^2)./(10.*constant.pc).^2];
@@ -24,7 +26,7 @@ function Mag = synMag(Spec, Trans, System, Args)
         case 'ab'
             Msys = 0;
             AB0  = 10.^(-0.4.*48.6).*1e23;  % AB zero point in Jy
-            FlambdaSys = [Wave, convert.flux(AB0.*1000, 'mJy', 'cga/A', Wave, 'A')];
+            FlambdaSys = [Wave, convert.flux(AB0.*1000, 'mJy', 'cgs/A', Wave, 'A')];
         case 'stmag'
             Msys = 0;
             STMAG0     = 10.^(-0.4.*21.10);
@@ -33,7 +35,7 @@ function Mag = synMag(Spec, Trans, System, Args)
             error('Unknown System option');
     end
     
-    Mag = trapz(Flux .* Trans(:,2) .* Wave)./trapz(FlambdaSys(:,2) .* Trans(:,2) .* Wave) + Msys;
+    Mag = -2.5.*log10(trapz(Flux .* Trans(:,2) .* Wave)./trapz(FlambdaSys(:,2) .* Trans(:,2) .* Wave)) + Msys;
     
     
 end
