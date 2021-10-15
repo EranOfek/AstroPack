9a7e088e5b77d7159bea3100118ec3faed64c6ca
EranOfek <ofek.eran@gmail.com>
2021-10-15 21:46:52 +0300

    ple_all

--- matlab/astro/+celestial/+SolarSys/ple_all.m
+++ matlab/astro/+celestial/+SolarSys/ple_all.m
@@ -6,9 +6,19 @@ function S = ple_all(JD, OutType)
         OutType
     end
    
+   
+    Npl  = 8;
+    Njd  = numel(JD);
+    Ncoo = 3;
+    
+    XYZ = zeros(Ncoo, Njd, Npl);
     
     [L, B, R] = celestial.SolarSys.ple_mercury(JD);
     [X, Y, Z] = celestial.SolarSys.ec_longlat2cart(L, B, R, JD, OutType);
+    
+    XYZ(1,:,1) = X;
+    XYZ(2,:,1) = Y;
+    XYZ(3,:,1) = Z;
     
     celestial.SolarSys.ple_venus
     celestial.SolarSys.ple_earth
