cbbb8732b3eeeb795b131934e749796a9ed83daa
EranOfek <ofek.eran@gmail.com>
2021-11-16 22:23:33 +0200

    fwhm_fromBank

--- matlab/image/+imProc/+asteroids/searchAsteroids_orphans.m
+++ matlab/image/+imProc/+asteroids/searchAsteroids_orphans.m
@@ -2,13 +2,58 @@ function searchAsteroids_orphans(Obj, Args)
     %
     
     arguments
-        Obj AstroCatalog
+        Obj                               % AstroImage or AstroCatalog
         
-        Args.OrphanFlag          = [];
+        Args.CooType              = 'sphere';
+        
+        
+        Args.OrphanSelectFun     = {'Nobs', @eq(X) X>0 & X<3};   % {ColName, Fun, ColName, Fun,...}
+        Args.JD                  = [];
     end
     
+    Nobj = numel(Obj);
     
-    R = tools.math.fit.ransacLinear2d(TXY(:,2:3),TXY(:,1))
+    if isempty(Args.JD)
+        if isa(Obj, 'AstroImage')
+            Args.JD = julday(Obj);
+        end
+    end
+    if isempty(Args.JD)
+        Args.JD = (1:1:Nobj).';
+    end
+    
+    switch lower(Args.CooType)
+        case 'sphere'
+            DistFun = @celestial.coo.sphere_dist_fast;
+        case 'pix'
+            DistFun = @tools.math.geometry.plane_dist;
+        otherwise
+            error('Unknown CooType option');
+    end
+    
+    for Iobj=1:1:Nobj
+        if isa(Obj, 'AstroImage')
+            Cat = Obj(Iobj).CatDat;
+        else
+            Cat = Obj(Iobj);
+        end
+        
+        [~, Flag] = queryFun(Cat, Args.OrphanSelectFun, 'ReturnResult', false, 'CreateNewObj',false);
+        switch lower(Args.CooType)
+            case 'sphere'
+                XY = getLonLat(Cat, 'rad');
+            case 'pix'
+                XY = getXY(Cat);
+        end
+        XY = XY(Flag,:);
+        
+        % search all XY positions that has enough neighboors
+        Nsrc = size(XY, 1);
+        for Isrc=1:1:Nsrc
+            Dist = DistFun(XY(Isrc,1), XY(Isrc,2), XY(:,1), XY(:,2));
+            
+        
+        R = tools.math.fit.ransacLinear2d(XY(Flag,:), Args.JD);
     
     
 end
