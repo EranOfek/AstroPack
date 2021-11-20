4ad06641dab9581ee2a7a48246a9f5d3895c07ca
EranOfek <ofek.eran@gmail.com>
2021-11-20 22:07:49 +0200

    celestial.coo.sphere_dist_fastSmall

--- matlab/astro/+celestial/+coo/sphere_dist_fastSmall.m
+++ matlab/astro/+celestial/+coo/sphere_dist_fastSmall.m
@@ -1,7 +1,7 @@
 function Dist = sphere_dist_fastSmall(RA1, Dec1, RA2, Dec2)
     % Spherical distance approximation for small angular distances
     %       For 50'' distances this approximation is good to about 1 mas.
-    %       This is about ~x2 faster than celestial.coo.sphere_dist_fast
+    %       This is about ~x1.5 faster than celestial.coo.sphere_dist_fast
     % Input  : - Longitude 1 [radians]
     %          - Latitude 1 [radians]
     %          - Longitude 2 [radians]
@@ -10,7 +10,9 @@ function Dist = sphere_dist_fastSmall(RA1, Dec1, RA2, Dec2)
     % Author : Eran Ofek (Nov 2021)
     % Example: D2=celestial.coo.sphere_dist_fastSmall(R1,R1,R2,R2)
     
-    Dist = sqrt(((RA1 - RA2).*cos(Dec1)).^2 + (Dec1-Dec2).^2);
+    error('BUG')
+    DRA  = mod(RA1 - RA2, 2.*pi).*cos(Dec1);
+    Dist = sqrt(DRA.^2 + (Dec1-Dec2).^2);
     
 end
 
