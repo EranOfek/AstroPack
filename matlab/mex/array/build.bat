11bc4481eebacfbe34271142c3c95da88979eecc
Chen Tishler <chen@tishler.dev>
2021-11-29 15:35:38 +0200

    mex

--- matlab/mex/array/build.bat
+++ matlab/mex/array/build.bat
@@ -1,3 +1,11 @@
-call mex mex_bitand_array64.c -lut
+call mex mex_bitor_array8.c -lut
+call mex mex_bitor_array16.c -lut
+call mex mex_bitor_array32.c -lut
 call mex mex_bitor_array64.c -lut
+
+call mex mex_bitand_array8.c -lut
+call mex mex_bitand_array16.c -lut
+call mex mex_bitand_array32.c -lut
+call mex mex_bitand_array64.c -lut
+
 pause
