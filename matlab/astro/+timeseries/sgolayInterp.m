0ebe5af50cc1c784693eb93deaec1346e8ad40fc
EranOfek <ofek.eran@gmail.com>
2021-12-02 14:08:57 +0200

    expediting code

---
+++ matlab/astro/+timeseries/sgolayInterp.m
@@ -0,0 +1,16 @@
+function sgolayInterp(T, Y, Order, Args)
+    %
+   
+    arguments
+        T
+        Y
+        TimeFrame
+        Order       = 1;
+        Args
+    end
+    
+    
+    
+    
+    
+end
