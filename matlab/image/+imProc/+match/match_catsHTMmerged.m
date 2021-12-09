638e869d5e6867db4fcc10abd22e5577ff8a09bf
EranOfek <ofek.eran@gmail.com>
2021-12-09 20:48:08 +0200

    imProc.match.match_catsHTMmerged

--- matlab/image/+imProc/+match/match_catsHTMmerged.m
+++ matlab/image/+imProc/+match/match_catsHTMmerged.m
@@ -2,16 +2,52 @@ function Result = match_catsHTMmerged(Obj, Args)
     % Match an AstroCatalog with the catsHTM MergedCat.
     %   MergedCat is a special catsHTM catalog of several merged catalogs.
     %   It contains 4 columns [RA, Dec, BitFlag, AngRadius]
-    
+    % Input  : - An AstroImage or AstroCatalog object.
+    %          * ...,key,val,...
+    %            'SameField' - A logical indicating if all the catalogs
+    %                   covers the same field of view.
+    %                   Used only for expediting the code.
+    %                   Default is false.
+    %            'Con' - A cell array of constraints.
+    %                   See catsHTM.cone_search for details.
+    %                   Default is {}.
+    %            'ColPos' - Position in which to add the new column.
+    %                   Dfault is Inf.
+    %            'FlagColNames' - Column name to add.
+    %                   This column is the bit-wise or between all the
+    %                   MergedCat sources found near the source.
+    %                   Default is 'MergedCatMask'.
+    %            'CreateNewObj' - Create a new copy of the inout object.
+    %                   Default is false.
+    %            'MergedCatName' - Name of the catsHTM MergedCat catalog.
+    %                   Default is 'MergedCat'.
+    %            'MergedCatMaskCol' - Col name in the MergedCat catalog
+    %                   containing the bit mask.
+    %                   Default is 3.
+    %            'MergedCatRadiusCol' - Col name in the MergedCat catalog
+    %                   containing the uncertany radius of the specific
+    %                   catalog from which the MergedCat object arrived.
+    %                   Default is 4.
+    % Output : - The input object, in which a new column with the bit mask
+    %            of all catalogs in MergedCat that were found near the
+    %            specific soource.
+    % Author : Eran Ofek (Dec 2021)
+    % Example: AC = AstroCatalog({[1 1]},{'RA','Dec'});
+    %          Result = imProc.match.match_catsHTMmerged(AC)
     
     arguments
         Obj
         Args.SameField logical            = false;
+        Args.Con                          = {};
+        Args.ColPos                       = Inf;
+        Args.FlagColNames                 = 'MergedCatMask';
+        
         Args.CreateNewObj logical         = false;
         
         Args.MergedCatName                = 'MergedCat';
+        Args.MergedCatMaskCol             = 3;
         Args.MergedCatRadiusCol           = 4;
-        Args.Con                          = {};
+        
     end
    
     if Args.CreateNewObj
@@ -42,23 +78,33 @@ function Result = match_catsHTMmerged(Obj, Args)
         % match CatH against the Cat
         % catH must be the ref catalog
         ResInd = imProc.match.matchReturnIndices(Cat, CatH, 'CooType','sphere',...
-                                                            'Radius',MaxSearchradius,...
+                                                            'Radius',MaxSearchRadius,...
                                                             'RadiusUnits','arcsec');
         
-        % git here
-        ResInd.Obj2_IndInObj1
-        
+        % Merged Mask bits for each source
+        % For each source in the ref catalog (CatH), if there is a matching
+        % source within the search radius, then add the bit mask of the ref
+        % source to that of the Cat source.
+        % The addition is 
         FlagNaN = ResInd.Obj2_Dist > CatH.Catalog(:,Args.MergedCatRadiusCol);
         ResInd.Obj2_IndInObj1(FlagNaN) = NaN;
         MergedCatFlag = zeros(numel(ResInd.Obj1_IndInObj2),1);
+        Nref = numel(ResInd.Obj2_IndInObj1);
+        for Iref=1:1:Nref
+            if ~isnan(ResInd.Obj2_IndInObj1(Iref))
+                IndCat = ResInd.Obj2_IndInObj1(Iref);
+                MergedCatFlag(IndCat) = MergedCatFlag(IndCat) + CatH.Catalog(Iref,Args.MergedCatMaskCol);
+            end
+        end
+     
+        % Insert column to catalog
+        Cat = insertCol(Cat, MergedCatFlag, Args.ColPos, Args.FlagColNames, '');
         
-        
-    %                   its matched indices in Obj1. NaN if no match.
-    %            'Obj2_Dist' - A vector, for each source in Obj2, of the
-    %                   angular distance ['rad' if 'sphere'] between the
-    %                   matched sources.
-    %            'Obj2_NmatchObj1' - A vector, for each source in Obj2, of the
-    %                   number of matches within search radius.
+        if isa(Obj, 'AstroImage')
+            Result(Iobj).CatData = Cat;
+        else
+            Result(Iobj) = Cat;
+        end
         
     end
 
