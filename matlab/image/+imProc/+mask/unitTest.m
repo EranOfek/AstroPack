5ea9c47baac58bcff7cf1d738aa6cac91acd4da1
amir-24 <amir.sharon@weizmann.ac.il>
2021-12-15 18:49:37 +0200

    imProc.mask tests

--- matlab/image/+imProc/+mask/unitTest.m
+++ matlab/image/+imProc/+mask/unitTest.m
@@ -1,6 +1,6 @@
 function Result = unitTest()
     % unitTest for +imProc.mask
-    % Example: imProc.mask.unitTest
+    io.msgLog(LogLevel.Test, 'imProc.mask test started');
     
     AI = AstroImage({rand(10,10).*1000});
     Result = imProc.mask.maskSaturated(AI, 'SatLevel',500);
@@ -16,6 +16,22 @@ function Result = unitTest()
         error('Problem with replaceMaskedPixVal');
     end
     
-    Result = true;
+    AI = AstroImage({rand(50).*5},'Back',{ones(50)},'Var',{ones(50)});
+
+%     doesn't work because of imUtil.image.local_maxima(-SN, 1, Args.Threshold, Args.Conn);
+%     Result = imProc.mask.maskHoles(AI,'Threshold',-3);
+%     Result = imProc.mask.maskHoles(AI, 'SN',AI.Image,'Threshold',3);
     
+    AI = AstroImage({rand(10).*linspace(1,1,10)});
+%     AI.Image( 
+    Flag = false(10);
+    Flag(2,2)= true;
+    AI.MaskData = maskSet(AI.MaskData, Flag, 'Saturated');
+    
+    imProc.mask.interpOverMaskedPix(AI,'BitNamesToInterp',{'Saturated'});
+    [Result, XY, Ind] = findBit(AI.MaskData, {'Interpolated'});
+%     assert(~isnan(AI.Image(2,2)) && ~XY
+
+    io.msgStyle(LogLevel.Test, '@passed', 'imProc.mask test passed');
+    Result = true; 
 end
