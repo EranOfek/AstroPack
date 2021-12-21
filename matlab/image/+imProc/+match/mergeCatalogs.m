0a3163ec0aceda5261927aa85cf4c7fc4758e43e
EranOfek <ofek.eran@gmail.com>
2021-12-21 16:55:29 +0200

    sysrem

--- matlab/image/+imProc/+match/mergeCatalogs.m
+++ matlab/image/+imProc/+match/mergeCatalogs.m
@@ -119,14 +119,14 @@ function [MergedCat, MatchedS, ResZP, ResVar, FitMotion] = mergeCatalogs(Obj, Ar
         Args.FitPM logical           = true;
         Args.fitMotionArgs cell      = {'Prob',1e-5};
         
-        Args.MatchedColums           = {'RA','Dec','X','Y','SN_1','SN_2','SN_3','SN_4','MAG_CONV_2','MAGERR_CONV_2','MAG_CONV_3','MAGERR_CONV_3','FLAGS','BACK_IM','VAR_IM','BACK_ANNULUS','STD_ANNULUS'};
+        Args.MatchedColums           = {'RA','Dec','X','Y','SN_1','SN_2','SN_3','SN_4','MAG_CONV_2','MAGERR_CONV_2','MAG_CONV_3','MAGERR_CONV_3','MAG_APER_2','MAG_APER_3','FLUX_APER_3','FLAGS','BACK_IM','VAR_IM','BACK_ANNULUS','STD_ANNULUS'};
         
         Args.ColNameFlags            = 'FLAGS';
         Args.ColNamesStat            = {'RA','Dec','X','Y','MAG_CONV_2', 'MAG_CONV_3','SN_1','SN_2','SN_3','SN_4','BACK_IM','VAR_IM','BACK_ANNULUS','STD_ANNULUS'};  % must be a subset of MatchedColums
         Args.FunIndStat              = {[1 3], [1 3], [1 3], [1 3], [1:8], [1:8], [1 3], [1 3], [1 3], [1 3], [1 3], [1 3], [1 3], [1 3]};
         
         Args.ColNamesAll             = {'MAG_CONV_2','MAGERR_CONV_2'};
-        Args.MagCalibColName         = 'MAG_CONV_2';
+        Args.MagCalibColName         = 'MAG_APER_3'; %'MAG_CONV_2';
         Args.MagCalibErrColName      = 'MAGERR_CONV_2';
 
         Args.unifiedSourcesCatalogArgs cell     = {};
