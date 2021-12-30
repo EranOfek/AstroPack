5890ecb48a6ccf3393a302f76e90c97fb9ca7b40
EranOfek <ofek.eran@gmail.com>
2021-12-30 22:59:47 +0200

    psfPhotCube - basic functionality works

--- matlab/image/+imUtil/+sources/psfPhotCube.m
+++ matlab/image/+imUtil/+sources/psfPhotCube.m
@@ -1,8 +1,12 @@
-function psfPhotCube(Cube, Args)
-    %
+function Result = psfPhotCube(Cube, Args)
+    % 
     % Example: P=imUtil.kernel2.gauss;
     %          Ps=imUtil.trans.shift_fft(P,0.4,0.7);
     %          imUtil.sources.psfPhotCube(Ps, 'PSF',P)
+    %          P=imUtil.kernel2.gauss(1.5.*ones(4,1));
+    %          Ps=imUtil.trans.shift_fft(P,[0.4;0.7;-1.1;3.6],[0.7;-0.2;-0.9;-2.6]);
+    %          Ps = Ps.*permute([100 110 200 300],[1 3 2]) + randn(15,15);
+    %          Result = imUtil.sources.psfPhotCube(Ps, 'PSF',P(:,:,1))
    
     arguments
         Cube
@@ -10,9 +14,10 @@ function psfPhotCube(Cube, Args)
         Args.Yinit      = [];
         Args.PSF        = 1.5;  % scalar will generate a Gaussian PSF - normalized to 1
         Args.MaxIter    = 10;
-        Args.Std        = 0.001;   % vector or cube
+        Args.Std        = 1;   % vector or cube
         Args.SmallStep  = 1e-4;
         Args.MaxStep    = 1;
+        Args.ConvThresh = 1e-4;
     end
     
     if ndims(Args.Std)<3
@@ -52,55 +57,65 @@ function psfPhotCube(Cube, Args)
     DY = Y - Ycenter + StepY;
         
     VecD = [0, Args.SmallStep, 2.*Args.SmallStep];
+    H    = VecD.'.^[0, 1, 2];
     Ind   = 0;
     NotConverged = true;
     while Ind<Args.MaxIter && NotConverged
         Ind = Ind + 1;
-        
-        %Xcenter + StepX
-        %Ycenter + StepY
-        DX
-        DY
-        
+       
         % calc \chi2 and gradient
         Chi2     = internalCalcChi2(Cube, Std, Args.PSF, DX,                   DY,                WeightedPSF);
         Chi2_Dx  = internalCalcChi2(Cube, Std, Args.PSF, DX+Args.SmallStep,    DY,                WeightedPSF);
         Chi2_Dx2 = internalCalcChi2(Cube, Std, Args.PSF, DX+Args.SmallStep.*2, DY,                WeightedPSF);
-        ParX     = polyfit(VecD, [Chi2, Chi2_Dx, Chi2_Dx2], 2);
         
+        %ParX     = polyfit(VecD, [Chi2, Chi2_Dx, Chi2_Dx2], 2);
+        ParX     = H\[Chi2.'; Chi2_Dx.'; Chi2_Dx2.'];
         
         %Chi2     = internalCalcChi2(Cube, Std, Args.PSF, DX, DY,                  WeightedPSF);
         Chi2_Dy  = internalCalcChi2(Cube, Std, Args.PSF, DX, DY+Args.SmallStep,   WeightedPSF);
         Chi2_Dy2 = internalCalcChi2(Cube, Std, Args.PSF, DX, DY+Args.SmallStep.*2,WeightedPSF);
-        ParY     = polyfit(VecD, [Chi2, Chi2_Dy, Chi2_Dy2], 2);
-        DY       = DY -ParY(2)./(2.*ParY(1));
         
-        DX       = DX -ParX(2)./(2.*ParX(1));
+        %ParY     = polyfit(VecD, [Chi2, Chi2_Dy, Chi2_Dy2], 2);
+        ParY     = H\[Chi2.'; Chi2_Dy.'; Chi2_Dy2.'];
         
-        Chi2_Dy = internalCalcChi2(Cube, Std, Args.PSF, DX,                DY+Args.SmallStep, WeightedPSF);
+        StepX    = -ParX(2,:)./(2.*ParX(3,:));
+        StepY    = -ParY(2,:)./(2.*ParY(3,:));
         
-        Chi2mDof = (Chi2 - Dof).*Args.SmallStep;
-        FlagNeg  = Chi2mDof < 0;
-        Chi2mDof(FlagNeg) = abs(Chi2mDof).*0.5;
+        NotMinimaX = ParX(3,:)<0;
+        NotMinimaY = ParY(3,:)<0;
         
-        StepX = min(Chi2mDof./(Chi2 - Chi2_Dx), Args.MaxStep);
-        StepY = min(Chi2mDof./(Chi2 - Chi2_Dy), Args.MaxStep);
-        DX    = DX + StepX;
-        DY    = DY + StepY;
+        % reverse sign for maxima...
+        StepX(NotMinimaX) = -StepX(NotMinimaX);
+        StepY(NotMinimaY) = -StepY(NotMinimaY);
         
+        
+        StepX    = sign(StepX).*min(abs(StepX), Args.MaxStep);
+        StepY    = sign(StepY).*min(abs(StepY), Args.MaxStep);
+        
+        DX       = DX + StepX;
+        DY       = DY + StepY;
+         
         % stoping criteria
-        %Ind
-        %StepX
-        %StepY
+        if max(abs(StepX))<Args.ConvThresh && max(abs(StepX))<Args.ConvThresh
+            NotConverged = false;
+        end
         
         
     end
+    % final fit and return flux
+    [Result.Chi2, Result.Flux]  = internalCalcChi2(Cube, Std, Args.PSF, DX,                   DY,                WeightedPSF);
     
+    Result.DX = DX(:);
+    Result.DY = DY(:);
+    Result.Xinit = Args.Xinit;
+    Result.Yinit = Args.Yinit;
+    Result.Xcenter = Xcenter;
+    Result.Ycenter = Ycenter;
     
     
 end
 
-function Chi2 = internalCalcChi2(Cube, Std, PSF, DX, DY, WeightedPSF)
+function [Chi2,WeightedFlux] = internalCalcChi2(Cube, Std, PSF, DX, DY, WeightedPSF)
     % Return Chi2 for specific PSF and Cube
     % shift PSF
     ShiftedPSF = imUtil.trans.shift_fft(PSF, DX, DY);
@@ -110,4 +125,9 @@ function Chi2 = internalCalcChi2(Cube, Std, PSF, DX, DY, WeightedPSF)
     % search / remove outliers
 
     Chi2  = sum( (Resid./Std).^2, [1 2], 'omitnan');
+    Chi2  = squeeze(Chi2);
+    
+    if nargout>1
+        WeightedFlux = squeeze(WeightedFlux);
+    end
 end
