31c762b059cd6c4e529ceba3b7da901c37462e93
EranOfek <ofek.eran@gmail.com>
2021-12-30 16:47:44 +0200

    header config synomyms

--- matlab/image/+imUtil/+sources/psfPhotCube.m
+++ matlab/image/+imUtil/+sources/psfPhotCube.m
@@ -3,15 +3,26 @@ function psfPhotCube(Cube, Args)
    
     arguments
         Cube
-        Args.Xinit   = [];
-        Args.Yinit   = [];
-        Args.PSF     = 1.5;  % scalar will generate a Gaussian PSF - normalized to 1
-        Args.MaxIter = 10;
+        Args.Xinit      = [];
+        Args.Yinit      = [];
+        Args.PSF        = 1.5;  % scalar will generate a Gaussian PSF - normalized to 1
+        Args.MaxIter    = 10;
+        Args.Std        = [];   % vector or cube
+        Args.SmallStep  = 1e-4;
+        Args.MaxStep    = 1;
     end
+    
+    if ndims(Args.Std)<3
+        Std = permute(Args.Std(:),[3 2 1]);
+    else
+        Std = Args.Std;
+    end
+    
     
     [Ny, Nx, Nim] = size(Cube);
     Xcenter = Nx.*0.5 + 0.5;
     Ycenter = Ny.*0.5 + 0.5;
+    Dof     = Nx.*Ny - 3;
     
     if numel(Args.PSF)==1
         Args.PSF = imUtil.kernel2.gauss(Args.PSF);
@@ -25,19 +36,30 @@ function psfPhotCube(Cube, Args)
     X = Args.Xinit;
     Y = Args.Yinit;
     
-    Ind = 0;
+    StepX = 0;
+    StepY = 0;
+    Ind   = 0;
     NotConverged = true;
     while Ind<Args.MaxIter && NotConverged
         Ind = Ind + 1;
         
-        DX = X - Xcenter;
-        DY = Y - Ycenter;
+        DX = X - Xcenter + StepX;
+        DY = Y - Ycenter + StepY;
         
-        Chi2 = internalCalcChi2(Cube, PSF, DX, DY, WeightedPSF);
+        % calc \chi2 and gradient
+        Chi2    = internalCalcChi2(Cube, Std, PSF, DX,                DY,                WeightedPSF);
+        Chi2_Dx = internalCalcChi2(Cube, Std, PSF, DX+Args.SmallStep, DY,                WeightedPSF);
+        Chi2_Dx = internalCalcChi2(Cube, Std, PSF, DX,                DY+Args.SmallStep, WeightedPSF);
         
-        % calc Chi2 gradient
+        Chi2mDof = (Chi2 - Dof).*Args.SmallStep;
+        FlagNeg  = Chi2mDof < 0;
+        Chi2mDof(FlagNeg) = abs(Chi2mDof).*0.5;
         
-    
+        StepX = min(Chi2mDof./(Chi2 - Chi2_Dx), Args.MaxStep);
+        StepY = min(Chi2mDof./(Chi2 - Chi2_Dy), Args.MaxStep);
+        
+        % stoping criteria
+        
         
     end
     
@@ -45,7 +67,7 @@ function psfPhotCube(Cube, Args)
     
 end
 
-function Chi2 = internalCalcChi2(Cube, PSF, DX, DY, WeightedPSF)
+function Chi2 = internalCalcChi2(Cube, Std, PSF, DX, DY, WeightedPSF)
     % Return Chi2 for specific PSF and Cube
     % shift PSF
     ShiftedPSF = imUtil.trans.shift_fft(PSF, DX, DY);
