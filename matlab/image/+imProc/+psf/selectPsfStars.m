4bb4d7028946cdec2c1035f5330439ab58c9f86d
EranOfek <ofek.eran@gmail.com>
2022-01-09 21:40:36 +0200

    selectPsfStars

--- matlab/image/+imProc/+psf/selectPsfStars.m
+++ matlab/image/+imProc/+psf/selectPsfStars.m
@@ -1,6 +1,28 @@
-function [PsfXY, Flag, Cube] = selectPsfStars(Obj, Args)
+function [PsfXY, Flag] = selectPsfStars(Obj, Args)
     % Select PSF stars from AstroCatalog
-    %
+    %   Selection is based on:
+    %   1. S/N in range 
+    %   2. Reject delta function by DeltaSN
+    %   3. Select sources with second moment around the mode
+    % Input  : - A single element AstroImage or AstroCatalog object.
+    %          * ...,key,val,...
+    %            'ColSN' - Column name for S/N. Default is 'SN_2'.
+    %            'RangeSN' - Select S/N in range. Default is [30 500].
+    %            'ColSNdelta' - Column name for S/N of delta fun.
+    %                   Default is 'SN_1'.
+    %            'MinDeltaSN' - Select sources with (SN_2-SN_1)>MinDeltaSN
+    %                   Default is 1.
+    %            'ColMom2' - Column names for second moments in X,Y,XY.
+    %                   Default is {'X2','Y2','XY'}.
+    %            'DeltaSigma' - Select sources with 2nd moment in this
+    %                   half0range around mode of second moment.
+    %                   Default is 0.5.
+    %            'ColMom1' - Column names for source position in X,Y.
+    %                   Default is {'X','Y'}.
+    % Outout : - A two column matrix of [X,Y] of selected sources.
+    %          - A logical vector of flags (true for selected object).
+    % Author : Eran Ofek (Jan 2022)
+    % Example: XY = imProc.psf.selectPsfStars(AI);
     
     arguments
         Obj(1,1)
@@ -11,10 +33,6 @@ function [PsfXY, Flag, Cube] = selectPsfStars(Obj, Args)
         Args.ColMom2           = {'X2','Y2','XY'};
         Args.DeltaSigma        = 0.5;
         Args.ColMom1           = {'X','Y'};
-        
-        Args.StampHalfSize     = 7;
-        Args.mexCutout logical    = true;
-        Args.Circle logical       = false;
     end
     
     if isa(Obj, 'AstroImage')
@@ -33,8 +51,9 @@ function [PsfXY, Flag, Cube] = selectPsfStars(Obj, Args)
     XY       = getCol(Cat, Args.ColMom1);
     
     FlagSN   = SN(:,2)>Args.RangeSN(1) & SN(:,2)<Args.RangeSN(2) & (SN(:,2)-SN(:,1))>Args.MinDeltaSN;
-    Sigma    = sqrt(Mom2(:,1)+Mom2(:,2));
-    MedSigma = median(Sigma);
+    Sigma    = sqrt(abs(Mom2(:,1))+abs(Mom2(:,2)));
+    %MedSigma = median(Sigma, 1, 'omitnan');
+    MedSigma = imUtil.background.mode(Sigma(FlagSN));
     FlagSig  = Sigma>(MedSigma - Args.DeltaSigma) & Sigma<(MedSigma + Args.DeltaSigma);
     
     Flag     = FlagSN & FlagSig;
@@ -42,11 +61,6 @@ function [PsfXY, Flag, Cube] = selectPsfStars(Obj, Args)
     % get PSF sources
     PsfXY    = XY(Flag,:);
     
-    if nargout>2
-        [Cube, RoundX, RoundY, X, Y] = image2cutouts(Obj(Obj.Image, PsfXY(:,1), PsfXY(:,2),...
-                                                                    Args.StampHalfSize,...
-                                                                    'mexCutout',Args.mexCutout,...
-                                                                    'Circle',Args.Circle);
-    end
+    
     
 end
