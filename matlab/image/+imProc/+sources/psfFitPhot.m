bb41a2eb2be25a3c87e85f32e3a69bf36f0e4a34
EranOfek <ofek.eran@gmail.com>
2022-01-11 13:43:26 +0200

    psfFitPhot

--- matlab/image/+imProc/+sources/psfFitPhot.m
+++ matlab/image/+imProc/+sources/psfFitPhot.m
@@ -1,5 +1,10 @@
 function Result = psfFitPhot(Obj, Args)
     %
+    % Example: AI=AstroImage('PTF_201411204943_i_p_scie_t115144_u023050379_f02_p100037_c02.fits');
+    %          AI=imProc.background.background(AI);
+    %          AI=imProc.sources.findMeasureSources(AI);
+    %          AI=imProc.psf.constructPSF(AI);
+    %          R = imProc.sources.psfFitPhot(AI);
     
     
     arguments
@@ -8,12 +13,13 @@ function Result = psfFitPhot(Obj, Args)
         Args.PSF                     = [];  % PSF, or function_handle
         Args.PSFArgs cell            = {};
         
-        Args.ColX                    = Obj(1).DefNamesX;        
-        Args.ColY                    = Obj(1).DefNamesY;       
+        Args.ColX                    = AstroCatalog.DefNamesX;        
+        Args.ColY                    = AstroCatalog.DefNamesY;       
         Args.ColBack                 = 'BACK_IM';
-        Args.ColVar                  = 'STD_IM';
+        Args.ColVar                  = 'VAR_IM';  % prefered over ColStd
         Args.ColStd                  = [];
         Args.FitRadius               = 3;
+        Args.HalfSize                = 8;
         Args.backgroundCubeArgs cell = {};
         
         Args.CreateNewObj logical    = false;
@@ -51,7 +57,7 @@ function Result = psfFitPhot(Obj, Args)
                 [Src] = imUtil.sources.findSources(Obj(Iobj).Image,...
                                                         'BackIm',Obj(Iobj).Back,...
                                                         'VarIm',Obj(Iobj).Var,...
-                                                        'Psf',PSF,...
+                                                        'Psf',PSF);
                                                         
                 XY = [Src.XPEAK, Src.YPEAK];
                 Back = Src.BACK_IM;
@@ -60,25 +66,32 @@ function Result = psfFitPhot(Obj, Args)
                 % get also the Back and STD
                 Back = getCol(Obj(Iobj).CatData, Args.ColBack);
                 if isempty(Args.ColVar)
-                    Std = sqrt(getCol(Obj(Iobj).CatData, Args.ColVar));
-                else
+                    if isempty(Args.ColStd)
+                        error('Either ColStd or ColVar must be provided');
+                    end
                     Std = getCol(Obj(Iobj).CatData, Args.ColStd);
+                else
+                    Std = sqrt(getCol(Obj(Iobj).CatData, Args.ColVar));
                 end
             end 
         else
             % XY provided by user
             XY = Args.XY;
             % get Back/Var at these positions
-            
+            Ind  = imUtil.image.sub2ind_fast(size(Obj(Iobj).Image), XY(:,1), XY(:,2));
+            Back = Obj(Iobj).Back(Ind);
+            Var  = Obj(Iobj).Var(Ind);
         end
         
         % subtract Background
         ImageSubBack = Obj(Iobj).Image - Obj(Iobj).Back;
         
         % get Cube of stamps around sources
-        [Cube, RoundX, RoundY, X, Y] = imUtil.cut.image2cutouts(Image, X, Y, MaxRadius, 'mexCutout',Args.mexCutout, 'Circle',Args.Circle);
+        [Cube, RoundX, RoundY, X, Y] = imUtil.cut.image2cutouts(ImageSubBack, XY(:,1), XY(:,2), Args.HalfSize, 'mexCutout',Args.mexCutout, 'Circle',Args.Circle);
         
         % PSF fitting
+        
+        % something wrong with CubePsfSub...
         [Result, CubePsfSub] = imUtil.sources.psfPhotCube(Cube, 'PSF',PSF,...
                                                                 'Std',Std,...
                                                                 'Back',Back,...
@@ -86,6 +99,9 @@ function Result = psfFitPhot(Obj, Args)
                                                                 'backgroundCubeArgs',Args.backgroundCubeArgs,...
                                                                 Args.psfPhotCubeArgs{:});
                                                                 
+        % source measured position is at:
+        % RoundX + Result.DX
+        
         % second iteration
         Image = imUtil.cut.cutouts2image(Cube, Image, X, Y)
         
