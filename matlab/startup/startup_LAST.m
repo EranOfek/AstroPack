af36aeed25dec89d21bdabfd4bc74fece1cc0403
EranOfek <ofek.eran@gmail.com>
2022-01-20 23:50:46 +0200

    startup_LAST

--- matlab/startup/startup_LAST.m
+++ matlab/startup/startup_LAST.m
@@ -1,17 +1,16 @@
 function startup_LAST
+    % startup for LAST directories
 
+    Home   = char(java.lang.System.getProperty('user.home'));
+    Base   = sprintf('%s%s%s%s%s',Home, filesep,'natlab',filesep, 'LAST');
 
-Home   = char(java.lang.System.getProperty('user.home'));
-Base   = sprintf('%s%s%s',Home, filesep, 'LAST');
+    List = {'LAST_OCS',...
+            'LAST_configuration'};
 
+    Nlist = numel(List);
+    for Ilist=1:1:Nlist
+        sprintf('%s%s%s',Base, filesep, List{Ilist})
+        %addpath(sprintf('%s%s%s',Base, filesep, List{Ilist}));
+    end
 
-List = {'LAST_OCS',...
-	'LAST_configuration'};
-
-Nlist = numel(List);
-for Ilist=1:1:Nlist
-	    addpath(sprintf('%s%s%s',Base, filesep, List{Ilist}));
 end
-
-
-
