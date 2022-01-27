0812b37c8161cc610079e2ccc30b4ae95eb0ad87
EranOfek <ofek.eran@gmail.com>
2022-01-27 12:19:44 +0200

    startup

--- matlab/startup/startup_LAST.m
+++ matlab/startup/startup_LAST.m
@@ -1,19 +1,35 @@
-function startup_LAST
+function startup_LAST(AddLast, AddAstroPath)
     % startup for LAST directories
 
-    Home   = char(java.lang.System.getProperty('user.home'));
-    Base   = sprintf('%s%s%s%s%s',Home, filesep,'matlab',filesep, 'LAST');
+    arguments
+        AddLast logical       = true;
+        AddAstroPath logical  = true;
+    end
+    
+    Home     = char(java.lang.System.getProperty('user.home'));
+    Base     = sprintf('%s%s%s%s%s',Home, filesep,'matlab',filesep, 'LAST');
+    Computer = char(java.net.InetAddress.getLocalHost.getHostName);
+    
+    if AddLast
+        AllFiles = dir(Base);
+        IsDir = [AllFiles.isdir];
+        List = {AllFiles(IsDir).name};
 
-    AllFiles = dir(Base);
-    IsDir = [AllFiles.isdir];
-    List = {AllFiles(IsDir).name};
-
-    Nlist = numel(List);
-    for Ilist=1:1:Nlist
-        if ~(strcmp(List(1),'+') || strcmp(List(1),'@'))
-            sprintf('%s%s%s',Base, filesep, List{Ilist})
-            %addpath(sprintf('%s%s%s',Base, filesep, List{Ilist}));
+        Nlist = numel(List);
+        for Ilist=1:1:Nlist
+            if ~(strcmp(List(1),'+') || strcmp(List(1),'@'))
+                sprintf('%s%s%s',Base, filesep, List{Ilist})
+                %addpath(sprintf('%s%s%s',Base, filesep, List{Ilist}));
+            end
         end
+    end
+    
+    if AddAstroPath
+        PWD = pwd;
+        AstroPathStartup = [Home, filesep, 'matlab', filesep, 'AstroPack', 'matlab', filesep, 'startup'];
+        cd(AstroPathStartup);
+        AstroPack_CatsHTMPath = [filesep, Computer, filesep, data', filesep, catsHTM];
+        startup('AstroPack_CatsHTMPath',AstroPack_CatsHTMPath);
     end
 
 end
