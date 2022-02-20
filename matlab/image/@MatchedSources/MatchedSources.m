c75a530b4b938567afd642c2dc0b374684895be4
EranOfek <ofek.eran@gmail.com>
2022-02-20 16:21:17 +0200

    MatchedSources/findOrphans

--- matlab/image/@MatchedSources/MatchedSources.m
+++ matlab/image/@MatchedSources/MatchedSources.m
@@ -1431,33 +1431,74 @@ classdef MatchedSources < Component
             %            'MaxNepochs' - Max number of epochs in which an
             %                   orphan sources is detected in.
             %                   Default is 1.
-            %            'FieldName' - Field name in which to look for NaNs
+            %            'SelectFieldName' - Field name in which to look for NaNs
             %                   in order to search for orphan sources.
             %                   Default is MatchedSources.DefNamesX
+            %            'OutputFields' - A cell array of fields in
+            %                   MatchedSources.Data
+            %                   For each field will return the value for
+            %                   the orphan sources at the detected epochs.
             % Output : - A structure array (element per MatchedSources
             %            element) with fields:
             %            .Flag - a row vector of logical indicating if a
             %                   source in the [Epoch, Src] matrix is an
             %                   orphan.
+            %            .NotNanMatrix - A matrix of logicals indicating
+            %                   the not-nan elemsnts in the matrix.
+            %            .Src - Astructure array. Element per orphan, with
+            %                   the following fields:
+            %               .SrcInd - Source index.
+            %               .EpochInd - Vector of epoch indices in which
+            %                       the source was detected.
+            %               .JD - Vector of JD.
+            %               .(all the fields in OutputFields) - Vector of
+            %                       requested properties.
             % Author : Eran Ofek (Feb 2022)
             % Example: MS = MatchedSources;
             %          MS.addMatrix(rand(100,200).*10,'MAG');
-            %          F = MS.findOrphans('FieldName','MAG')
+            %          MS.Data.MAG(:,[1 10]) = NaN; MS.Data.MAG(1,1) = 10; MS.Data.MAG(10:11,10) = 10;
+            %          MS.addMatrix(rand(100,200).*10,'RA');
+            %          MS.addMatrix(rand(100,200).*10,'Dec');
+            %          F = MS.findOrphans('SelectFieldName','MAG', 'MaxNepochs',2)
             
             arguments
                 Obj
-                Args.MaxNepochs  = 1;
-                Args.FieldName   = MatchedSources.DefNamesX;
+                Args.MaxNepochs        = 1;
+                Args.SelectFieldName   = MatchedSources.DefNamesX;
+                
+                Args.OutputFields      = {'RA','Dec'};
             end
             
+            Nout = numel(Args.OutputFields);
             Nobj = numel(Obj);
             for Iobj=1:1:Nobj
-                Matrix = getMatrix(Obj, Args.FieldName);
+                Matrix = getMatrix(Obj(Iobj), Args.SelectFieldName);
                 % number of epochs in which each source was detected
-                NepochPerSrc = sum(~isnan(Matrix), 1);
+                NotNanMatrix = ~isnan(Matrix);
+                NepochPerSrc = sum(NotNanMatrix, 1);
 
                 % flag orphan sources
-                FlagOrphan(Iobj).Flag = NepochPerSrc <= Args.MaxNepochs;
+                FlagOrphan(Iobj).Flag         = NepochPerSrc <= Args.MaxNepochs;
+                FlagOrphan(Iobj).NotNanMatrix = NotNanMatrix;
+                
+                if ~isempty(Args.OutputFields)
+                    % retrieve data for orphan sources
+                    [OutCell{1:1:Nout}] = getMatrix(Obj(Iobj), Args.OutputFields);
+                end
+                
+                % look for indices of epochs in which the orphan sources
+                % were detected
+                IndOrphans  = find(FlagOrphan(Iobj).Flag);
+                NIndOprhans = numel(IndOrphans);
+                for Iind=1:1:NIndOprhans
+                    FlagOrphan(Iobj).Src(Iind).SrcInd   = IndOrphans(Iind);
+                    FlagOrphan(Iobj).Src(Iind).EpochInd = find(NotNanMatrix(:,IndOrphans(Iind)));
+                    FlagOrphan(Iobj).Src(Iind).JD       = Obj(Iobj).JD(NotNanMatrix(:,IndOrphans(Iind)));
+                
+                    for Iout=1:1:Nout
+                        FlagOrphan(Iobj).Src(Iind).(Args.OutputFields{Iout}) = OutCell{Iout}(NotNanMatrix(:,IndOrphans(Iind)), IndOrphans(Iind) );
+                    end
+                end
             end
         end
     end
