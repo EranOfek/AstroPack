eb85aa5a3a4f45969b3ee8d735a72e0d3230cb16
EranOfek <ofek.eran@gmail.com>
2022-04-04 16:50:01 +0300

    continuousLong.m

---
+++ matlab/astro/+celestial/+search/polygonIntersection.m
@@ -0,0 +1,17 @@
+function polygonIntersection(PolygonsLon, PolygonsLat, SearchPolygonLon, SearchPolygonLat, Args)
+    %
+    
+    arguments
+        PolygonsLon
+        PolygonsLat
+        SearchPolygonLon
+        SearchPolygonLat
+        Args
+    end
+    
+    PolygonsLon
+    
+    
+    
+    
+end
