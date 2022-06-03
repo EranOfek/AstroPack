4ff73cc11aea19fdd7d39043a9dec1faafd89813
EranOfek <ofek.eran@gmail.com>
2022-06-03 11:42:05 +0300

    ds9.unitTest copied from old ver

--- matlab/image/@ds9/unitTest.m
+++ matlab/image/@ds9/unitTest.m
@@ -1,6 +1,5 @@
 function Result = unitTest()
-	% unitTest for DS9
-    
+	% unitTest for ds9
 	io.msgStyle(LogLevel.Test, '@start', 'ds9 test started');
      
 	if ~isunix && ~ismac
@@ -9,84 +8,22 @@ function Result = unitTest()
         return;
     end
     
-    % create a DS9 object
-    D = DS9;
-    D.exit;
-    D = DS9(rand(100,100),1);
-    % open a window
-    D.open
-    % open another window
-    D.open(true);
-    D.exit
-    % open again
-    D.open(true);
-    D.isOpen
-    D.isWindowExist
-    D.isOpen
-    [Result,AllMethods] = DS9.getAllWindows;
-    D.mode([]) 
-    
-    % switch ds9 window
-    D.Frame=1;
-    D.Frame=2;
-    pause(0.5)
-    if D.nframe~=2
-        error('Number of frames suppose to be 2');
-    end
-    R = D.frame('delete'); % delete current frame
-    if D.Frame~=1
-        error('Frame number was supposed to be 1');
+    % Check that we have connection to DS9 service
+    if system('xpaget ds9')
+    	io.msgStyle(LogLevel.Warning, 'red', 'You may need to run ds9 manually at terminal before running unitTest');
+    else
+        %f = fullfile(Configuration.getSingleton.Data.System.Folders.TestImages, 'asu.fit');
+        % o.s. this already has a bug, how do I get a reference to the data
+        % folder path.
+        %f = '/home/omrisee/matlab/data/LAST/TestImages/LAST.2.1.2_20200820.163931.354_clear_0_twflat.fits' ;
+        f = fullfile(tools.os.getTestDataDir, 'asu.fit');
+        ds9(f)
+        a = ds9.read2AstroImage();
+        assert(isa(a, 'AstroImage'))
     end
     
-    R = D.frame('center') % center current frame
-    R = D.frame('clear') % clear current frame
-    R = D.frame('new') % create new frame
-    R = D.frame('new rgb') % create new rgb frame
-    R = D.frame('reset') % reset current frame
-    R = D.frame('refresh') % refresh current frame
-    R = D.frame('hide') % hide current frame
-    R = D.frame('first') % goto first frame
-    R = D.frame('prev') % goto previous frame
-    R = D.frame('last') % goto last frame
-    R = D.frame('next') % goto next frame
-    R = D.frame('match wcs') % 
-    R = D.frame('lock wcs') % 
-    
-    D.clearFrame(2);
-    D.clearFrame('all');
-   
-    D.frame('new');
-    D.frame('new');
-    D.frame('new');
-    D.deleteFrame(2)
-    D.deleteFrame('all');
-    
-    % xpasetFrame
-    D = DS9(rand(100,100),1);
-    D.load(rand(200,200),2)
-    D.xpasetFrame(true,'zoom to 2')
-    D.xpasetFrame(true,'zoom to 4')
-    
-    % zoom
-    D.exit
-    D.load(rand(100,100));
-    FN = D.save2fits;
-    delete(FN);
-    
-    D = DS9(rand(100,100),1);
-    D.load(rand(100,100),2);
-    D.zoom(5)
-    D.zoom(-0.5)
-    D.zoom(4,true)
-    D.zoom   % zoom to fit
-    D.zoom('to 5')
-    D.zoom('in')
-    D.zoom('out',2)
-    
-    % scale
-    D = DS9(rand(100,100));
-    [L,ST] = D.scale;
-    D.scale([0 0.5]);
+    % Test plot regions
+    %function plot(varargin)    
     
 	io.msgStyle(LogLevel.Test, '@passed', 'ds9 test passed');
 	Result = true;
