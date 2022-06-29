0ecfb94457c9eeff2312145731f9a72f8f6d466d
EranOfek <ofek.eran@gmail.com>
2022-06-29 23:10:00 +0300

    FITS

--- matlab/image/@FITS/FITS.m
+++ matlab/image/@FITS/FITS.m
@@ -435,6 +435,221 @@ classdef FITS < handle
         
         end
 
+        % old version
+        function [Out, Col] = readTableOld(TableName,varargin)
+            % Read binary or ascii FITS tables.
+            % Package: @FITS
+            % Description: Read binary or ascii FITS tables.
+            % Input  : - List of FITS tables to read. Any input valid to Util.files.create_list.m.
+            %            If multiple files are provided then all the files hould be of
+            %            the same type (e.g., fits binary table).
+            %          * Arbitrary number of pairs of arguments: ...,keyword,value,...
+            %            where keyword are one of the followings:
+            %            'TableType'- FITS table type {'auto'|'bintable'|'table'}.
+            %                         Default is 'auto'.
+            %                         'auto' will attempt to read the table type
+            %                         from the 'XTENSION' header keyword.
+            %            'HDUnum'   - HDU number in which the FITS table header is.
+            %                         Default is 2.
+            %            'ModColName' - If the program failed because some columns
+            %                         have name starting with numbers or invalid signs
+            %                         set this to true (default is false).
+            %                         This will modify the column names.
+            %            'OutTable' - Type of table output:
+            %                         'astcat' - AstCat object.
+            %                         'astcat_t' - AstCat object in which
+            %                                      the data is stored as a table.
+            %            'XTENkey'  - Header keyword from which to read the table type.
+            %                         Default is 'XTENSION'.
+            %            'StartRow' - First row to read from FITS table. Default is [].
+            %                         If empty, then read the entire table.
+            %            'NRows'    - Number of rows to read from FITS table.
+            %                         Default is []. If empty, then read the entire
+            %                         table.
+            %            'OutClass' - A function to use in order to force all the
+            %                         columns to be of the same class (e.g., @single).
+            %                         Default is @double. If empty, then will keep
+            %                         the original class. This option shoyld be used
+            %                         if you want to read the data into a matrix.
+            %            'NullVal'  - If the column is of numeric type will attempt
+            %                         to replace the FITS null value with this value.
+            %                         Default is NaN.
+            %            'BreakRepCol'- {true|false}. If true and FITS table columns
+            %                         are repeating then will change column information
+            %                         according to the matrix column count.
+            %                         Default is true.
+            % Output : - AstCat object containing the FITS table.
+            %          - A structure array of additional columns
+            %            information, like format.
+            % Tested : Matlab R2014a
+            %     By : Eran O. Ofek                    Jan 2015
+            %    URL : http://weizmann.ac.il/home/eofek/matlab/
+            % Example: [Out,Col]=FITS.read_table('asuWDs.fit','ModColName',true);
+            % Reliable: 2
+            %--------------------------------------------------------------------------
+
+            HeaderField     = HEAD.HeaderField;
+            CatField        = AstCat.CatField;
+            ColField        = AstCat.ColField;
+            ColCellField    = AstCat.ColCellField;
+            ColUnitsField   = AstCat.ColUnitsField';
+
+
+            DefV.TableType      = 'auto';    % {'auto'|'bintable'|'table'}
+            DefV.HDUnum         = 2;
+            DefV.ModColName     = false;
+            DefV.OutTable       = 'astcat';  % {'astcat'|'astcat_t'|...}
+            DefV.XTENkey        = 'XTENSION';
+            DefV.StartRow       = [];
+            DefV.NRows          = [];
+            DefV.OutClass       = @double;
+            DefV.NullVal        = NaN;       % [] do nothing
+            DefV.BreakRepCol    = true;   
+
+            InPar = InArg.populate_keyval(DefV,varargin,mfilename);
+
+            % prep list of fits table names
+            [~,ListTableName] = i.files.create_list(TableName,NaN);
+            Nfile = numel(ListTableName);
+            
+            
+            Out = AstCat(Nfile,1);
+            Col = tools.struct.struct_def({'Col','Cell','Units','TypeChar','Repeat','Scale','Zero','Nulval','Tdisp','Data'},Nfile,1);
+            
+            import matlab.io.*
+            for Ifile=1:1:Nfile
+                
+                % get header
+                Head     = FITS.get_head(ListTableName{1},InPar.HDUnum);
+                % populate header
+                Out(Ifile).(HeaderField) = Head.(HeaderField);
+                
+                % identify table type
+                switch lower(InPar.TableType)
+                    case 'auto'
+                        HeadCell = Head.(HeaderField);
+                        Ixten    = find(strcmp(HeadCell(:,1),InPar.XTENkey));
+                        if (isempty(Ixten))
+                            error('Automatic table identification mode was unable to access FITS table type');
+                        end
+                        InPar.TableType = HeadCell{Ixten,2};
+                    otherwise
+                        % do nothing
+                end
+
+                % Table type
+                switch lower(InPar.TableType)
+                    case 'bintable'
+                        Fun_getColParms = @fits.getBColParms;
+                    case 'table'
+                        Fun_getColParms = @fits.getAColParms;
+                    otherwise
+                        error('Unknown TableType option');
+                end
+
+                if (isempty(InPar.StartRow) || isempty(InPar.NRows))
+                    CellRowPar = {};
+                else
+                    CellRowPar = {InPar.StartRow,InPar.NRows};
+                end
+
+                
+                % for each fits table
+                Fptr = fits.openFile(ListTableName{Ifile});
+                fits.movAbsHDU(Fptr,InPar.HDUnum);
+                Ncol = fits.getNumCols(Fptr);
+                %[ttype,tunit,typechar,repeat,scale,zero,nulval,tdisp]= fits.getBColParms(Fptr,2);
+                Col(Ifile).Cell     = cell(1,Ncol);
+                Col(Ifile).Units    = cell(1,Ncol);
+                Col(Ifile).TypeChar = cell(1,Ncol);
+                Col(Ifile).Repeat   = cell(1,Ncol);
+                Col(Ifile).Scale    = cell(1,Ncol);
+                Col(Ifile).Zero     = cell(1,Ncol);
+                Col(Ifile).Nulval   = cell(1,Ncol);
+                Col(Ifile).Tdisp    = cell(1,Ncol);
+                Col(Ifile).Data     = cell(1,Ncol);
+                for Icol=1:1:Ncol
+                    [Col(Ifile).Cell{Icol},Col(Ifile).Units{Icol},Col(Ifile).TypeChar{Icol},...
+                                           Col(Ifile).Repeat{Icol},Col(Ifile).Scale{Icol},Col(Ifile).Zero{Icol},...
+                                           Col(Ifile).Nulval{Icol},Col(Ifile).Tdisp{Icol}]= Fun_getColParms(Fptr,Icol);
+                                       
+                                       
+                    [Col(Ifile).Data{Icol}] = fits.readCol(Fptr,Icol,CellRowPar{:});
+                    if (~isempty(InPar.OutClass))
+                        Col(Ifile).Data{Icol} = InPar.OutClass(Col(Ifile).Data{Icol});
+                    end
+                    if (~isempty(InPar.NullVal) && ~isempty(Col(Ifile).Nulval{Icol}) && isnumeric(Col(Ifile).Data{Icol}))
+                        Col(Ifile).Data{Icol}(Col(Ifile).Data{Icol}==Col(Ifile).Nulval{Icol}) = InPar.NullVal;
+
+                    end
+                    % override ColRepeat using the actual data
+                    Col(Ifile).Repeat{Icol} = size(Col(Ifile).Data{Icol},2);
+                end
+                fits.closeFile(Fptr);
+
+                % deal with repeating columns
+                if (InPar.BreakRepCol)
+                    Nnc  = sum(cell2mat(Col(Ifile).Repeat));
+                    NewCol.Cell = cell(Nnc,1);
+
+                    Icol1 = 1;
+                    for Icol=1:1:Ncol            
+                        IcolN = Icol1 + Col(Ifile).Repeat{Icol} - 1;
+                        %Icol1 = Icol1 + Col.Repeat{Icol}; % at the end of the loop
+                        for Irep=1:1:Col(Ifile).Repeat{Icol}
+                            if (Col(Ifile).Repeat{Icol}>1)
+                                NewCol.Cell{Icol1+Irep-1} = sprintf('%s_%d_',Col(Ifile).Cell{Icol},Irep);
+                            else
+                                NewCol.Cell{Icol1+Irep-1} = Col(Ifile).Cell{Icol};
+                            end
+                        end
+                        [NewCol.Units{Icol1:IcolN}] = deal(Col(Ifile).Units{Icol});
+                        [NewCol.TypcChar{Icol1:IcolN}] = deal(Col(Ifile).TypeChar{Icol});
+                        [NewCol.Repeat{Icol1:IcolN}] = deal(1);
+                        [NewCol.Scale{Icol1:IcolN}] = deal(Col(Ifile).Scale{Icol});
+                        [NewCol.Zero{Icol1:IcolN}] = deal(Col(Ifile).Zero{Icol});
+                        [NewCol.Tdisp{Icol1:IcolN}] = deal(Col(Ifile).Tdisp{Icol});
+                        Icol1 = Icol1 + Col(Ifile).Repeat{Icol}; % at the end of the loop
+
+                    end
+                    Col(Ifile).Cell     = NewCol.Cell;
+                    Col(Ifile).Units    = NewCol.Units;
+                    Col(Ifile).TypeChar = NewCol.TypcChar;
+                    Col(Ifile).Repeat   = NewCol.Repeat;
+                    Col(Ifile).Scale    = NewCol.Scale;
+                    Col(Ifile).Zero     = NewCol.Zero;
+                    Col(Ifile).Tdisp    = NewCol.Tdisp;   
+                end
+                if (InPar.ModColName)
+                    % modify column names to legal variable names
+                    Col(Ifile).Cell = regexprep(Col(Ifile).Cell,{'-','/','(',')','&','@','#','^','%','*','~'},'');
+                    Col(Ifile).Cell = strcat('T',Col(Ifile).Cell);
+                end
+                Col(Ifile).Col      = cell2struct(num2cell(1:1:length(Col(Ifile).Cell)),Col(Ifile).Cell,2);
+
+                % output
+                switch lower(InPar.OutTable)
+                    case 'astcat'
+                        Out(Ifile).(CatField)     = [Col(Ifile).Data{:}];
+                        Out(Ifile).(ColField)     = Col(Ifile).Col;
+                        Out(Ifile).(ColCellField) = Col(Ifile).Cell;
+                        Out(Ifile).(ColUnitsField)= Col(Ifile).Units;
+                        
+                    case 'astcat_t'
+                        Out(Ifile).(CatField)     = table(Col(Ifile).Data{:});
+                        Out(Ifile).(ColField)     = Col(Ifile).Col;
+                        Out(Ifile).(ColCellField) = Col(Ifile).Cell;
+                        Out(Ifile).(ColUnitsField)= Col(Ifile).Units;
+                    
+                    otherwise
+                        error('Unknown OuTable option');
+                end
+
+            end
+        
+        end 
+        
+        
         function Col = getTableCol(Fptr, TableType)
             % Table type
             % Note that these function have different return values and order
@@ -959,7 +1174,7 @@ classdef FITS < handle
             % Description: Read FITS images into SIM object.
             %              Can read N-dimensional images.
             %              Can also read multi extension files.
-            % Input  : - List of images to read. See Util.files.create_list.m for
+            % Input  : - List of images to read. See io.files.create_list.m for
             %            details.
             %          * Arbitrary number of pairs of ...,key,val,...
             %            input arguments. Available keywords are:
