958839275af07f1dd76fe6a27dab4149bb6766e8
EranOfek <ofek.eran@gmail.com>
2022-07-07 13:31:32 +0300

    searchVarMerged

--- matlab/image/+pipeline/+lastAnalysis/searchVarMerged.m
+++ matlab/image/+pipeline/+lastAnalysis/searchVarMerged.m
@@ -15,6 +15,8 @@ function searchVarMerged(Files, Args)
         cd(Args.Dir);
     end
     %%
+    BD=BitDictionary;
+    
     List  = io.files.rdir(Files);
     Nlist = numel(List);   
    
@@ -22,10 +24,21 @@ function searchVarMerged(Files, Args)
         FileName = fullfile(List(Ilist).folder, List(Ilist).name);
         
         T = FITS.readTable1(FileName);
-        if sum(T.PolyDeltaChi2>18)>0
+        [FlagBad] = findBit(BD, T.FLAGS, {'Saturated','NearEdge','Overlap','NaN'});
+        
+        FlagSelected = T.PolyDeltaChi2>16 & ~FlagBad & T.Std_MAG_CONV_2>0.1;
+                
+        %T = T(~FlagBad,:);
+        if any(FlagSelected)
+            
             Ilist
+            
+            
             [SC2, SI] = sort(T.PolyDeltaChi2);
-            [Max, MaxInd] = max(T.PolyDeltaChi2);
+            FlagBad   = FlagBad(SI);
+            
+            %[Max, MaxInd] = max(T.PolyDeltaChi2);
+            MaxInd = SI(end);
             
             [Ilist, Max, T.Mean_SN_3(MaxInd), T.RA(MaxInd), T.Dec(MaxInd)]
         
@@ -34,8 +47,9 @@ function searchVarMerged(Files, Args)
             
             FileMergedMat = strrep(FileName, '_sci_merged_Cat_001.fits', '_sci_merged_MergedMat_001.hdf5');
             MatRA = h5read(FileMergedMat, '/RA');
-            MatMag = h5read(FileMergedMat, '/MAG_APER_2');
+            MatMag = h5read(FileMergedMat, '/MAG_CONV_2');
             nanmean(MatRA)
+            plot(MatMag(:,SI(end-0)),'o')
         end
         
     end
