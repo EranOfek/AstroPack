131d2328a2c0b0092f2bf7b12c553c7470f72647
EranOfek <ofek.eran@gmail.com>
2022-07-27 14:56:58 +0300

    extension of wmedian to Dim and matrices

--- matlab/util/+tools/+math/+stat/wmedian.m
+++ matlab/util/+tools/+math/+stat/wmedian.m
@@ -1,37 +1,58 @@
-function D=wmedian(varargin)
-% Weighted median for a vector.
-% Package: Util.stat
-% Description: Weighted median for a vector.
-%              Calculates the weighted median of a vector
-%              given the error on each value in the vector.
-% Input  : * Either two arguments Data,Error or a two column matrix
-%            of [Data,Error].
-%            The weights are given by 1./Error.^2
-% Output : - Weighted median
-% Reference: https://en.wikipedia.org/wiki/Weighted_median
-% License: GNU general public license version 3
-% Tested : Matlab R2015a
-%     By : Eran O. Ofek                    Jun 2015
-%    URL : http://weizmann.ac.il/home/eofek/matlab/
-% Example: D=tools.math.stat.wmedian(V,E);
-% Reliable: 2
-%--------------------------------------------------------------------------
+function WMed = wmedian(Val, Err, Dim)
+    % Weighted median for a vector.
+    % Package: Util.stat
+    % Description: Weighted median for a vector.
+    %              Calculates the weighted median of a vector
+    %              given the error on each value in the vector.
+    % Input  : - Data (vector or matrix only)
+    %          - Error for each data point in Data.
+    %            The weights are given by 1./Error.^2
+    % Output : - Weighted median. If the inputs are matrices, than this is
+    %            a vector of weighted median (one per column if Dim=1) or one per row
+    %            (for Dim=2).
+    % Reference: https://en.wikipedia.org/wiki/Weighted_median
+    % License: GNU general public license version 3
+    % Tested : Matlab R2015a
+    %     By : Eran O. Ofek                    Jun 2015
+    %    URL : http://weizmann.ac.il/home/eofek/matlab/
+    % Example: D=tools.math.stat.wmedian(V,E);
+    % Reliable: 2
+    %--------------------------------------------------------------------------
 
-if (numel(varargin)==1)
-    Val = varargin{1}(:,1);
-    Err = varargin{1}(:,2);
-else
-    Val = varargin{1};
-    Err = varargin{2};
-end
-
-[SV,SI] = sort(Val);
-CSW     = cumsum(1./Err(SI).^2);  % cumsum of the weights
-if (any(isinf(CSW)))
-    D = NaN;
-else
-    CSWnorm = CSW./CSW(end);
-    D = interp1(CSWnorm,SV,0.5,'linear');
-end
-
+    arguments
+        Val
+        Err
+        Dim   = 1;  % 1, 2 or [1 2]/'all'
+    end
     
+    switch Dim
+        case 1
+            % do nothing
+        case 2
+            Val = Val.';
+            Err = Err.';
+        otherwise
+            % Dim is [1 2] or 'all'
+            Val = Val(:);
+            Err = Err(:);
+    end
+    
+    [SV,SI] = sort(Val, 1);
+    Weights = 1./Err(SI).^2;
+    CSW     = cumsum(Weights, 1);  % cumsum of the weights
+    if any(isinf(CSW),'all')
+        WMed = NaN;
+    else
+        NVar    = size(Val,2);
+        
+        Norm    = sum(Weights, 1);
+        %CSWnorm = CSW./CSW(end);
+        CSWnorm = CSW./Norm;
+        
+        WMed = nan(1,NVar);
+        for Iv=1:1:NVar
+            WMed(Iv) = interp1(CSWnorm(:,Iv),SV(:,Iv),0.5,'linear');
+        end
+    end
+
+end
