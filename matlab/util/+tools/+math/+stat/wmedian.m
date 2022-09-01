bc1c38be701c5ddfa9ffbed4b1b19758f49034d5
EranOfek <ofek.eran@gmail.com>
2022-09-01 15:13:50 +0300

    wmedian bug fix

--- matlab/util/+tools/+math/+stat/wmedian.m
+++ matlab/util/+tools/+math/+stat/wmedian.m
@@ -36,26 +36,30 @@ function WMed = wmedian(Val, Err, Dim)
             Err = Err(:);
     end
     
-    [SV,SI] = sort(Val, 1);
-    Weights = 1./Err(SI).^2;
-    CSW     = cumsum(Weights, 1, 'omitnan');  % cumsum of the weights
-    if any(isinf(CSW),'all')
-        WMed = NaN;
+    if size(Val,1)==0
+        WMed = nan(1, size(Val,2));
     else
-        NVar    = size(Val,2);
-        
-        Norm    = sum(Weights, 1, 'omitnan');
-        %CSWnorm = CSW./CSW(end);
-        CSWnorm = CSW./Norm;
-        
-        WMed = nan(1,NVar);
-        for Iv=1:1:NVar
-            CSWnorm(1,Iv) = min(CSWnorm(1,Iv), 0.5);
-            if isnan(SV(end,Iv))
-                CSWnorm(end,Iv) = 1;
-                SV(end,Iv)      = max(SV(:,end));
+        [SV,SI] = sort(Val, 1);
+        Weights = 1./Err(SI).^2;
+        CSW     = cumsum(Weights, 1, 'omitnan');  % cumsum of the weights
+        if any(isinf(CSW),'all')
+            WMed = NaN;
+        else
+            NVar    = size(Val,2);
+
+            Norm    = sum(Weights, 1, 'omitnan');
+            %CSWnorm = CSW./CSW(end);
+            CSWnorm = CSW./Norm;
+
+            WMed = nan(1,NVar);
+            for Iv=1:1:NVar
+                CSWnorm(1,Iv) = min(CSWnorm(1,Iv), 0.5);
+                if isnan(SV(end,Iv))
+                    CSWnorm(end,Iv) = 1;
+                    SV(end,Iv)      = max(SV(:,end));
+                end
+                WMed(Iv) = interp1(CSWnorm(:,Iv),SV(:,Iv),0.5,'linear');
             end
-            WMed(Iv) = interp1(CSWnorm(:,Iv),SV(:,Iv),0.5,'linear');
         end
     end
 
