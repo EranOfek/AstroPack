410a37929c12bbfefe7cb56d122c0507e8569c46
EranOfek <ofek.eran@gmail.com>
2022-09-12 13:15:11 +0300

    imUtil.asteroids.generateReportMPC

---
+++ matlab/image/+imUtil/+asteroids/generateReportMPC.m
@@ -0,0 +1,112 @@
+function Msg = generateReportMPC(Table, Args)
+    % Generate an MPC asteroids/comet report
+    %
+    % Author : Eran Ofek (Sep 2022)
+    % Example: Table = [2451545.16, 100.1, -12.1, 17.12, NaN, 1; 2451545.16, 100.1, -12.1, 17.12, NaN, 1; 2451545.16, 100.1, -12.1, 17.12, NaN, 2]
+    %          Msg = imUtil.asteroids.generateReportMPC(Table, 'Filter','C');
+    
+    arguments
+        Table                % [JD, RA, Dec, Mag, Filter, AstIndex]
+        Args.Filter           = 'C';
+        Args.JD               = 1;
+        Args.ColRA            = 2;
+        Args.ColDec           = 3;
+        Args.ColMag           = 4;
+        Args.ColFilter        = 5;
+        Args.ColAstIndex      = 6;
+        Args.CooUnits         = 'deg';
+        
+        Args.IsComet logical  = false;
+        Args.IsPrecision logical = true;
+        Args.ObsCode          = 'XXX';
+        Args.Contributer      = 'D. Polishook';
+        Args.EMail            = 'david.polishook@weizmann.ac.il';
+        Args.Observer         = 'LAST';
+        Args.Measurer         = 'E. O. Ofek';
+        Args.ObsLon           = 35.0407331;
+        Args.ObsLat           = 30.0529838;
+        Args.ObsAlt           = 415.4;
+        Args.ObsAltUnits      = 'm';
+        Args.ObsSys           = 'WGS84';
+        Args.ObsName          = 'Large Array Survey Telescope (LAST) Node 01 Mount 01';  %'Weizmann Institute Observatory at Neot Smadar';
+        Args.ObsAddress       = 'Weizmann Institute of Science, 234 Herzl St. Rehovot 76100, Israel';
+        Args.Telescope        = '11-inch f/2.2 Schmidt + 9K x 6K CMOS';
+        Args.RefCatalog       = 'GAIA-DRE3';
+        
+        Args.InstType         = 'B';  % B for CMOS; C for CCD   % https://minorplanetcenter.net/iau/info/OpticalObs.html
+        Args.SkyCond          = '';    % https://minorplanetcenter.net/iau/info/ObsNote.html
+        Args.DesigPrefix      = 'L';   % single letter
+        
+        
+    end
+    
+    switch sign(Args.ObsLon)
+        case 1
+            SignLon = 'E';
+        case -1
+            SignLon = 'W';
+        otherwise
+            error('Error with sign of longitude');
+    end
+    switch sign(Args.ObsLat)
+        case 1
+            SignLat = 'N';
+        case -1
+            SignLat = 'S';
+        otherwise
+            error('Error with sign of latitude');
+    end
+    
+    
+    Position = sprintf('Lon. %12.7f %1s, Lat. %12.7f %1s, Alt. %7.1f %s, %s',abs(Args.ObsLon), SignLon, abs(Args.ObsLat), SignLat, Args.ObsAlt, Args.ObsAltUnits, Args.ObsSys); 
+    
+    Msg = sprintf('COD %s\n',Args.ObsCode);
+    Msg = sprintf('%sCON %s\n',Msg, Args.Contributer);
+    Msg = sprintf('%sCON %s\n',Msg, Args.EMail);
+    Msg = sprintf('%sOBS %s\n',Msg, Args.Observer);
+    Msg = sprintf('%sMEA %s\n',Msg, Args.Measurer);
+    Msg = sprintf('%sCOM %s\n',Msg, Position);
+    Msg = sprintf('%sCOM %s\n',Msg, Args.ObsName);
+    Msg = sprintf('%sCOM %s\n',Msg, Args.ObsAddress);
+    Msg = sprintf('%sTEL %s\n',Msg, Args.Telescope);
+    Msg = sprintf('%sNET %s\n',Msg, Args.RefCatalog);
+    
+    % sort table by asteroid index and JD
+    Table = sortrows(Table, [6 1]);
+    
+    LastAstIndex = Table(1,Args.ColAstIndex);
+    N = size(Table(:,1));
+    DesigCounter = 1;
+    for I=1:1:N
+        if Table(I,Args.ColAstIndex)==LastAstIndex
+            % keep DesigCounter as is
+        else
+            DesigCounter = DesigCounter + 1;
+        end
+        LastAstIndex = Table(I,Args.ColAstIndex);
+        
+        
+        Desig   = sprintf('%s%05d');
+        Date    = celestial.time.jd2date(Table(I, Args.JD), 'f','YMD');
+        DateStr = sprintf('%04d %02d %08.5f',Date);
+        
+        RAdeg   = convert.angular(Args.CooUnits,'deg',Table(I, Args.ColRA));
+        Decdeg  = convert.angular(Args.CooUnits,'deg',Table(I, Args.ColDec));
+        RA      = celestial.coo.convertdms(RAdeg, 'd','SHb');
+        Dec     = celestial.coo.convertdms(Decdeg, 'd','SDb');
+
+        if ~Args.IsPrecision
+            RA(end)  = ' ';
+            Dec(end) = ' ';
+        end
+        
+        Mag = Table(I, Args.ColMag);
+        if isempty(Args.Filter)
+            Filter = Table(I,Args.ColFilter);
+        else
+            Filter = Args.Filter;
+        end
+        Msg = sprintf('%s%5s%6s%3s%1s%s %s%s%9s%5.2f%1s%6s%3s\n',Msg, '', Desig, '', Args.InstType, DateStr, RA, Dec, '',Mag, Filter, '', Args.ObsCode);
+    
+    
+end
