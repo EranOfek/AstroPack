9ae4de3d920196cea26a9e0764ec878c80565e28
EranOfek <ofek.eran@gmail.com>
2022-09-29 20:10:17 +0300

    ransacLinearModel

---
+++ matlab/util/+tools/+math/+fit/ransacLinearModel.m
@@ -0,0 +1,60 @@
+function [FlagGood, BestPar, BestStd] = ransacLinearModel(H, Y, Args)
+    % Fit a general linear model using a simplified RANSAC-like scheme
+    % Input  : - Design matrix of linear model [f(x), g(x), h(x),...]
+    %          - Vector of observables.
+    %          * ...,key,val,...
+    %            'Nsim' - Number of simulations. Default is 100.
+    %            'FracPoints' - Fraction of points to use in each
+    %                   simulation. Default is 0.5.
+    %            'CleanNaN' - Clean NaNs before fitting.
+    %                   Default is false.
+    %            'NsigmaClip' - Sigma clipping in good points selection.
+    %                   Default is [3 3].
+    % Output : - A vector of logicals indicating good data points.
+    %          - Vector of best fit parameters, using all good data points.
+    %          - Vector of best std, using all good data points.
+    % Author : Eran Ofek (Sep 2022)
+    % Example: [FlagGood, BestPar, BestStd] = tools.math.fit.ransacLinearModel(H, Y, Args)
+    
+    arguments
+        H
+        Y
+        Args.Nsim               = 100;
+        Args.FracPoints         = 0.5;
+        Args.CleanNaN logical   = false;
+        Args.NsigmaClip         = [3 3];
+    end
+    
+    if Args.ClearNaN
+        Flag = ~isnan(Y) & all(~isnan(H),2);
+        H    = H(Flag,:);
+        Y    = Y(Flag);
+    end
+    
+    Npt = numel(Y);
+    NptSim = ceil(Npt.*Args.FracPoints);
+    
+    PrevStd = Inf;
+    for Isim=1:1:Args.Nsim
+        IndRand = randi([1 Npt], NptSim, 1);
+        Par     = H(IndRand,:)\Y(IndRand);
+        Resid   = Y(IndRand) - H(IndRand,:)*Par;
+        Std     = std(Resid);
+        if Std<PrevStd
+            PrevStd     = Std;
+            BestIndRand = IndRand;
+            BestPar     = Par;
+            BestStd     = Std;
+        end
+    end
+    
+    
+    % remove outliers
+    Resid   = Y - H*BestPar;
+    FlagGood = Resid<(BestStd.*NsigmaClip(2)) & Resid>(BestStd.*NsigmaClip(1));
+    
+    BestPar = H(FlagGood,:)\Y(FlagGood);
+    Resid   = Y - H(FlagGood,:)\Y(FlagGood);
+    BestStd = std(Resid);
+    
+end
