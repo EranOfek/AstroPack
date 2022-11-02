3fed1e4e41628525d4ef5634d21031d7a94fff25
EranOfek <ofek.eran@gmail.com>
2022-11-02 17:22:49 +0200

    nothing

---
+++ matlab/astro/+celestial/+SolarSys/antiSunDirection.m
@@ -0,0 +1,20 @@
+function antiSunDirection(JD, RA, Dec, R, Delta)
+    %
+    
+    % Earth Ecliptic position at JD relative to the Sun
+    
+    % Object Ecliptic position relative to Earth
+    
+    % Object Ecliptic position relative to Sun
+    
+    
+    % Anti-Sun point
+    
+    % PA to anti-Sun point
+    
+    
+    
+   
+    
+    
+end
