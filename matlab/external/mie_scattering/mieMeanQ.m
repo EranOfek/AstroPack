8fd28c9d44b307e19bd8a555d39f5395506d5618
EranOfek <ofek.eran@gmail.com>
2022-11-24 21:10:25 +0200

    mie

--- matlab/external/mie_scattering/mieMeanQ.m
+++ matlab/external/mie_scattering/mieMeanQ.m
@@ -29,8 +29,12 @@ function Result = mieMeanQ(R, Fun_dNdR, Par_dNdR, Theta, N, Lambda, Rho)
     Result.S_sca            = zeros(Nr,1);
     Result.S_abs            = zeros(Nr,1);
     Result.S_ext            = zeros(Nr,1);
-    Result.S_Stheta         = zeros(Nr,1);
-    Result.Q_Stheta         = zeros(Nr,1);
+    Result.S_scaT           = zeros(Nr,1);
+    Result.Q_sca            = zeros(Nr,1);
+    Result.Q_abs            = zeros(Nr,1);
+    Result.Q_ext            = zeros(Nr,1);
+    Result.Q_scaT           = zeros(Nr,1);
+    Result.MR2Qabs          = zeros(Nr,1);
     
     
     GeomSigma = pi.*R.^2;
@@ -52,17 +56,23 @@ function Result = mieMeanQ(R, Fun_dNdR, Par_dNdR, Theta, N, Lambda, Rho)
         Q_IthetaT = interp1(Ang(:),Itheta(:), Theta);   % [1/sr]
         S_IthetaT = Q_IthetaT.*pi.*R(Ir).^2;   % [area/sr]
 
-
+        Result.MR2Qabs(Ir) = dNdR(Ir) .* S_IthetaT;  % pi r^2 Q r^-alpha - replacing: m Qabs pi r^2
+        Result.S_sca(Ir)   = dNdR(Ir) .* C.sca;
+        Result.S_abs(Ir)   = dNdR(Ir) .* C.abs;
+        Result.S_ext(Ir)   = dNdR(Ir) .* C.ext;
+        Result.S_scaT(Ir)  = dNdR(Ir) .* S_IthetaT;
+        Result.Q_sca(Ir)   = dNdR(Ir) .* C.sca./GeomSigma(Ir);
+        Result.Q_abs(Ir)   = dNdR(Ir) .* C.abs./GeomSigma(Ir);
+        Result.Q_ext(Ir)   = dNdR(Ir) .* C.ext./GeomSigma(Ir);
+        Result.Q_scaT(Ir)  = dNdR(Ir) .* Q_IthetaT;
+        
+        
+        
         dR(Ir)              = R(Ir+1)-R(Ir);
         dN(Ir)              = dNdR(Ir).*dR(Ir);
         
-        MassInBin(Ir) = MassPerParticle(Ir).*dN(Ir);
+        MassInBin(Ir) = MassPerParticle(Ir).*dNdR(Ir);
 
-        Result.S_sca(Ir)     = C.sca.*dN(Ir);
-        Result.S_abs(Ir)     = C.abs.*dN(Ir);
-        Result.S_ext(Ir)     = C.ext.*dN(Ir);
-        Result.S_Stheta(Ir)  = S_IthetaT.*dN(Ir);
-        Result.Q_Stheta(Ir)  = Q_IthetaT.*dN(Ir);
     end
     
     Result.R       = R;
@@ -75,6 +85,10 @@ function Result = mieMeanQ(R, Fun_dNdR, Par_dNdR, Theta, N, Lambda, Rho)
     Result.IntS_aca    = trapz(R, Result.S_sca);
     Result.IntS_abs    = trapz(R, Result.S_abs);
     Result.IntS_ext    = trapz(R, Result.S_ext);
-    Result.IntS_theta  = trapz(R, Result.S_Stheta);
-    
+    Result.IntS_scaT   = trapz(R, Result.S_scaT);
+    Result.IntQ_aca    = trapz(R, Result.Q_sca);
+    Result.IntQ_abs    = trapz(R, Result.Q_abs);
+    Result.IntQ_ext    = trapz(R, Result.Q_ext);
+    Result.IntQ_scaT   = trapz(R, Result.Q_scaT);
+    Result.IntMR2Qabs  = trapz(R, Result.MR2Qabs);
 end
