9151be9fabcfb7e2784abde69e21d0f416efc50a
EranOfek <ofek.eran@gmail.com>
2022-11-26 11:18:14 +0200

    mie

--- matlab/external/mie_scattering/mieMeanQ.m
+++ matlab/external/mie_scattering/mieMeanQ.m
@@ -1,5 +1,15 @@
 function Result = mieMeanQ(R, Fun_dNdR, Par_dNdR, Theta, N, Lambda, Rho)
     % Calculate mean mie scattering properties for paarticle size distribution
+    % Input  : - Vector of particle radius at which dN/dR are given.
+    %          - Vector of dN/dR, or function Fun(R, args)
+    %          - Cell array of argumnets for Fun. Default is {}.
+    %          - Theta [deg] for sigma(Theta). Default is 58.1
+    %          - Refractive index, Default is 1.7 + 0.3.*1i
+    %          - Wavelength, or [wave, weight] of filter.
+    %            Default is 5000.
+    %          - Particle density. Default is 1.
+    % Output : - Structure of integration results.
+    % Author : Eran Ofek (Nov 2022)
     % Example: R = logspace(log10(10),log10(1000),20);
     %          dNdR = R.^-4;
     %          Result = mieMeanQ(R, dNdR);
@@ -10,7 +20,7 @@ function Result = mieMeanQ(R, Fun_dNdR, Par_dNdR, Theta, N, Lambda, Rho)
         Par_dNdR  = {5, 1};  % Alpha, A
         Theta     = 58.1;
         N         = 1.7 + 0.3.*1i;
-        Lambda    = 5000;
+        Lambda    = 5000;  % or filter
         Rho       = 1;
     end
     
@@ -43,22 +53,44 @@ function Result = mieMeanQ(R, Fun_dNdR, Par_dNdR, Theta, N, Lambda, Rho)
     GeomSigma = pi.*R.^2;
     MassPerParticle = 4./3.*pi.*Rho.*R.^3;
     
+    if numel(Lambda)==1
+        Lambda = [Lambda, 1];
+    end
+    Nlambda  = size(Lambda, 1);
+    SumWeight = sum(Lambda(:,2));
+    
     for Ir=1:1:Nr-1
-        [S, C, Ang] = calcmie(R(Ir), N, 1, Lambda,180);
+        V_Q_IthetaT = zeros(Nlambda, 1);
+        V_S_IthetaT = zeros(Nlambda, 1);
+        C_sca       = zeros(Nlambda, 1);
+        C_abs       = zeros(Nlambda, 1);
+        C_ext       = zeros(Nlambda, 1);
+        
+        for Il=1:1:Nlambda
+            [S, C, Ang] = calcmie(R(Ir), N, 1, Lambda(Il,1),180);
 
-        S1=squeeze(abs(S(1,1,:))).^2;
-        S2=squeeze(abs(S(2,2,:))).^2;
-        a      = 2.*pi.*R(Ir)./Lambda; % size parameter (alpha)
+            S1=squeeze(abs(S(1,1,:))).^2;
+            S2=squeeze(abs(S(2,2,:))).^2;
+            a      = 2.*pi.*R(Ir)./Lambda(Il,1); % size parameter (alpha)
 
         
-        % Note that there is an error in Eq. 8 in REF 1 - should be ^-1 instead of
-        % ^-2
-        %Itheta = (2.*k.^2).^-1 .*(S1 + S2);
-        Itheta = (2.*pi.*a.^2).^-1 .*(S1 + S2);
-        % to calculate the angular Mie cross section
-        Q_IthetaT = interp1(Ang(:),Itheta(:), Theta);   % [1/sr]
-        S_IthetaT = Q_IthetaT.*pi.*R(Ir).^2;   % [area/sr]
-
+            % Note that there is an error in Eq. 8 in REF 1 - should be ^-1 instead of
+            % ^-2
+            %Itheta = (2.*k.^2).^-1 .*(S1 + S2);
+            Itheta = (2.*pi.*a.^2).^-1 .*(S1 + S2);
+            % to calculate the angular Mie cross section
+            V_Q_IthetaT(Il) = interp1(Ang(:),Itheta(:), Theta);   % [1/sr]
+            V_S_IthetaT(Il) = V_Q_IthetaT(Il).*pi.*R(Ir).^2;   % [area/sr]
+            C_sca(Il) = C.sca;
+            C_abs(Il) = C.abs;
+            C_ext(Il) = C.ext;
+        end
+        Q_IthetaT = sum(V_Q_IthetaT.*Lambda(:,2))./SumWeight;
+        S_IthetaT = sum(V_S_IthetaT.*Lambda(:,2))./SumWeight;
+        C.sca     = sum(C_sca.*Lambda(:,2))./SumWeight;
+        C.abs     = sum(C_abs.*Lambda(:,2))./SumWeight;
+        C.ext     = sum(C_ext.*Lambda(:,2))./SumWeight;
+        
         dR(Ir)              = R(Ir+1)-R(Ir);
         dN(Ir)              = dNdR(Ir).*dR(Ir);
         
