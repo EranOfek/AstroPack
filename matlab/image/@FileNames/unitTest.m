38a1a03f39f1acde1f7880675c4f2ca8ff2abb36
EranOfek <ofek.eran@gmail.com>
2022-12-06 21:31:40 +0200

    FileNames class

---
+++ matlab/image/@FileNames/unitTest.m
@@ -0,0 +1,32 @@
+function Result = unitTest()
+    % FileNames.unitTest        
+    io.msgStyle(LogLevel.Test, '@start', 'FileNames test started\n');
+
+    
+    F = FileNames;
+    F.ProjName = 'LAST';
+    F.Time     = 2451545;
+    
+    Ans = F.validate;
+    if ~Ans
+        Result = false;
+    end
+    
+    F.jd2str;
+    
+    F = FileNames;
+    F.ProjName = 'LAST';
+    F.Time     = 2451545+(1:1:1000)';
+    
+    F.getProp('CropID');
+    F.getProp('Time',1);
+    
+    F.genFile(1,true)
+    tic;F.genFile;toc
+    
+    
+    % Done
+    io.msgStyle(LogLevel.Test, '@passed', 'FileNames test passed')
+    %Result = true;
+end
+
