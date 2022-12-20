34c29a02da78247e2ab654d9524bda8edc430bfd
EranOfek <ofek.eran@gmail.com>
2022-12-20 12:12:09 +0200

    surfaceAreaFromMag 4pi removed

--- matlab/astro/+celestial/+SolarSys/surfaceAreaFromMag.m
+++ matlab/astro/+celestial/+SolarSys/surfaceAreaFromMag.m
@@ -40,12 +40,14 @@ function [Area,AreaErr] = surfaceAreaFromMag(Mag, MagErr, Args)
         Args.Mag_Sun = AS.synphot(Args.Family,Args.Band,'IsOutMat',true,'MagSys',Args.MagSys);
     end
     
-    Area = 10.^(0.4.*(Args.Mag_Sun - Mag) + log10(4.*pi) + 2.*log10(Args.Delta) + 2.*log10(Args.r) - log10(Args.P)) .*constant.au.^2;
-    %Area = 10.^(0.4.*(Args.Mag_Sun - Mag) + 2.*log10(Args.Delta) + 2.*log10(Args.r) - log10(Args.P)) .*constant.au.^2;
+    %Area = 10.^(0.4.*(Args.Mag_Sun - Mag) + log10(4.*pi) + 2.*log10(Args.Delta) + 2.*log10(Args.r) - log10(Args.P)) .*constant.au.^2;
+    Area = 10.^(0.4.*(Args.Mag_Sun - Mag) + 2.*log10(Args.Delta) + 2.*log10(Args.r) - log10(Args.P)) .*constant.au.^2;
 
     if ~isempty(MagErr)
         %vectorize(simplify(tools.math.symbolic.symerror(10.^(0.4.*(MagSun - Mag) + log10(4.*pi) + 2.*log10(Delta) + 2.*log10(r) - log10(P)),Mag)))
-        AreaErr = (2.*log(10).*((10.^((4.*Args.Mag_Sun)./5 - (4.*Mag)./5 + 2475200567005943./1125899906842624).*MagErr.^2.*Args.Delta.^4.*Args.r.^4)./Args.P.^2).^(1./2))./5 .*constant.au.^2;
+        %AreaErr = (2.*log(10).*((10.^((4.*Args.Mag_Sun)./5 - (4.*Mag)./5 + 2475200567005943./1125899906842624).*MagErr.^2.*Args.Delta.^4.*Args.r.^4)./Args.P.^2).^(1./2))./5 .*constant.au.^2;
+        %vectorize(simplify(tools.math.symbolic.symerror(10.^(0.4.*(MagSun - Mag) + 2.*log10(Delta) + 2.*log10(r) - log10(P)),Mag)))
+        AreaErr = (2.*log(10).*((10.^((4.*Args.Mag_Sun)./5 - (4.*Mag)./5).*MagErr.^2.*Args.Delta.^4.*Args.r.^4)./Args.P.^2).^(1./2))./5 .* constant.au.^2;
     else
         AreaErr = [];
     end
