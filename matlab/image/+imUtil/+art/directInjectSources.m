467eec85c6f4ab7435c0baf931cac07654eff206
A.M. Krassilchtchikov <agioffe@gmail.com>
2023-02-23 17:07:17 +0200

    bug fixes

--- matlab/image/+imUtil/+art/directInjectSources.m
+++ matlab/image/+imUtil/+art/directInjectSources.m
@@ -1,18 +1,17 @@
 function Image = directInjectSources (Image0, Cat, Scaling, PSF)
-
-% Inject sources to catalog postions with PSFs scaled by the Scaling factor 
-% Package: imUtil.art
-% Description: Inject sources to catalog postions with PSFs scaled by the Scaling factor 
-%          - Image0: a 2D array containing the initial image 
-%          - Cat: an 3-column table: X, Y, full band flux normalization
-%          - Scaling: a scaling factor, typically > 1
-%          - PSF: a 2+1 D array of source PSFs
-%          NB: the PSF stamp for all the sources is the same
-% Output : - Image: a 2D array containing the resulting image
-%            
-% Tested : Matlab R2020b
-%     By : A. Krassilchtchikov et al.    Feb 2023
-% Example: Image1 = directInjectSources (Image0,Cat,Scaling,PSF)
+    % Inject sources to catalog postions with PSFs scaled by the Scaling factor 
+    % Package: imUtil.art
+    % Description: Inject sources to catalog postions with PSFs scaled by the Scaling factor 
+    %          - Image0: a 2D array containing the initial image 
+    %          - Cat: an 3-column table: X, Y, full band flux normalization
+    %          - Scaling: a scaling factor, typically > 1
+    %          - PSF: a 2+1 D array of source PSFs
+    %          NB: the PSF stamp for all the sources is the same
+    % Output : - Image: a 2D array containing the resulting image
+    %            
+    % Tested : Matlab R2020b
+    %     By : A. Krassilchtchikov et al.    Feb 2023
+    % Example: Image1 = directInjectSources (Image0,Cat,Scaling,PSF)
 
     % image summation methods: 
     
@@ -49,42 +48,44 @@ function Image = directInjectSources (Image0, Cat, Scaling, PSF)
         SzX     = Xright-Xleft;
         SzY     = Yright-Yleft;
         
-        if strcmp(Method,'Pad')
-        
-        % pad the stamp with zeros upto the full image size and add the images
-        
-            PadXL   = max(Xleft-1, 0);
-            PadXR   = max(SizeImX-Xright+1, 0);
-            PadYL   = max(Yleft-1, 0);
-            PadYR   = max(SizeImY-Yright+1, 0);  
-
-            Src = PSF(:,:,Isrc) .* Cat(Isrc,3);
-
-            Src = padarray(Src,[PadXL 0],'pre'); 
-            Src = padarray(Src,[PadXR 0],'post'); 
-            Src = padarray(Src,[0 PadYL],'pre'); 
-            Src = padarray(Src,[0 PadYR],'post');
-
-            Im = Im + Src .* Scaling^2;  
-            % NB! "imresize" scales the sum of the counts as Scale^2, so we need to scale the added signal
-        
-        elseif strcmp(Method,'Regular')
+        switch lower(Method)
             
-            for iX = 1:1:SzX
-
-                for iY = 1:1:SzY
-                    
-                    Im( Xleft+iX-1, Yleft+iY-1 ) = Im( Xleft+iX-1, Yleft+iY-1) + ...
-                        PSF(iX, iY, Isrc) .* Cat(Isrc,3) .* Scaling^2; 
-                    % NB! "imresize" scales the sum of the counts as Scale^2, so we need to scale the added signal
-                    
-                end
+            case 'pad'
                 
-            end
+                % pad the stamp with zeros upto the full image size and add the images
+        
+                PadXL   = max(Xleft-1, 0);
+                PadXR   = max(SizeImX-Xright+1, 0);
+                PadYL   = max(Yleft-1, 0);
+                PadYR   = max(SizeImY-Yright+1, 0);  
+
+                Src = PSF(:,:,Isrc) .* Cat(Isrc,3);
+
+                Src = padarray(Src,[PadXL 0],'pre'); 
+                Src = padarray(Src,[PadXR 0],'post'); 
+                Src = padarray(Src,[0 PadYL],'pre'); 
+                Src = padarray(Src,[0 PadYR],'post');
+
+                Im = Im + Src .* Scaling^2;  
+                % NB! "imresize" scales the sum of the counts as Scale^2, so we need to scale the added signal
+
+            case 'regular'
             
-        else
+                for iX = 1:1:SzX
+
+                    for iY = 1:1:SzY
+
+                        Im( Xleft+iX-1, Yleft+iY-1 ) = Im( Xleft+iX-1, Yleft+iY-1) + ...
+                            PSF(iX, iY, Isrc) .* Cat(Isrc,3) .* Scaling^2; 
+                        % NB! "imresize" scales the sum of the counts as Scale^2, so we need to scale the added signal
+
+                    end
+
+                end
+
+            otherwise
             
-            fprintf('Summation method not defined!\n');
+                fprintf('Summation method not defined!\n');
             
         end
                 
