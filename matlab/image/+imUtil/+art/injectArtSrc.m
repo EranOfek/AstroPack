df9fa3d1be4590678613350a7e5caeacee66d68a
A.M. Krassilchtchikov <agioffe@gmail.com>
2023-02-27 18:00:59 +0200

    daily update

--- matlab/image/+imUtil/+art/injectArtSrc.m
+++ matlab/image/+imUtil/+art/injectArtSrc.m
@@ -1,22 +1,25 @@
-function [Image, RotPSF] = injectArtSrc (X, Y, CPS, SizeX, SizeY, PSF, Args)
-    % Make an artificial image with rotated source PSFs injected to the catalog postions     
+function [Image, JPSF] = injectArtSrc (X, Y, CPS, SizeX, SizeY, PSF, Args)
+    % Make an artificial image with rotated and jitter-blurred source PSFs injected to the catalog postions     
     % Package: imUtil.art
-    % Description: Make an artificial image with rotated source PSFs injected to the catalog postions
-    %          - X, Y, CPS   : pixel coordinates and countrates of the sources
-    %          - SizeX, SizeY: pixel sizes of the image containing the source PSFs
-    %          - PSF         : either a single 2D PSF for all the object or 
-    %                          a 3D array of individual PSFs
+    % Description: Make an artificial image with rotated and jitter-blurred source PSFs injected to the catalog postions
+    %          - X, Y, CPS      : pixel coordinates and countrates of the sources
+    %          - SizeX, SizeY   : pixel sizes of the image containing the source PSFs
+    %          - PSF            : either a single 2D PSF for all the object or 
+    %                             a 3D array of individual PSFs
     %          - Args.PSFScaling: Image pixel size / PSF pixel size ratio
     %          - Args.RotatePSF : PSF rotation angle, either a single value
-    %                          for all the sources or a vector of angles
-    %          - Args.Method:  source injection method, either 'direct' or
-    %                          'PSFshift'
+    %                             for all the sources or a vector of angles
+    %          - Args.Jitter    : apply PSF blurring due to the S/C jitter  
+    %          - Args.Method    : source injection method, either 'direct' 
+    %                             or 'PSFshift'
     %          
-    % Output : - Image: a 2D array containing the resulting image and a 2+1 D array of rotated source PSFs
+    % Output : - Image: a 2D array containing the resulting source image 
+    %                   and a 2+1 D array of rotated and jittered source PSFs
     %            
     % Tested : Matlab R2020b
     %     By : A. Krassilchtchikov et al.    Feb 2023
-    % Example: Image = injectArtSrc (X, Y, CPS, PSF, Args)
+    % Example: [Image, JPSF] = injectArtSrc (X, Y, CPS, SizeX, SizeY, PSF,...
+    %                                        'PSFScaling',5,'RotatePSF',-90,'Jitter',1);
 
     arguments
         
@@ -28,6 +31,7 @@ function [Image, RotPSF] = injectArtSrc (X, Y, CPS, SizeX, SizeY, PSF, Args)
         PSF 
         Args.PSFScaling     =    1;       % Image/PSF pixel size ratio
         Args.RotatePSF      =    0;       % PSF stamp rotation angle
+        Args.Jitter         =    0;       % PSF blurring due to the S/C jitter
         Args.Method         =   'direct'; % injection method
                                           % 'direct' or 'FFTShift'
         Args.OutPut         =   'image';  % 'image' - image only; 
@@ -38,9 +42,11 @@ function [Image, RotPSF] = injectArtSrc (X, Y, CPS, SizeX, SizeY, PSF, Args)
 
     Image0 = zeros(SizeX, SizeY);
 
-    % get the number of sources
+    % get the number of sources and produce a source "catalog" array
 
     NumSrc = size(CPS,1);
+    
+    Cat = [X Y CPS];
 
     % consistency checks
 
@@ -50,7 +56,10 @@ function [Image, RotPSF] = injectArtSrc (X, Y, CPS, SizeX, SizeY, PSF, Args)
     end
 
     % rotate the PSFs (if needed)  
+    %
     % the rotation does not conserve the flux, thus also need to renormalize
+    % NB: the actual size of rotated PSF stamp depends on the particular rotation angle,
+    % varying between Nx x Ny and sqrt(2) * Nx x sqrt(2) * Ny
 
     if size(Args.RotatePSF,1) == NumSrc % an individual angle for each source
         for Isrc = 1:1:NumSrc
@@ -59,30 +68,36 @@ function [Image, RotPSF] = injectArtSrc (X, Y, CPS, SizeX, SizeY, PSF, Args)
         end
     elseif abs( Args.RotatePSF ) < 1 || abs( Args.RotatePSF - 360) < 1 % do nothing for small angles
         RotPSF = PSF;
-    else                      % rotate all the PSFs by the same angle
+    else                                % rotate all the PSFs by the same angle
         for Isrc = 1:1:NumSrc
             RotPSF(:,:,Isrc) = imrotate(PSF(:,:,Isrc), Args.RotatePSF(1), 'bilinear', 'loose'); 
             RotPSF(:,:,Isrc) = RotPSF(:,:,Isrc) / sum ( RotPSF(:,:,Isrc), 'all' ); % rescale
         end
     end
-            
-    % NB: the actual size of rotated PSF stamp depends on the particular rotation angle,
-    % varying between Nx x Ny and sqrt(2) * Nx x sqrt(2) * Ny
-    StampSize  = size(RotPSF);   
+                
+    % apply PSF blurring due to the S/C jitter
     
-        % fprintf('%s%4.1f%s\n','Final PSF stamp size ', StampSize / Args.PSFScaling , ' image pixels');
+    if Args.Jitter
+        JPSF = ultrasat.jitter(RotPSF, Cat, 'Exposure',300,'SigmaX0',2.,'SigmaY0',2.,...
+                               'Rotation',10,'Scaling',Args.PSFScaling);    
+    else
+        JPSF = RotPSF;
+    end
+    
+    % StampSize  = size(JPSF);   
+    % fprintf('%s%4.1f%s\n','Final PSF stamp size ', StampSize / Args.PSFScaling , ' image pixels');
     
     % test PSF containment width and pseudoFWHM width
     
-    ContWidth  = zeros(NumSrc,1);  % radius of encircled flux PSF region
-    PseudoFWHM = zeros(NumSrc,1);  % pseudo FWHM
+    ContWidth  = zeros(NumSrc,1);  % radius of the encircled flux PSF region
+    PseudoFWHM = zeros(NumSrc,1);  % pseudo FWHM of the PSFs (see imUtil.psf.pseudoFWHM for the particular algorithm)
     
     for Isrc = 1:1:NumSrc
         
-        ContWidth(Isrc) = imUtil.psf.containment('PSF',RotPSF(:,:,Isrc),'Level',0.5);
+        ContWidth(Isrc) = imUtil.psf.containment('PSF',JPSF(:,:,Isrc),'Level',0.5);
         
         [ widthX, widthY ] = ... 
-                    imUtil.psf.pseudoFWHM('PSF',RotPSF(:,:,Isrc),'Level',0.5);
+                    imUtil.psf.pseudoFWHM('PSF',JPSF(:,:,Isrc),'Level',0.5);
 
         PseudoFWHM(Isrc) = sqrt ( widthX^2 + widthY^2 );
         
@@ -100,23 +115,21 @@ function [Image, RotPSF] = injectArtSrc (X, Y, CPS, SizeX, SizeY, PSF, Args)
 %       xlabel('Radius, deg'); ylabel('pseudoFWHM, arcsec')
    
     % PSF injection: inject all the rotated source PSFs into the blank image 
-
-    Cat = [X Y CPS];
-
+  
     switch lower(Args.Method)
         
         case 'fftshift'
                    
-            if rem( size(RotPSF,1) , 2) == 0 
+            if rem( size(JPSF,1) , 2) == 0 
                 fprintf('The size of RotPSF is even, while imUtil.art.injectSources accepts odd size only! Exiting..');
                 return
             end
     
-            Image = imUtil.art.injectSources(Image0,Cat,RotPSF); 
+            Image = imUtil.art.injectSources(Image0,Cat,JPSF); 
 
         case 'direct'
                   
-            Image = imUtil.art.directInjectSources(Image0,Cat,Args.PSFScaling,RotPSF);
+            Image = imUtil.art.directInjectSources(Image0,Cat,Args.PSFScaling,JPSF);
     
         otherwise
         
