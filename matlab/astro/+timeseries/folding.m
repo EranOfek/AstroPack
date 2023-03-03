bdb8448dfa3cd5aaf208e4de1bea1d9bb59c2ab1
EranOfek <ofek.eran@gmail.com>
2023-03-03 15:15:43 +0200

    timeseries.folding - new version

--- matlab/astro/+timeseries/folding.m
+++ matlab/astro/+timeseries/folding.m
@@ -1,35 +1,84 @@
-function y=folding(x,p,c)
+function [Result, RefT0] = folding(LC, Period, Args)
 % Folding a timeseries by some period
-  % Package: timeseries
+% Package: timeseries
 % Description: Folding a time series into a period, and calculate the phase
 %              for each data point in the time series.
-% Input  : - Matrix in which one of the columns is the time.
+% Input  : - Matrix or table in which one of the columns is the time.
 %          - period to fold into.
-%          - Column number to fold by (time column). Defualt is 1.
+%          * ...,key,val,...
+%            'ColT' - Index of time column in input matrix.
+%                   Default is 1.
+%            'RefT0' - Reference t0 to subtract from time (time of
+%                   phase=0). If a function handle then will execute this
+%                   function on ther time column. Default is @median.
+%            'SortT' - a logical indicating if to sort the output by phase.
+%                   Default is false.
 % Output : - Matrix similar to the input matrix, but in which
-%            the time column is replaced by the phase., and
-%            the time column is concatenate in the last column.
-%            The output matrix is sorted by phase.
+%            the time column is replaced by the phase.
+%          - Reference t0 used.
 % Tested : Matlab 3.5
-%     By : Eran O. Ofek                    Nov 1993
+% Author : Eran Ofek (Nov 1993)
 %    URL : http://weizmann.ac.il/home/eofek/matlab/
 % Example: timeseries.folding(rand(100,2).*100,17);
 % Reliable: 1
 %--------------------------------------------------------------------------
-if nargin==2,
-   c=1;
-elseif nargin==3,
-   % do nothing
-else
-   error('Illegal number of input arguments');
+
+arguments
+    LC
+    Period
+    Args.ColT          = 1;
+    Args.RefT0         = @median;
+    Args.SortT logical = false;
 end
 
-jd_col      = length(x(1,:)) + 1;
-y           = zeros(length(x(:,1)),jd_col);
-TEMP        = x(:,c);
-r           = TEMP;
-TEMP        = TEMP./p-floor(TEMP./p);
-y           = x;
-y(:,c:c)    = TEMP;
-y(:,jd_col) = r;
-y           = sortrows(y,c);
+if isa(Args.RefT0, 'function_handle')
+    if istable(LC)
+        RefT0 = Args.RefT0(table2array(LC(:,Args.ColT)));
+    else
+        RefT0 = Args.RefT0(LC(:,Args.ColT));
+    end
+else
+    RefT0 = Args.RefT0;
+end
+    
+if istable(LC)
+    ColName = LC.Properties.VariableNames{Args.ColT};
+    T = LC.(ColName) - RefT0;
+else
+    T     = LC(:,Args.ColT) - RefT0;
+end
+TP    = T./Period;
+Phase = TP - floor(TP);
+Result = LC;
+if istable(LC)
+    Result.(ColName) = Phase;
+else
+    Result(:,Args.ColT) = Phase;
+end
+
+if Args.SortT
+    if istable(LC)
+        Result = sortrows(Result, ColName);
+    else
+        Result = sortrows(Result, Args.ColT);
+    end
+end
+
+%% old version
+%if nargin==2,
+%   c=1;
+%elseif nargin==3,
+%   % do nothing
+%else
+%   error('Illegal number of input arguments');
+%end
+%
+%jd_col      = length(x(1,:)) + 1;
+%y           = zeros(length(x(:,1)),jd_col);
+%TEMP        = x(:,c);
+%r           = TEMP;
+%TEMP        = TEMP./p-floor(TEMP./p);
+%y           = x;
+%y(:,c:c)    = TEMP;
+%y(:,jd_col) = r;
+%y           = sortrows(y,c);
