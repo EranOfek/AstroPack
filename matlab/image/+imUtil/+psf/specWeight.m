b2bb40d2f7ad1730849bffa942801e1b59f8bd2a
A.M. Krassilchtchikov <agioffe@gmail.com>
2023-03-06 11:25:30 +0200

    a faster version

--- matlab/image/+imUtil/+psf/specWeight.m
+++ matlab/image/+imUtil/+psf/specWeight.m
@@ -1,64 +1,71 @@
-function Result = specWeight(PSFdata, RadSrc, Rad, Spec)
+function Result = specWeight(SpecSrc, RadSrc, PSFdata, Args)
     % Make source PSFs at certain pixel distances on the detector weighted with the given spectra of the sources
     % Package: imUtil.psf
     % Description: make source PSFs at certain pixel distances on the detector weighted with the given spectra of the sources
-    % Input  : - PSFdata: a 4-D matrix of experimental PSF: (X, Y, wavelength, radius) 
-    %          - RadSrc: an array of radial distances of the sources from the detector tile reference point
-    %          - Rad: an array of radial distances sampled on the lab PSFs 
-    %          - Spec (Src, wavelength): a 2D array of source spectra 
-    % Output : - A 3-D matrix of spectrum-weighted PSFs
+    % Input  : - SpecSrc (SrcNum, Intensity) : an array of source spectra 
+    %          - RadSrc  (SrcNum, RadDistance): an array of radial distances of the sources from the detector tile reference point
+    %          - PSFdata: a 4-D matrix of experimental PSF: (X, Y, wavelength, radius) 
+    %          - Args.Rad: a vector of radial distances of the lab PSFs 
+    %          - Args.Lambda: a vector of wavelengths of the lab PSFs 
+    %          - Args.SpecLam: a vector of wavelengths of the source spectra
+    % Output : - Result(X,Y, SrcNum): A 3-D matrix of spectrum-weighted PSFs
     %            
     % Tested : Matlab R2020b
     %     By : A. Krassilchtchikov et al.    Feb 2023
     % Example: PSF = imUtil.psf.specWeight( PSFdata, RadSrc, Rad, Spec );
 
-    % check input parameters:
-
-    NSrc    = size(Spec,1); 
-    Nwave   = size(Spec,2); 
-
-    Nx      = size(PSFdata,1); 
-    Ny      = size(PSFdata,2); 
-    
-    if size(PSFdata,3) ~= Nwave
-        fprintf('Warning: Wavelength dimensions do not match!\n');
+    arguments
+        
+        SpecSrc 
+        
+        RadSrc 
+        
+        PSFdata 
+        
+        Args.Rad  
+        
+        Args.Lambda  = 0;   % if Args.Lambda and Args.SpecLam are not put in
+                            % that implies that the spectral grid in the
+                            % PSFdata array and in the input source spectra
+                            % SpecSrc is the same ! 
+        Args.SpecLam = 0;
+        
     end
-
-    if size(PSFdata,4) ~= size(Rad)
-        fprintf('Warning: Radial dimensions do not match!\n');
+    
+    % rescale the data array to the actual source positions:
+    
+    X       = 1:size(PSFdata,1);  
+    Y       = 1:size(PSFdata,2);
+    Lam     = 1:size(PSFdata,3);
+    
+    PSFdataS = interpn(X,Y,Lam, Args.Rad, PSFdata, X,Y,Lam, RadSrc);
+    
+    % regrid the input spectra if the spectral grids in PSFdata and in SpecSrc are not the same 
+    
+    if Args.SpecLam ~= 0 && Args.Lambda ~= 0   % regrid the spectra
+        
+        Spec = interp1(Args.SpecLam, SpecSrc, Args.Lambda);
+        
+    else                                        % the spectral grids in PSFdata and in SpecSrc are the same 
+         
+        Spec = SpecSrc;
+        
     end
     
-    Result  = zeros( Nx, Ny, NSrc );
-        
-    % make weighted PSFs for each of the sources:
+    % put the spectrum into the right dimension
+    % NOTE that the Spec array should be transposed before reshaping! 
     
-    for ISrc = 1:1:NSrc
+    Spec2 = reshape(Spec',[1 1 size(Spec,2) size(Spec,1)]);
+    
+    % multiply the PSF array sampled by the source positions by the source spectra
         
-        % find a nearest (left) grid node and interpolate the PSF matrix:
-        Ir = find (Rad  <= RadSrc(ISrc),  1, 'last');
-        
-        % linearly interpolate the PSF matrix:
-        
-        PSFint = zeros(Nx, Ny, Nwave); 
-                        
-        if Ir < size(Rad,2)                     % if we are not at the last point of the grid, interpolate
-            PSFint(:,:,:) = ( 1. / ( Rad(Ir+1) - Rad(Ir) ) ) * ...
-                ( PSFdata(:,:,:,Ir) * ( Rad(Ir+1)-RadSrc(ISrc) ) + PSFdata(:,:,:,Ir+1) * ( RadSrc(ISrc)-Rad(Ir) ) ) ;
-        else
-            PSFint(:,:,:) = PSFdata(:,:,:,Ir);  % do not extrapolate over the last point of the grid 
-        end
-        
-                
-        for Iw = 1:1:Nwave
-            
-            Result( :, :, ISrc) = Result( :, :, ISrc) + PSFint(:, :, Iw) .* Spec(ISrc,Iw);
-            
-        end
-        
-        % normalize the PSF to unity:
-        
-        Result( :, :, ISrc) = Result( :, :, ISrc) / sum( Result( :, :, ISrc), 'all' );
-                        
-    end
+    Wcube = PSFdataS .* Spec2;
+           
+    % sum over the wavelengths and delete the degenerate dimension 
+    
+    Result  = squeeze( sum(Wcube,3) ); 
+    
+    % normalize 
+    Result = Result ./ sum( Result, [1,2] );
 
 end
