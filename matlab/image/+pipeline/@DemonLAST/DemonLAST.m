f5b6206c043cdb28ac6b33410b6944bf373b9456
EranOfek <ofek.eran@gmail.com>
2023-03-15 16:43:14 +0200

    DemonLAST

---
+++ matlab/image/+pipeline/@DemonLAST/DemonLAST.m
@@ -0,0 +1,240 @@
+% ImagePath - A class for generating stand storing image/path names
+%       for ULTRASAT and LAST.
+%
+% File name format: <ProjName>_YYYYMMDD.HHMMSS.FFF_<filter>_<FieldID>_<counter>_<CCDID>_<CropID>_<type>_<level>.<sublevel>_<product>_<version>.<FileType>
+
+
+classdef DemonLAST < Component
+    % 
+
+    properties       
+        % These fields are the input parameters for getPath() and getFileName()
+        ProjectName  = 'LAST';
+        Node         = 1;
+        DataDir      = 1;
+        CamNumber
+        
+    end
+    
+    properties (Hidden)
+        % Fields formatting
+        %FormatFieldID   = '%06d';       % Used with FieldID
+        FormatCounter   = '%03d';       % Used with Counter        
+        FormatCCDID     = '%03d';       % Used with CCDID
+        FormatCropID    = '%03d';       % Used with CropID
+        FormatVersion   = '%03d';       % Used with Version
+        
+    end
+
+    properties (Hidden, SetAccess=protected, GetAccess=public)
+     
+    end
+    
+    properties (Hidden, Constant)
+        ListType        = { 'bias', 'dark', 'flat', 'domeflat', 'twflat', 'skyflat', 'fringe', 'focus', 'sci', 'wave', 'type' };
+        ListLevel       = {'log', 'raw', 'proc', 'stack', 'ref', 'coadd', 'merged', 'calib', 'junk'};
+        ListProduct     = { 'Image', 'Back', 'Var', 'Exp', 'Nim', 'PSF', 'Cat', 'Spec', 'Mask', 'Evt', 'MergedMat', 'Asteroids'};
+    end
+    
+    
+    methods % Constructor
+       
+        function Obj = FileNames(Pars)
+            % Constructor for DemonLAST
+            
+        end
+        
+    end
+    
+    methods % setter/getters
+    
+    end
+      
+    methods (Static) % path and files
+        function Result = constructProjectName(Project,Node,Mount,Camera)
+            % Construct project name of the form LAST.01.02.03
+            % Input  : - Project name.
+            %          - Node number
+            %          - Mount number (integer or string).
+            %          - Camera number
+            % Output : - Projet name
+            % Author : Eran Ofek (Mar 2023)
+
+            if ~ischar(Mount)
+                Mount = sprintf('%02d',Mount);
+            end
+            Result = sprintf('%s.%02d.%s.%02d',Project, Node, Mount, Camera);
+        end
+
+        function [MountNumberStr, MountNumber]=getMountNumber            
+            % Get mount number from computer name
+            % Output : - Mount number string.
+            %          - Mount number.
+            % Author : Eran Ofek (Mar 2023)
+
+            HostName = tools.os.get_computer;
+            MountNumberStr = HostName(5:6);
+            if nargout>1
+                MountNumber = str2double(MountNumberStr);
+            end
+        end
+
+        function [DataNumber, DataDir]=camNumber2dataDir(CameraNumber)
+            % Camera number to data dir
+            % Input  : - Camera number (1 to 4)
+            % Output : - Data dir number (1 or 2)
+            %          - Data dir name (e.g., 'data1')
+            % Author : Eran Ofek (Mar 2023)
+            
+            DataNumber = mod(CameraNumber-1,2)+1;
+            if nargout>1
+                DataDir = sprintf('data%d',DataNumber);
+            end
+        end
+
+        function Side=camNumber2computerSide(CameraNumber)
+            % Camera number to computer side
+            % Input  : - camera number (1 to 4)
+            % Output : - computer side ('e'|'w')
+            % AUthor : Eran Ofek (Mar 2023)
+
+            switch ceil(CameraNumber./2)
+                case 1
+                    Side = 'e';
+                case 2
+                    Side = 'w';
+                otherwise
+                    error('Unknown Computer side');
+            end
+        end
+
+        function [CameraNumber,Side]=dataDir2cameraNumber(DataDirNum,HostName)
+            % data dir number to camera number and computer side
+            % Input  : - DataDir number (1 or 2)
+            %          - Host name (e.g., 'last02w'). If empty, then will
+            %            get from computer host name.
+            %            Default is [].
+            % Output : - Camera number (1 to 4)
+            %          - Computer side ('e'|'w')
+            % Author : Eran Ofek (Mar 2023)
+
+            arguments
+                DataDirNum
+                HostName = [];
+            end
+            if isempty(HostName)
+                HostName = tools.os.get_computer;
+            end
+            Side = HostName(7);
+
+            switch Side
+                case 'e'
+                    CameraNumber = DataDirNum;
+                case 'w'
+                    CameraNumber = DataDirNum + 2;
+                otherwise
+                    error('Unknown Side')
+            end
+
+        end
+
+        function [BasePath,CameraNumber,Side,HostName,ProjName,MountNumberStr]=getBasePath(Args)
+            % get base path for LAST computers
+            % Input  : * ...,key,val,...
+            %            'DataDir' - DataDir number (1 or 2). If given than
+            %                   superceed the CameraNumber. Default is [].
+            %            'CamNumber' - Camera number (1 to 4).
+            %                   Default is [].
+            %            'HostName' - Computer host name. If empty, then
+            %                   get from computer host name.
+            %                   Default is [].
+            % Output : - Base path - e.g., '/last02e/data1/archive/LAST.01.02.01'
+            %          - Camera number
+            %          - Computer side
+            %          - Computer host name
+            %          - Project name
+            %          - Mount number string
+            % Author : Eran Ofek (Mar 2023)
+
+            arguments
+                Args.DataDir    = [];
+                Args.CamNumber  = [];
+                Args.HostName   = [];
+            end
+
+            if isempty(Args.CamNumber)
+                if isempty(Obj.DataDir)
+                    error('DataDir or CamNumber must be supplied');
+                else
+                    % only DataDir is given
+                    DataDir = Args.DataDir;
+                end
+            else
+                if isempty(Args.DataDir)
+                    % only CamNumber is given
+                    [DataDir] = pipeline.DemonLAST.camNumber2dataDir(Args.CamNumber);
+
+                else
+                    % both DataDir and CamNumber are given - use DataDir
+                    DataDir = Args.DataDir;
+                end
+            end
+
+            if isempty(Args.HostName)
+                HostName = tools.os.get_computer;
+            else
+                HostName = Args.HostName;
+            end
+            MountNumberStr = HostName(5:6);
+         
+            [CameraNumber,Side] = dataDir2cameraNumber(DataDirNum,HostName);
+            ProjName = pipeline.DemonLAST.constructProjectName(Obj.ProjectName,Obj.Node,MountNumberStr,CameraNumber);
+
+            % e.g., '/last02e/data1/archive/LAST.01.02.01'
+
+            DataStr = sprintf('data%d',DataDir);
+            BasePath = fullfile(filesep,HostName,DataStr,'archive','ProjName');
+
+        end
+
+    end
+
+    methods
+        
+
+        function Path=getPath(Obj, Args)
+            %
+
+            arguments
+                Obj
+                SubDir          = 'new';
+
+                Args.DataDir    = [];
+                Args.CamNumber  = [];
+            end
+
+            
+            if ~isempty(Args.DataDir)
+                Obj.DataDir = Args.DataDir;
+            end
+            if ~isempty(Args.CamNumber)
+                Obj.CamNumber = Args.CamNumber;
+            end
+
+            [BasePath,CameraNumber,Side,HostName,ProjName,MountNumberStr]=getBasePath('DataDir',Obj.DataDir,'CameraNumber',Obj.CamNumber);
+            Path = fullfile(BasePath,SubDir);
+
+
+
+        end
+
+    end
+
+
+    %----------------------------------------------------------------------
+    % Unit test
+    methods(Static)
+        Result = unitTest()
+    end
+    
+end
