7b11644856e98cf9a0b1bff26c23f31dcbadeeac
EranOfek <ofek.eran@gmail.com>
2023-05-03 23:34:53 +0300

    specUtil

---
+++ matlab/image/+specUtil/+trace/traceLocate.m
@@ -0,0 +1,29 @@
+function traceLocate(Image, Args)
+    %
+    
+    arguments
+        Image
+        Args.DimWave     = 2;
+        Args.
+    end
+    
+    if Args.DimWave==1
+        Image = Image.';
+    end
+    
+    [Nspat, Nwave] = size(Image);
+    
+    % collapse image in the wave-dir
+    
+    
+            
+    
+    
+    
+    
+   
+    
+    
+    
+    
+end
