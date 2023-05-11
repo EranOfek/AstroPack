38e14e31501ea685cacf65fef218ec3af1796045
Chen Tishler <chen@tishler.dev>
2023-05-11 15:03:34 +0300

    xxhash

--- matlab/mex/xxhash/test_xxhash.m
+++ matlab/mex/xxhash/test_xxhash.m
@@ -1,29 +1,16 @@
-function test()
+function test_xxhash()
 
-	testHashString();
-	testHashFile();
-end
-
-
-function testHashString()
 	% Test hashing a string
-	data = 'hello world';
-	expected = 'b5f4a5cc25822d41';
-	actual = dec2hex(xxhash_mex(data, length(data)));
-	testCase.verifyEqual(actual, expected);
-end
+    data = 'a';
+    xx = xxhash('Data', data);
+    fprintf('text: %u\n', xx);
 
-
-function testHashFile()
-	% Test hashing a file
-	filename = 'testfile.bin';
-	data = uint8([1 2 3 4 5]);
-	fid = fopen(filename, 'wb');
-	fwrite(fid, data, 'uint8');
-	fclose(fid);
-	expected = 'd9cf9a56b1a31b10';
-	actual = dec2hex(xxhash_mex(filename, 0));
-	delete(filename);
-	testCase.verifyEqual(actual, expected);
+	filename = 'README.md';
+    xx = xxhash('FileName', filename);
+    fprintf('file: %s - %u\n', filename, xx);    
+    
+	filename = 'c:\\temp\\v08.mp4';
+    xx = xxhash('FileName', filename);
+    fprintf('file: %s - %u\n', filename, xx);    
 end
 
