533cbe1767f120e2f80ecbd369dcd005ef86f861
Chen Tishler <chen@tishler.dev>
2023-05-29 16:37:55 +0300

    removed bat

--- matlab/util/+tools/+checksum/+mex/_build_mex.bat
+++
@@ -1,8 +0,0 @@
-rem Compile all MEX files in folder
-rem NOTE: You may need to type 'clear all' in MATLAB to release the current compiled binary file.
-
-call mex mex_xxhash.cpp
-call mex mex_xxhashFile.cpp
-
-:exit
-pause
