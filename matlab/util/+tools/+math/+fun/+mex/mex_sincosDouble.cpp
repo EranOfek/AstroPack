0b4f3d6a369e24bd43f14002c104c3ed065e8a9b
Chen Tishler <chen@tishler.dev>
2023-05-29 16:38:13 +0300

    draft

--- matlab/util/+tools/+math/+fun/+mex/mex_sincosDouble.cpp
+++
@@ -1,5 +0,0 @@
-typedef unsigned char __Type;
-#define MEX_INT  mxDOUBLE_CLASS
-#define MEX_UINT mxDOUBLE_CLASS
-
-#include "mex_sincos_include.cpp"
--- matlab/util/+tools/+math/+fun/+mex/mex_sincosSingle.cpp
+++
@@ -1,5 +0,0 @@
-typedef unsigned char __Type;
-#define MEX_INT  mxSINGLE_CLASS
-#define MEX_UINT mxSINGLE_CLASS
-
-#include "mex_sincos_include.cpp"
