2d7f4d19652d8f3aeff5e9dde2defbbd6633fe18
EranOfek <ofek.eran@gmail.com>
2023-05-30 17:45:05 +0300

    GALEX pipeline

--- matlab/image/+pipeline/+galex/procGALEX.m
+++ matlab/image/+pipeline/+galex/procGALEX.m
@@ -1,7 +1,17 @@
-function AC=procGALEX
+function [AC,MS]=procGALEX
 
-    %
+    % Example: [AC,MS]=pipeline.galex.procGALEX;
 
+
+ColCell             = {'XPEAK','YPEAK',...
+                                                 'X1', 'Y1',...
+                                                 'X2','Y2','XY',...
+                                                 'SN','BACK_IM','VAR_IM',...  
+                                                 'BACK_ANNULUS', 'STD_ANNULUS', ...
+                                                 'FLUX_APER', 'FLUXERR_APER',...
+                                                 'MAG_APER', 'MAGERR_APER',...
+                                                 'FLUX_CONV', 'MAG_CONV', 'MAGERR_CONV','MAG_APER','MAGERR_APER'};
+MatchedColums    = {'RA','Dec','X1','Y1','SN_1','SN_2','SN_3','SN_4','MAG_CONV_2','MAGERR_CONV_2','MAG_CONV_3','MAGERR_CONV_3','FLAGS', 'MAG_PSF','MAGERR_PSF'};
 
     FileList = io.files.filelist('WTTHV2*-int.fits');
     Nim = numel(FileList);
@@ -36,7 +46,9 @@ function AC=procGALEX
         AI.Var = MeanBack./ExpAI.Image;
         AI.Back = zeros(size(AI.Var));
     
-        AI = imProc.sources.findMeasureSources(AI);
+        AI = imProc.sources.findMeasureSources(AI, 'ColCell',ColCell);
+        AI = imProc.psf.constructPSF(AI);
+        AI = imProc.sources.psfFitPhot(AI);
         
         AI.populateWCS;
         AI.propagateWCS('UpdateHead',false);
@@ -48,6 +60,6 @@ function AC=procGALEX
     MAC = imProc.match.match(AC, AC(1));
 
     MS = MatchedSources;
-    [MS, Matched] = unifiedCatalogsIntoMatched(MS, AC);
+    [MS, Matched] = unifiedCatalogsIntoMatched(MS, AC, 'MatchedColums',MatchedColums);
 
 end
