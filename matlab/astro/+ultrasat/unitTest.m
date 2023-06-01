cb5f2978a6161bd747aef1599d79c69b334116e1
A.M. Krassilchtchikov <agioffe@gmail.com>
2023-06-01 15:09:20 +0300

    unit test updated and checked

--- matlab/astro/+ultrasat/unitTest.m
+++ matlab/astro/+ultrasat/unitTest.m
@@ -10,7 +10,17 @@ function Result = unitTest()
     % Package Unit-Test   
 	io.msgStyle(LogLevel.Test, '@start', 'test started');
     
-    func_unitTest();
+    % testing usim simulation utility:
+    
+    SimA = ultrasat.usim('InCat',1000, 'Tile', 'A');
+    SimB = ultrasat.usim('InCat',1000, 'Tile', 'B');
+    SimC = ultrasat.usim('InCat',1000, 'Tile', 'C');
+    SimD = ultrasat.usim('InCat',1000, 'Tile', 'D');
+    
+    MergedImage = ultrasat.umergeTileImages ();
+    
+    
+%     func_unitTest();
     
 	io.msgStyle(LogLevel.Test, '@passed', 'test passed');
 	Result = true;
