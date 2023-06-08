40f552a70e07130b98e24e11be0e1c853ff04d84
EranOfek <ofek.eran@gmail.com>
2023-06-08 20:28:10 +0300

    procCoadd

--- matlab/image/+pipeline/+generic/procCoadd.m
+++ matlab/image/+pipeline/+generic/procCoadd.m
@@ -1,34 +1,145 @@
-function [MergedCat, MatchedS, Coadd, ResultSubIm, ResultAsteroids, ResultCoadd] = procMergeCoadd(AllSI, Args)
-    % Given a list of processed images, merged their catalogs, coadd
-    % their images, and produce coadd catalogs.
+function [Coadd,ResultCoadd]=procCoadd(AllSI, Args)
+    % Coadd a list of processed images, estimate background and find sources in the coadd images.
+    %   The function works on 2-D array of of AstroImage objects in which
+    %   one dimension (default is 1) corresponds to the epoch and the other
+    %   to the field (i.e., different field). Each field will be coadded,
+    %   so the result is a vector of AstroImage objects which length equal
+    %   to the number of fields.
+    %   
     %   This is a basic generic pipeline conducting the following steps:
-    %   0. Input is an array of processed images in which different epochs
-    %      are in different lines, while different fields are in columns.
-    %   1. Merge the catalogs for each field (columns of the input array)
-    %      using imProc.match.mergeCatalogs
-    %   2. Search asteroids using proper motions measured in the merged
-    %      catalogs and using imProc.asteroids.searchAsteroids_pmCat
-    %   3. Register the images of each field using their existing WCS and
+    %   1. Register the images of each field using their existing WCS and
     %      imProc.transIm.imwarp
-    %   4. Coadd the images of each field using imProc.stack.coadd
-    %   5. Measure background of coadd images using imProc.background.background
-    %   6. Mask pixels dominated by source noise using imProc.mask.maskSourceNoise
-    %   7. Find and measure sources using imProc.sources.findMeasureSources
-    %   8. AStrometry of the coadd images using imProc.astrometry.astrometryRefine
-    %   9. Photometric ZP of the coadd images using imProc.calib.photometricZP
-    %   10. Match the coadd catalogs against the catsHTM MergedCat using
+    %   2. Coadd the images of each field using imProc.stack.coadd
+    %   3. Measure background of coadd images using imProc.background.background
+    %   4. Mask pixels dominated by source noise using imProc.mask.maskSourceNoise
+    %   5. Find and measure sources using imProc.sources.findMeasureSources
+    %   6. AStrometry of the coadd images using imProc.astrometry.astrometryRefine
+    %   7. Photometric ZP of the coadd images using imProc.calib.photometricZP
+    %   8. Match the coadd catalogs against the catsHTM MergedCat using
     %       imProc.match.match_catsHTMmerged
-    % Input  : -
+    % Input  : - A 2-D array of AstroImage objects in which
+    %            one dimension (default is 1) corresponds to the epoch and the other
+    %            to the field (i.e., different field). Each field will be coadded,
+    %            so the result is a vector of AstroImage objects which length equal
+    %            to the number of fields.
+    %          * ...,key,val,..
+    %            'EpochDim' - Dimension (1 or 2) of the epoch axis in the
+    %                   input AstroImage object. Default is 1.
+    %
+    %            'coaddArgs' - A cell array of arguments to pass to the 
+    %                   imProc.stack.coadd function.
+    %                   default is {'StackArgs',{'MeanFun',@mean, 'StdFun',@tools.math.stat.nanstd, 'Nsigma',[3 3], 'MaxIter',2}};
+    %            'backgroundArgs' - A cell array of arguments to pass to
+    %                   the imProc.background.background function.
+    %                   Default is {}.
+    %            'BackSubSizeXY' - A value to pass to the SubSizeXY of the
+    %                   background function (i.e., the sub images size in
+    %                   which the background is calculated.
+    %                   Default is [128 128].
+    %            'findMeasureSourcesArgs' - A cell array of arguments to
+    %                   pass to the imProc.sources.findMeasureSources
+    %                   Default is {}.
+    %            'ZP' - Zero point of the photometry to pass to the findMeasureSources
+    %                   and psfPhotCube functions.
+    %                   Default is 25.
+    %            'ColCell' - A cell array of 'ColCell' argument to pass to
+    %                   findMeasureSources. This indicate the columns
+    %                   output of the source finding routine.
+    %                   Default is {'XPEAK','YPEAK',...
+    %                                'X1', 'Y1',...
+    %                                'X2','Y2','XY',...
+    %                                'SN','BACK_IM','VAR_IM',...  
+    %                                'BACK_ANNULUS', 'STD_ANNULUS', ...
+    %                                'FLUX_APER', 'FLUXERR_APER',...
+    %                                'MAG_APER', 'MAGERR_APER',...
+    %                                'FLUX_CONV', 'MAG_CONV', 'MAGERR_CONV'};
+    %            'Threshold' - Detection threshold for source finding.
+    %                   Default is 5.
+    %            'astrometryRefineArgs' - A cell array of arguments to pass
+    %                   to imProc.astrometry.astrometryRefine
+    %                   Default is {}.
+    %            'Scale' - Pixel scale of detector.
+    %                   Default is 1.25 arcsec/pix.
+    %            'Tran' - A Tran2D object to use for the astrometric solution.
+    %                   Default is Tran2D('poly3')
+    %            'CatName' - catsHTM astrometric catalog to use for the
+    %                   astrometric solution.
+    %                   Default is 'GAIADR3'
+    %            'photometricZPArgs' - A cell array of arguments to pass to
+    %                   the imProc.calib.photometricZP function.
+    %                   Default is {}.
+    %            'ReturnRegisteredAllSI' - If true, then the 1st input argument
+    %                   AllSI will be modified and contain the registered
+    %                   images.
+    %                   Default is true.
+    %            'CoaddLessFrac' - A fraction of images. If the fraction of
+    %                   images in the coadd of a specific pixel is smaller than
+    %                   this value, then the 'CoaddLessImages' bit in the bit mask image
+    %                   will be flagged.
+    %                   Default is 0.6.
+    %            'BitName_CoaddLess' - The bit name in the bit mask image
+    %                   containing the coadd less images.
+    %                   Default is 'CoaddLessImages'.
+    %            'HighBackNsigma' - If not empty, then will remove images
+    %                   with high background. This is the number of sigmas
+    %                   of the background above the median images
+    %                   background, to remove.
+    %                   Default is 3.
+    %
+    %            'MatchedS' - An optional MatchedSources object containing
+    %                   the matched sources of the ind the individual
+    %                   object (element per image field).
+    %                   If given, then the the x/y shift between the images
+    %                   will be calculated from this input, and will be
+    %                   used in the imProc.transIm.imwarp function.
+    %                   If empty, then will use the WCS for transformation.
+    %                   Default is [].
+    %            'ColX' - Column name in the input MatchedS MatchedSources that
+    %                   will be used to calculate the X shift between the images.
+    %                   Default is 'X1'.
+    %            'ColY' - The same as 'ColX', but for the Y axis.
+    %                   Default is 'Y1'.
+    %            'DeleteBackBeforeCoadd' - Delete Back property from input
+    %                   AstroImage object. Default is false.
+    %            'DeleteVarBeforeCoadd' - Delete Var property from input
+    %                   AstroImage object. Default is false.
+    %            'AddGlobalMotion' - A logical indicating if to add a
+    %                   keywords to the header of the coadd images that
+    %                   contains information on the mean X and Y motion
+    %                   between the images.
+    %                   Default is true.
+    %            'constructPSFArgs' - A cell array of arguments to pass to
+    %                   imProc.psf.constructPSF
+    %                   Default is {}.
+    %            'psfFitPhotArgs' - A cell array of arguments to pass to
+    %                   imProc.sources.psfFitPhot
+    %                   Default is {}.
+    %
+    %            'CoaddMatchMergedCat' - A logical indicating if to add a
+    %                   MergedCat column to the catalog of sources in the coadd
+    %                   image. If true, then each source will be matched
+    %                   against the catsHTM MergedCat catalog and a bit
+    %                   mask of catalogs in which the source appear will be
+    %                   added to the catalog.
+    %                   Default is true.
+    %            'MergedCat' - An optional AstroCatalog Merged catalog of all the images
+    %                   to coadd. If not empty, then will copy columns
+    %                   specified in 'Col2copy' from the MergedCat to the
+    %                   catalogs in the coadd images.
+    %                   The number of elements shold be equal to the numbre
+    %                   of coadd images.
+    %                   Default is [].
+    %            'Col2copy' - A cell array of columns to copy from the
+    %                   the MergedCat input argument to the catalog of the
+    %                   coadd image.
+    %                   Default is {'Nobs'}.
+    
     % Output : -
-    % Author : Eran Ofek (Jan 2022)
-    % Example: 
-    
-    
+   
     arguments
         AllSI
-        Args.mergeCatalogsArgs cell           = {};
-        Args.MergedMatchMergedCat logical     = true;
-        Args.CoaddMatchMergedCat logical      = true;
+        Args.EpochDim   = 1;
+        
         Args.coaddArgs cell                   = {'StackArgs',{'MeanFun',@mean, 'StdFun',@tools.math.stat.nanstd, 'Nsigma',[3 3], 'MaxIter',2}};
         Args.backgroundArgs cell              = {};
         Args.BackSubSizeXY                    = [128 128];
@@ -56,21 +167,28 @@ function [MergedCat, MatchedS, Coadd, ResultSubIm, ResultAsteroids, ResultCoadd]
         %Args.RemoveHighBackImages logical     = true;   % remove images which background differ from median back by 'HighBackNsigma' sigma
         Args.HighBackNsigma                   = 3;
         
+        Args.MatchedS                         = [];
         Args.ColX                             = 'X1';   % used for shift estimate
         Args.ColY                             = 'Y1';
-        Args.StackMethod                      = 'sigmaclip';        
-        Args.Asteroids_PM_MatchRadius         = 3;
-
+        
         Args.DeleteBackBeforeCoadd logical    = false;
         Args.DeleteVarBeforeCoadd logical     = false;
 
         Args.AddGlobalMotion logical          = true;
         Args.constructPSFArgs cell            = {};
         Args.psfFitPhotArgs cell              = {};
-        
+
+        Args.CoaddMatchMergedCat logical      = true;
+        Args.MergedCat                        = [];
         Args.Col2copy cell                    = {'Nobs'};  % cell array of columns to copy from MergedCat to Coadd
     end
+    
     SEC_DAY = 86400;
+    
+    if Args.EpochDim==2
+        % transpose in order to make the epochs in the 1st dimension
+        AllSI = AllSI.';
+    end
     
     % get JD
     JD = julday(AllSI(:,1));
@@ -87,25 +205,7 @@ function [MergedCat, MatchedS, Coadd, ResultSubIm, ResultAsteroids, ResultCoadd]
     if ~all(FlagGoodAstrometry)
         warning('Some sub images have bad astrometry');
     end
-    [MergedCat, MatchedS, ResultSubIm.ResZP, ResultSubIm.ResVar, ResultSubIm.FitMotion] = imProc.match.mergeCatalogs(AllSI,...
-                                                                                                            Args.mergeCatalogsArgs{:},...
-                                                                                                            'FlagGood',FlagGoodAstrometry,...
-                                                                                                            'MergedMatchMergedCat',Args.MergedMatchMergedCat);
-    
-    % search for asteroids - proper motion channel
-    [MergedCat, ResultAsteroids.AstCrop] = imProc.asteroids.searchAsteroids_pmCat(MergedCat,...
-                                                                                  'BitDict',AllSI(1).MaskData.Dict,...
-                                                                                  'JD',JD,...
-                                                                                  'PM_Radius',Args.Asteroids_PM_MatchRadius,...
-                                                                                  'Images',AllSI);
-    
-    % search for asteroids - orphan channel
-    % imProc.asteroids.searchAsteroids_orphans
-    
-    % cross match with external catalogs
-    
-    % flag orphans
-    
+   
     
     % delete Back and Var before coaddition
     if Args.DeleteBackBeforeCoadd
@@ -138,8 +238,13 @@ function [MergedCat, MatchedS, Coadd, ResultSubIm, ResultAsteroids, ResultCoadd]
     Coadd       = AstroImage([Nfields, 1]);  % ini Coadd AstroImage
     for Ifields=1:1:Nfields
         if FlagGoodAstrometry(Ifields)
-            ResultCoadd(Ifields).ShiftX = median(diff(MatchedS(Ifields).Data.(Args.ColX),1,1), 2, 'omitnan');
-            ResultCoadd(Ifields).ShiftY = median(diff(MatchedS(Ifields).Data.(Args.ColY),1,1), 2, 'omitnan');
+            if isempty(Args.MatchedS)
+                ResultCoadd(Ifields).ShiftX = NaN;
+                ResultCoadd(Ifields).ShiftY = NaN;
+            else
+                ResultCoadd(Ifields).ShiftX = median(diff(Args.MatchedS(Ifields).Data.(Args.ColX),1,1), 2, 'omitnan');
+                ResultCoadd(Ifields).ShiftY = median(diff(Args.MatchedS(Ifields).Data.(Args.ColY),1,1), 2, 'omitnan');
+            end
 
             ShiftXY = cumsum([0 0; -[ResultCoadd(Ifields).ShiftX, ResultCoadd(Ifields).ShiftY]]);
 
@@ -147,13 +252,13 @@ function [MergedCat, MatchedS, Coadd, ResultSubIm, ResultAsteroids, ResultCoadd]
             FlagGoodWCS = imProc.astrometry.isSuccessWCS(AllSI(:,Ifields));
 
             % Remove Images with high background
-            if ~isempty(Args.HighBackNsigma)
+            if isempty(Args.HighBackNsigma)
+                FlagGood = FlagGoodWCS;
+            else
                 MedBack = imProc.stat.median(AllSI(:,Ifields));
                 FlagGoodBack = MedBack < (median(MedBack) + Args.HighBackNsigma.*tools.math.stat.rstd(MedBack));
 
                 FlagGood = FlagGoodWCS & FlagGoodBack;
-            else
-                FlagGood = FlagGoodWCS;
             end
             
             % no need to transform WCS - as this will be dealt later on
@@ -162,13 +267,22 @@ function [MergedCat, MatchedS, Coadd, ResultSubIm, ResultAsteroids, ResultCoadd]
             % if sum(FlagGood)<20
             %     'a'
             % end
-            RegisteredImages = imProc.transIm.imwarp(AllSI(FlagGood,Ifields), ShiftXY(FlagGood,:),...
+            if isempty(Args.MatchedS)
+                Igood = find(FlagGood, 1, 'first');
+                RegisteredImages = imProc.transIm.imwarp(AllSI(FlagGood,Ifields), AllSI(Igood, Ifields).WCS,...
                                                      'TransWCS',false,...
                                                      'FillValues',0,...
                                                      'ReplaceNaN',true,...
                                                      'CreateNewObj',~Args.ReturnRegisteredAllSI);
 
+            else
+                RegisteredImages = imProc.transIm.imwarp(AllSI(FlagGood,Ifields), ShiftXY(FlagGood,:),...
+                                                     'TransWCS',false,...
+                                                     'FillValues',0,...
+                                                     'ReplaceNaN',true,...
+                                                     'CreateNewObj',~Args.ReturnRegisteredAllSI);
 
+            end
 
             % use sigma clipping...
             % 1. NOTE that the mean image is returned so that the effective gain
@@ -209,7 +323,7 @@ function [MergedCat, MatchedS, Coadd, ResultSubIm, ResultAsteroids, ResultCoadd]
 
 
             % PSF photometry
-            [Coadd(Ifields), ResPSF] = imProc.sources.psfFitPhot(Coadd(Ifields), 'CreateNewObj',false, 'ZP',Args.ZP, Args.psfFitPhotArgs{:});                  
+            [Coadd(Ifields), ResPSF] = imProc.sources.psfFitPhot(Coadd(Ifields), 'CreateNewObj',false, 'ZP',Args.ZP, Args.psfFitPhotArgs{:}); 
 
             % astrometry    
             % Note that if available, will use the "X" & "Y" positions produced
@@ -278,8 +392,11 @@ function [MergedCat, MatchedS, Coadd, ResultSubIm, ResultAsteroids, ResultCoadd]
     end
     
     % match Coadd catalog against MergedCat
-    [Coadd] = imProc.match.insertColFromMatched_matchIndices(Coadd, MergedCat, [], 'CreateNewObj',false, 'Col2copy', Args.Col2copy);
+    if ~isempty(Args.MergedCat)
+        [Coadd] = imProc.match.insertColFromMatched_matchIndices(Coadd, Args.MergedCat, [], 'CreateNewObj',false, 'Col2copy', Args.Col2copy);
+    end
     
-        
-end
+       
 
+
+end
