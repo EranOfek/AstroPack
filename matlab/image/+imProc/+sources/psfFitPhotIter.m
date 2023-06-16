d996d07ff140e9b493943d160062008523cbe516
EranOfek <ofek.eran@gmail.com>
2023-06-16 12:16:08 +0300

    psfFitPhotIter

--- matlab/image/+imProc/+sources/psfFitPhotIter.m
+++ matlab/image/+imProc/+sources/psfFitPhotIter.m
@@ -10,11 +10,14 @@ function [Result,AISub] = psfFitPhotIter(AI,Args)
     %                   duplicate it 'Niter' (below) times.
     %                   Default is [100,50,5]. 
     %             'CreateNewObj' - A logical indicating if to copy the
-    %                   content of the AI object. Default is true.
-    %             'Niter' - number of iteration to perform. This value will
+    %                   content of the AI object.
+    %                   Default is true.
+    %             'Niter' - Number of iteration to perform. This value will
     %                   be used in case 'Threshold' contains only single number.
+    %                   Default is 3.
     %             'ReCalcBack' - A logical indicating if to calculate the
     %                   background in each iteration.
+    %                   Default is true.
     %             'ColNameIter' - A string indicating the name of column that
     %                   indicates the iteration in which the a specific sources
     %                   were detected in the output catalog. If empty (''),
@@ -35,6 +38,8 @@ function [Result,AISub] = psfFitPhotIter(AI,Args)
     %             'backgroundArgs' - A cell array of additional args to
     %                   pass to imProc.background.background.
     %                   Default is {}.
+    %             'ColSN' - Column name containing the S/N to use for the
+    %                   thresholding. Default is 'SN_2'.
     %
     % Output  :  -An AstroImage or AstroCatalog that conatin the new catalog.
     %            -An AstroImage contain the substracted images and the new catalog.
@@ -49,39 +54,37 @@ function [Result,AISub] = psfFitPhotIter(AI,Args)
 
     arguments
 
-        AI;
-        Args.Threshold=[100,50,5];
-        Args.CreateNewObj =true;
-        Args.Niter = 3;
-        Args.ReCalcBack = true;
-        Args.ColNameIter  = 'PSF_ITER';
-        Args.OutType  = 'AstroImage'; %|AstroImage or AstroCatalog
-        Args.findSourcesArgs = {};
-        Args.psfFitPhotArgs = {};
-        Args.injectSourcesArgs= {};
-        Args.backgroundArgs = {};
+        AI
+        Args.Threshold              = [100,50,5];
+        Args.CreateNewObj logical   = true;
+        Args.Niter                  = 3;
+        Args.ReCalcBack logical     = true;
+        Args.ColNameIter            = 'PSF_ITER';
+        Args.OutType                = 'AstroImage'; %|AstroImage or AstroCatalog
+        Args.findSourcesArgs cell   = {};
+        Args.psfFitPhotArgs cell    = {};
+        Args.injectSourcesArgs cell = {};
+        Args.backgroundArgs cell    = {};
+        
+        Args.ColSN                  = 'SN_2';
     end
-
-
-
 
     if numel(Args.Threshold)==1
         Threshold = Args.Threshold.*ones(Args.Niter,1);
     else
         Threshold =  Args.Threshold ;
     end
-
+    Niter         = numel(Threshold);
+    
+    
     if Args.CreateNewObj
         AISub = AI.copy();
     else
         AISub = AI;
     end
 
-
-
-
-    Niter = numel(Threshold);
-    [Imsz1,Imsz2] =AISub.sizeImage;
+    
+    [Imsz1,Imsz2] = AISub.sizeImage;
 
     for Iiter = 1:1:Niter
         if Args.ReCalcBack % recalculate background
@@ -89,12 +92,15 @@ function [Result,AISub] = psfFitPhotIter(AI,Args)
         end
         % Source detection using the psf from AISub 
         AISub = imProc.sources.findSources(AISub,'Psf',AISub.PSF,'Threshold',Threshold(Iiter),Args.findSourcesArgs{:});
+        
+        % why is this needed?
         AISub.CatData.Catalog = double(AISub.CatData.Catalog);
+        
         if AISub.CatData.isemptyCatalog
             continue;
         end
         % psf photometry. The use of SN_1 isn't robust.
-        [AISub] = imProc.sources.psfFitPhot(AISub,'PSF',AISub.PSF,'ColSN','SN_1','HalfSize',floor(numel(AISub.PSF(:,1))/2),Args.psfFitPhotArgs{:});
+        [AISub] = imProc.sources.psfFitPhot(AISub, 'PSF',AISub.PSF, 'ColSN',Args.ColSN, 'HalfSize',floor(numel(AISub.PSF(:,1))/2), Args.psfFitPhotArgs{:});
         if ~isempty(Args.ColNameIter) % Add iter number to the catalog.
             AISub.CatData.insertCol((Iiter).*ones(numel(AISub.CatData.Catalog(:,1)),2) ,Inf,{Args.ColNameIter},{''});
         end
