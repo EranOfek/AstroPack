c4e3584e6ed11648a209ec8f49502a88f08868e4
Enrico Segre <enrico@euler.weizmann.ac.il>
2023-07-03 12:24:17 +0300

    refactor imUtil.art.injectSources and correct indexing bugs (issue #184)

--- matlab/image/+imUtil/+art/injectSources.m
+++ matlab/image/+imUtil/+art/injectSources.m
@@ -7,11 +7,11 @@ function S = injectSources(Image,Cat,PSFin,Args)
     % Input  :  - An image (matrix) to inject the source. In case of two
     %             components vector (i.e., [1200,1000]), the function will
     %             inject sources to an empty image with size set by Image. 
-    %             In case of scalar, the function will inject into an equal
-    %             size empty image. In case of a matrix with more than two
+    %             For a scalar, the function will inject into an equal
+    %             sized empty image. In case of a matrix with more than two
     %             elements, the function will add the injected sources to
     %             the input image.
-    %           - A three column vector contains X, Y and flux, for each
+    %           - A three column vector containing X, Y and flux, for each
     %             source. 
     %           - PSF stamps. Either a single PSF stamp (2D) or PSF stamp per source
     %             in a 3D matrix, where the size of third dimension equal the nubmer of sources.
@@ -19,22 +19,21 @@ function S = injectSources(Image,Cat,PSFin,Args)
     %
     %           * ...,key,val,...
     %             'RecenterPSF' - a logical indicating if to set the center
-    %                   of the PSF by the first moment instead of the centeral pixel. 
+    %                   of the PSF by the first moment instead of the central pixel. 
     %                   Default is false.
     %
     % Output : - An image with the injected sources (matrix).
     % Author : Noam Segev (Jan 2023)
+    %          refactored by Enrico Segre (Jul 2023)
     % Example: PSF = imUtil.kernel2.gauss; 
     %          Cat = [rand(10,2)*100,rand(10,1)*1e5];
-    %          S = imUtil.art.injectSources(100,Cat,PSF);
+    %          S = imUtil.art.injectSources(100,Cat,PSF)
     
     arguments
-
         Image;
         Cat;
         PSFin;
         Args.RecenterPSF = false;
-    
     end
 
     if numel(Image)==2     % Assume the output is imagesize
@@ -55,87 +54,46 @@ function S = injectSources(Image,Cat,PSFin,Args)
     Xround=  floor(X);
     Yround=  floor(Y);
     Nsrc = numel(flux);
+    Npsf = size(PSFin,3);
+    if Npsf~=1 && Npsf ~= Nsrc
+        error("injectSources must get either a single PSF or as many PSFs as sources")
+    end
 
-    switch size(PSFin,3)
-        
-        case Nsrc % Case of psf per each source
-            
-            for i = 1:Nsrc
-                
-                PSF(:,:) = PSFin(:,:,i);
-                
-                [SizePSFX,SizePSFY] = size(PSF);
-                Xcenter = ceil(SizePSFX./2);
-                Ycenter = ceil(SizePSFX./2);
-                
-                if Args.RecenterPSF
-                    momt = imUtil.image.moment2(PSF,SizePSFX,SizePSFY);
-                    DX = DX - (Xcenter - momt.X);
-                    DY = DY - (Ycenter - momt.Y);
-                end
-                
-                Xind_vec = 1:1:SizePSFX;
-                Yind_vec = 1:1:SizePSFY;
-                VecX= Xind_vec-Xcenter;
-                VecY= Yind_vec-Ycenter;
-                [matx,maty]= meshgrid(VecX,VecY);
-                PSF_shiftted = imUtil.trans.shift_fft(PSF,DX,DY);
-                
-                Xind = matx+ Xround(i);
-                Yind = maty+ Yround(i);
-                
-                flag = ~(Xind <=0 | Yind<=0 |Xind>SizeImageX| Yind>SizeImageY);
-                ind = sub2ind([SizeImageX,SizeImageY],Yind(flag),Xind(flag));
-                
-                ind(isnan(ind))=[];
-                
-                psf_t = squeeze(PSF_shiftted(:,:,i));
-                S(ind) = S(ind) +  psf_t(flag(:)).*flux(i);
-                
-            end
-            
-        case 1    % one PSF for all the sources
-            
-            PSF(:,:) = PSFin(:,:,1);
-            
+    for i = 1:Nsrc
+
+        if Npsf>1 || i==1
+            PSF(:,:) = PSFin(:,:,i);
             [SizePSFX,SizePSFY] = size(PSF);
             Xcenter = ceil(SizePSFX./2);
-            Ycenter = ceil(SizePSFX./2);
-            
+            Ycenter = ceil(SizePSFY./2);
+
             if Args.RecenterPSF
                 momt =imUtil.image.moment2(PSF,SizePSFX,SizePSFY);
                 DX = DX - (Xcenter - momt.X);
                 DY = DY - (Ycenter - momt.Y);
-                
             end
-            
+
             Xind_vec = 1:1:SizePSFX;
             Yind_vec = 1:1:SizePSFY;
             VecX= Xind_vec-Xcenter;
             VecY= Yind_vec-Ycenter;
             [matx,maty]= meshgrid(VecX,VecY);
-            PSF_shiftted = imUtil.trans.shift_fft(PSF,DX,DY);
-            
-            for i = 1:Nsrc
-                
-                
-                Xind = matx+ Xround(i);
-                Yind = maty+ Yround(i);
-                
-                flag = ~(Xind <=0 | Yind<=0 |Xind>SizeImageX| Yind>SizeImageY);
-                ind = sub2ind([SizeImageX,SizeImageY],Yind(flag),Xind(flag));
-                
-                ind(isnan(ind))=[];
-                
-                psf_t = squeeze(PSF_shiftted(:,:,i));
-                S(ind) = S(ind) +  psf_t(flag(:)).*flux(i);
-                
-            end
-            
+            PSF_shifted = imUtil.trans.shift_fft(PSF,DX,DY);
+        end
+
+        Xind = matx+ Xround(i);
+        Yind = maty+ Yround(i);
+
+        flag = ~(Xind <=0 | Yind<=0 | Xind>SizeImageX | Yind>SizeImageY);
+        ind = sub2ind([SizeImageX,SizeImageY],Xind(flag),Yind(flag));
+
+        % ind(isnan(ind))=[]; % ? for what pathological case ind may be NaN?
+
+        psf_t = squeeze(PSF_shifted(:,:,i));
+        S(ind) = S(ind) +  psf_t(flag(:)).*flux(i);
+
     end
-    
+
 end
-
-
 
 
