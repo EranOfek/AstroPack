0ad187340821b470600ac2f881ad0987d3d3baea
The LAST Ocs <>
2023-07-17 12:24:59 +0000

    fixed automounts

--- matlab/image/+pipeline/@DemonLAST/unitTest.m
+++ matlab/image/+pipeline/@DemonLAST/unitTest.m
@@ -3,7 +3,7 @@ function Result = unitTest()
     
     D = pipeline.DemonLAST;
     
-    D.BasePath  = '/last08e/data2/archive/LAST.01.08.01/';
+    D.BasePath  = '/last10e/data2/archive/LAST.01.10.02/';
 %     D.CamNumber = 2;
 %     D.StartJD   = [20 4 2023];
 %     D.EndJD     = [27 4 2023];
@@ -11,7 +11,7 @@ function Result = unitTest()
 %     D.main('RegenCalib',true);
     
     D.main('Insert2DB',true,'DB_Table_Raw','test_raw_images','DB_Table_Proc','test_proc_images',...
-            'DB_Table_Coadd','test_coadd_images', 'DB_ImageBulk', true)
+            'DB_Table_Coadd','test_coadd_images', 'DB_ImageBulk', false)
 
     Result = 0;
     
