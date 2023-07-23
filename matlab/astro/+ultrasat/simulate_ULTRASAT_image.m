7d1460e47c772de40954b923534a3c05c4574bc5
A.M. Krassilchtchikov <agioffe@gmail.com>
2023-07-23 20:09:52 +0300

    bug fixes

--- matlab/astro/+ultrasat/simulate_ULTRASAT_image.m
+++ matlab/astro/+ultrasat/simulate_ULTRASAT_image.m
@@ -9,26 +9,24 @@ function simImage = simulate_ULTRASAT_image (Args)
     
     arguments    
         Args.Size       = 0.5;            % [deg] size of the modelled FOV 
+        Args.X0         = 1e-6;           % [deg] the lower left corner of the modelled square region
+        Args.Y0         = 1e-6;           % [deg] the lower left corner of the modelled square region
         Args.ExpNum     = 3;              % number of standard 300 s exposures
         Args.Same       = 0;              % read in a source distribution or generate a random new one
-        Args.Distfile   = '/../data/ULTRASAT/fitted_distr8.44deg_cat.mat'; % if Same=1, read the input distr. from this file
+        Args.Distfile   = 'fitted_distr_cat.mat'; % if Same=1, read the input distr. from this file
         Args.OutDir     =  '.'  ;         % output directory
     end
     
     %%%%% ULTRASAT parameters
     
-    ImageSizeX = 4738;
-    ImageSizeY = 4738;
+%     ImageSizeX = 4738;
+%     ImageSizeY = 4738;
     PixSize    = 5.44; % pixel size (arcsec)
     
-    STile = PixSize^2 * ImageSizeX * ImageSizeY / (3600^2); % tile size in [deg]
+%     STile = PixSize^2 * ImageSizeX * ImageSizeY / (3600^2); % tile size in [deg]
     
-    Nwave   =  9001; %          
-    MinWave =  2000; % [A] the band boundaries
-    MaxWave = 11000; % [A]
-    Wave    = linspace(MinWave,MaxWave,Nwave);
-            
-            
+    Wave       = 2000:11000; % the wavelength band in A
+                     
     if ~Args.Same % model a new distribution
         
         MagL = 13; MagH = 26; Delta_m = 0.2; % the distribution grid in NUV Mag (from GALEX)       
@@ -48,61 +46,50 @@ function simImage = simulate_ULTRASAT_image (Args)
         
         NumSrc = sum(SrcDist,'all');
         
-        Cat   = zeros(NumSrc,2);
-        MagUS = zeros(NumSrc,1);
-        IndR  = zeros(NumSrc,1);
-        Filt  = repmat({ },1,NumSrc);
-        
+        Cat    = zeros(NumSrc,2);
+        MagUS  = zeros(NumSrc,1);
+        FiltUS = repmat({ },1,NumSrc);
+%         
+%         S(1,:) = AstroSpec.blackBody(Wave',3500).Flux;
+%         S(2,:) = AstroSpec.blackBody(Wave',5800).Flux;
+%         S(3,:) = AstroSpec.blackBody(Wave',20000).Flux;
+%         
         S(1) = AstroSpec.blackBody(Wave',3500);
         S(2) = AstroSpec.blackBody(Wave',5800);
         S(3) = AstroSpec.blackBody(Wave',20000);
         
-        io.files.load1('/home/sasha/magu.mat');
+        % read the relation of NUV magnitudes and ULTRASAT magnitude for a
+        % give set of source spectra (usually, just the 3 BB temperatures)  
+        io.files.load1('/home/sasha/magu.mat'); % variables: MagU (3D), Temp, MagNUV, Rad  
+             
+        X0 = ceil(Args.X0 * 3600 / PixSize); % left corner of the modelled square region
+        Y0 = ceil(Args.Y0 * 3600 / PixSize); % left corner of the modelled square region        
+        Range = Args.Size * 3600 / PixSize;  % X and Y size of the modelled square region
+        rng('shuffle');                      % reseed the random number generator
+        Cat(:,1) = X0 + Range * rand(NumSrc,1);
+        rng('shuffle');                      % reseed the random number generator
+        Cat(:,2) = Y0 + Range * rand(NumSrc,1); 
         
-        Isrc = 0;
-        
-        for iMag = 1:1:MagBins
-            
-            rng('shuffle');   % reseed the random number generator
+        Isrc = 0; 
+        for iMag = 1:1:MagBins 
             
             for jSrc = 1:1:SrcDist(iMag)
                 
-                Isrc = Isrc +1;
+                Isrc = Isrc + 1;
                 
-                % Cat(Isrc,1) = 1800 + 333 * rand ; % about 4.22 deg from the corner
-                % Cat(Isrc,2) = 1800 + 333 * rand ; % hence using ULTRASAT R11 filter
-                
-                % Cat(Isrc,1) =   30 + 333 * rand ; % about 0.42 deg from the corner
-                % Cat(Isrc,2) =   30 + 333 * rand ; % hence using ULTRASAT R2 filter
-                %
-                % Cat(Isrc,1) = 3783 + 333 * rand; % about 8.44 deg from the corner
-                % Cat(Isrc,2) = 3783 + 333 * rand; % hence using ULTRASAT R21 filter
-                
-                % NB: the range of pixel positions should comply to the Args.Size:
-                Range = Args.Size * 3600 / PixSize;
-                Cat(Isrc,1) = 1 + Range * rand; 
-                Cat(Isrc,2) = 1 + Range * rand; 
+%                 Cat(Isrc,1) = X0 + Range * rand; 
+%                 Cat(Isrc,2) = Y0 + Range * rand; 
                 
                 RadSrc = sqrt( Cat(Isrc,1)^2 + Cat(Isrc,2)^2 ) * (PixSize/3600); %deg
-                [~, IndR(Isrc)] = min( abs(RadSrc - Rad) ); % search for the nearest node
+                [~, IndR] = min( abs(RadSrc - Rad) ); % search for the nearest node
+                RXX = sprintf('R%d',IndR); FiltUS{Isrc} = RXX; % fill in the appropriate filter
                 
-                RXX        = sprintf('R%d',IndR(Isrc));
-                Filt{Isrc} = RXX;
+                % divide the population into 3 colours: 
+                IndT = rem(Isrc,3) + 1; Spec(Isrc,:) = S(IndT);
                 
-                % divide the population into 3 colours
+                [~, IndM] = min( abs( Mag(iMag) - MagNUV ) ); % search for the nearest magnitude (from the magu.mat grid)
                 
-                IndT = rem(Isrc,3) + 1;
-                
-                Spec(Isrc,:) = S( IndT );
-                
-                % recalculate the magnitudes into the ULTRASAT R system
-                
-                %                         Sp         = scaleSynphot(Spec(Isrc,:), Mag(iMag), 'GALEX', 'NUV');
-                %                         MagU(Isrc) = astro.spec.synthetic_phot([Wave', Sp.Flux],'ULTRASAT','R21','AB');
-                
-                [~, IndM] = min( abs( Mag(iMag) - MagNUV ) ); % search for the nearest
-                
-                MagUS(Isrc) = MagU( IndT, IndM, IndR(Isrc) );
+                MagUS(Isrc) = MagU( IndT, IndM, IndR );
                 
             end
             
@@ -110,17 +97,16 @@ function simImage = simulate_ULTRASAT_image (Args)
         
         % save the catalog for the case you wish to repeat the same setup
         
-        save('fitted_distr_cat.mat', 'Cat','MagUS', 'Spec', 'Filt','-v7.3');
+        save('fitted_distr_cat.mat', 'Cat','MagUS', 'FiltUS', 'Spec', '-v7.3');
         
     else
-        % read in the catalog and spectra to be modelled
-        
+        % read in the catalog, magnitudes, and spectra to be modelled       
         DataFile = sprintf('%s%s',tools.os.getAstroPackPath,Args.Distfile);
-        io.files.load1(DataFile);  % load Cat, MagUS, Spec, Filt
+        io.files.load1(DataFile);  % load Cat, MagUS, FiltUS, Spec,
         
     end
     
-    simImage = ultrasat.usim_dev('InCat',Cat,'MaxNumSrc',10000,'InMag',MagUS,'InSpec',Spec,...
-               'Exposure',[Args.ExpNum 300],'FiltFam',{'ULTRASAT'},'Filt',Filt,'OutDir',Args.OutDir);
+    simImage = ultrasat.usim_dev('InCat',Cat,'MaxNumSrc',10000,'InMag',MagUS,'Filt',FiltUS, ...
+               'Spec',Spec,'Exposure',[Args.ExpNum 300],'FiltFam',{'ULTRASAT'},'OutDir',Args.OutDir);
     
 end
