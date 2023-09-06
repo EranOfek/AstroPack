1bf3294f1f36a0808f75dbb5e19006d071bef298
EranOfek <ofek.eran@gmail.com>
2023-09-06 18:54:43 +0300

    mextractor

--- matlab/image/+imProc/+sources/mextractor.m
+++ matlab/image/+imProc/+sources/mextractor.m
@@ -11,6 +11,7 @@ function Result=mextractor(Obj, Args)
         Args.PrelimCleanSrc logical  = true;
         Args.PrelimCleanSrcArgs cell = {'ColSN_sharp',1, 'ColSN_psf',2, 'SNdiff',0, 'MinEdgeDist',15, 'RemoveBadSources',true};
 
+        Args.backgroundArgs cell     = {};
         Args.ReBack logical          = false; % remeasure if background exits 
         Args.CreateNewObj logical    = false;
     end
@@ -25,27 +26,32 @@ function Result=mextractor(Obj, Args)
     Nobj = numel(Obj);
     for Iobj=1:1:Nobj
         % measure background/variance if needed
-        if Args.ReBack || ~all(Obj(Iobj).isemptyImage({'Back','Var'}))
+        if Args.ReBack || any(Obj(Iobj).isemptyImage({'Back','Var'}))
             % measure background and variance
-            
+            Obj(Iobj) = imProc.background.background(Obj(Iobj), Args.backgroundArgs{:});
 
         end
 
-        % find sources using a prelimnary PSF
-        ResSt = imProc.sources.findSources(Result, 'Threshold',Args.PrelimThreshold,...
-                                                    'Psf',Args.PrelimPsf,...
-                                                    'PsfArgs',Args.PrelimPsfArgs,...
-                                                    'ForcedList',[],...
-                                                    'OnlyForced',false,...
-                                                    'Conn',Args.Conn,...
-                                                    'CleanSources',Args.PrelimCleanSrc,...
-                                                    'cleanSourcesArgs',Args.PrelimCleanSrcArgs);
+        if Obj(Iobj).PSFData.isemptyPSF
+            % NO PSF - attempt to measure
+        
+            % find sources using a prelimnary PSF
+            ResSt = imUtil.sources.findSources(Result(Iobj), 'Threshold',Args.PrelimThreshold,...
+                                                        'Psf',Args.PrelimPsf,...
+                                                        'PsfArgs',Args.PrelimPsfArgs,...
+                                                        'ForcedList',[],...
+                                                        'OnlyForced',false,...
+                                                        'Conn',Args.Conn,...
+                                                        'CleanSources',Args.PrelimCleanSrc,...
+                                                        'cleanSourcesArgs',Args.PrelimCleanSrcArgs);
 
 
-        % construct PSF
+            % construct PSF
 
-        % what to do if no PSF?
+            % what to do if no PSF?
 
+        end
+        
         % find sources using updated PSF
 
         % cleaning iteration
