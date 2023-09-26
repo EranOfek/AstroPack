95b414c323ec30f030c75dde7ccabae9138b2e64
A.M. Krassilchtchikov <agioffe@gmail.com>
2023-09-26 19:01:47 +0300

    dereddening added

--- matlab/astro/+ultrasat/simulateKeplerField.m
+++ matlab/astro/+ultrasat/simulateKeplerField.m
@@ -11,10 +11,10 @@ function simImage = simulateKeplerField(Args)
         Args.Ebv     = 0; % the actual E(B-V) in the Kepler field is about 0.4 ?
         Args.Catalog = 'Kepler_ULTRASAT_all.tbl';
         Args.Dir     = '/home/sasha/KeplerField';
+        Args.SNR     = false; % calculate source SNRs with telescope.sn.snr 
     end
     
     Wave    = 2000:11000;   % the wavelength band in A
-    
     % make a grid of BB spectra
     Temp = 2500:250:12000;
     S = repmat(AstroSpec,1,numel(Temp));
@@ -22,10 +22,7 @@ function simImage = simulateKeplerField(Args)
         S(i) = AstroSpec.blackBody(Wave',Temp(i));
     end
     
-    cd(Args.Dir)
-    
-%     SrcList = 'Kepler_ULTRASAT_all.tbl'; % 'Kepler_ULTRASAT_all_10lines.tbl';
-    
+    cd(Args.Dir);
     SrcTab  = readtable(Args.Catalog,'FileType','text');
     
     switch Args.Tile 
@@ -46,26 +43,64 @@ function simImage = simulateKeplerField(Args)
     end
     
     Tab = SrcTab(SrcTab.x_ra > ra1 & SrcTab.x_ra < ra2 & SrcTab.dec > dec1 & SrcTab.dec < dec2,:);
+
+    %%% test: look at the the brightest objects only:
+    %     sortedTable = sortrows(Tab, 'Vmag');
+    %     Tab = sortedTable(1:2000,:);
     
-    % test:
-%     sortedTable = sortrows(Tab, 'Vmag');
-%     Tab = sortedTable(1:2000,:);
-        
-    Cat = [Tab.x_ra Tab.dec];
-    Mag =  Tab.Vmag;
+    Cat  = [Tab.x_ra Tab.dec];
+    Mag0 =  Tab.Vmag;
+    
+    % account for extinction (the simulator deals with dereddened values!)
+    Filt = AstFilter.get('Johnson','V');
+    deltaMag = astro.spec.extinction(Args.Ebv,Filt.pivot_wl/1e4);
+    Mag = Mag0 - deltaMag;
     
     NSrc    = size(Tab,1);
     Spec    = repmat(AstroSpec,1,NSrc);
     
-    for ISrc = 1:NSrc
-        
-        diff = abs(Tab.Teff(ISrc)-Temp);
+    for ISrc = 1:NSrc        
+        diff = abs(Tab.Teff(ISrc)-Temp);  % find the nearest neighbour in the spectrum grid
         [~, ind] = min(diff);
         Spec(ISrc) = S(ind);
+%         Spec(ISrc)  = AstroSpec.blackBody(Wave',Tab.Teff(ISrc)); % DON't use: this is way to slow and voluminous!        
+    end
         
-%         Spec(ISrc)  = AstroSpec.blackBody(Wave',Tab.Teff(ISrc));
-%         % this is way to slow and voluminous!
+    %%%%%%%%%%%%%%%%%%%% calculate ULTRASAT magnitudes and SNR for the catalog objects from the table:
+    if (Args.SNR)        
+        tic       % very slow! takes  ~ 90 sec for 1000 objects => ~ 100 hrs for 4 x 10(6) obj !
+        UP_db = sprintf('%s%s',tools.os.getAstroPackPath,'/../data/ULTRASAT/P90_UP_test_60_ZP_Var_Cern_21.mat');
+        io.files.load1(UP_db,'UP');
         
+        % parameters for telescope.sn.snr:
+        Exp = 300; % sec
+        NIm = 3;   % number of exposures
+        T   = Tab.Teff;
+        SN  = 5;  % S/N for detection
+        PSFeff = 0.8;
+        
+        % the extinction curve:
+        ExtMag     = astro.spec.extinction(Args.Ebv,(Wave./1e4)');
+        Extinction = 10.^(-0.4.*ExtMag);
+        
+        % set the radial position (in reality it depends on the particular aim point and rotation of the s/c):
+        Irad = 1; 
+        
+        NSrc = 10; % TEST ONLY!
+        
+        % rescale the spectra, account for the absorption:
+        MagU = zeros(NSrc,1);
+        for Isrc = 1:1:NSrc
+            MagSc = astro.spec.synthetic_phot([Spec(Isrc).Wave Spec(Isrc).Flux],'Johnson','V','AB');
+            Factor = 10.^(-0.4.*(MagSc - Tab.Vmag(Isrc)));
+            SpecObs = Spec(Isrc).Flux .* Extinction ./ Factor;
+            MagU(Isrc) = astro.spec.synthetic_phot([Wave' SpecObs],UP.U_AstFilt(Irad),'R1','AB');
+            SNR(Isrc) = telescope.sn.snr('ExpTime',Exp,'Nim',NIm,'TargetSpec',T(Isrc),'PSFeff',PSFeff,'Mag',MagU(Isrc),'CalibFilterFamily',UP.U_AstFilt(Irad),...
+                'CalibFilter','','Wave',Wave','SN',SN,'FWHM',UP.EffPSF(Irad));
+        end
+        
+%         fprintf('%s%10.2f\n','SNR: ',SNR.SNR);
+        toc
     end
     
     %%%% run the simulation 
