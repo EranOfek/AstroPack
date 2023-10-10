9bc7df505edeb5c0b7db4a45eb45aa12d302b0fa
EranOfek <ofek.eran@gmail.com>
2023-10-10 17:49:18 +0300

    add value at position in findSources

--- matlab/image/+imProc/+sources/mextractor.m
+++ matlab/image/+imProc/+sources/mextractor.m
@@ -4,6 +4,17 @@ function Result=mextractor(Obj, Args)
     arguments
         Obj AstroImage
 
+        % back,var,PSF finding
+        Args.populatePSFArgs cell      = {};
+        Args.ThresholdPSF              = 20;
+        Args.RangeSN                   = [50 1000];
+        Args.InitPsf                   = @imUtil.kernel2.gauss
+        Args.InitPsfArgs cell          = {[0.1;2]};
+        
+        % PSF fit
+        Args.Threshold                 = [50 16.5 5]; % This also specifies the # of iterations
+        Args.ReMeasBack logical        = true;
+        
         Args.PrelimPsf               = @imUtil.kernel2.gauss;
         Args.PrelimPsfArgs cell      = {[0.1 2]};
         Args.PrelimThreshold         = 30;
@@ -23,6 +34,54 @@ function Result=mextractor(Obj, Args)
         Result = Obj;
     end
 
+    
+    % populatePSF will:
+    %   1. populate background
+    %   2. populate variance
+    %   3. populate PSF
+    [Result, ResPSF] = imProc.psf.populatePSF(Result, Args.populatePSFArgs{:},...
+                                                      'ThresholdPSF',Args.ThresholdPSF,...
+                                                      'RangeSN',Args.RangeSN,...
+                                                      'InitPsf',Args.InitPsf,...
+                                                      'InitPsfArgs',Args.InitPsfArgs);
+        
+    % find sources using PSF - multi teration
+    Niter = numel(Args.Threshold);
+    Nobj = numel(Obj);
+    for Iobj=1:1:Nobj
+        SubImage = Result(Iobj).Image;
+        for Iiter=1:1:Niter
+            % re-measure background
+            if Iiter>1 && Args.ReMeasBack
+
+            end
+
+            % find sources
+            ResSrc(Iobj,Iiter) = imUtul.sources.findSources(SubImage, 'Threshold',Args.Threshold(Iiter),...
+                                                                      'Psf',Result(Iobj).PSFData.getPSF,...
+                                                                      'BackIm',Result(Iobj).Back,...
+                                                                      'VarIm',Result(Iobj).Var,...
+                                                                      'CleanSources',flase);
+            % Clean sources
+                                                                  
+   
+        
+            % PSF fit sources
+
+            % Add sources to list
+
+            % subtract suorces
+
+        end
+    end
+    
+    % Find diffraction spikes
+    
+    % Cleaning
+    
+    
+    
+    
     Nobj = numel(Obj);
     for Iobj=1:1:Nobj
         % measure background/variance if needed
