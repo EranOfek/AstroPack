272cef0282bf89300176fd49bf7a2b8fd7250cde
A.M. Krassilchtchikov <agioffe@gmail.com>
2023-11-29 17:46:50 +0200

    *.m

--- matlab/image/+pipeline/+generic/multiRaw2procCoadd.m
+++ matlab/image/+pipeline/+generic/multiRaw2procCoadd.m
@@ -265,7 +265,7 @@ function [AllSI, MergedCat, MatchedS, Coadd, ResultSubIm, ResultAsteroids, Resul
         % PSF phot for single images
         Args.SingleImAddPSF logical                   = true;
         Args.SingleImconstructPSFArgs cell            = {}; % {'CropByQuantile',true,'Quantile',0.999};
-        Args.SingleImPsfPhot logical                  = true;
+        Args.SingleImPsfPhot logical                  = true;   
 
         % save products
         Args.SaveAll               = [];  % empty - check individuals
