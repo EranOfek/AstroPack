0466ad547f74412cbab4c922ebe3a90cf3c540bb
EranOfek <ofek.eran@gmail.com>
2023-11-30 17:14:55 +0200

    Convert spectrum + transmission to AB magnitude

--- matlab/astro/+astro/+mag/transmissionMeanWave.m
+++ matlab/astro/+astro/+mag/transmissionMeanWave.m
@@ -1,21 +1,19 @@
-function Result=transmissionEffWave(Lambda, Response, Flambda, Dim)
-    % Calculate the effective wavelength of a transmission function
+function Result=transmissionMeanWave(Lambda, Response, Dim)
+    % Calculate the mean wavelength of a transmission function
     % Input  : - Array of wavelengths.
     %          - Array of transmissions.
-    %          - Array of F_lambda. Default is 1.
     %          - Dimension over which to perform the integration.
     % Output : - Mean wavelength for each transmission.
     % Author : Eran Ofek (Oct 2023)
     % Reference: See Bohlin et al. 2014 for definitions
-    % Example: astro.spec.transmissionEffWave((1:1:10)',rand(10,3))
+    % Example: astro.mag.transmissionMeanWave((1:1:10)',rand(10,3))
     
     arguments
         Lambda
         Response
-        Flambda   = 1;
         Dim       = 1;
     end
     
-    Result = trapz(Lambda, Flambda.*Lambda.^2.*Response, Dim)./trapz(Lambda, Flambda.*Lambda.*Response, Dim);
+    Result = trapz(Lambda, Lambda.*Response, Dim)./trapz(Lambda, Response, Dim);
     
 end
