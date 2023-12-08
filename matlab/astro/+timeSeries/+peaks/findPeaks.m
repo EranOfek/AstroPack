763c4cbc2b84c75424e574f9001e46f667d0581f
EranOfek <ofek.eran@gmail.com>
2023-12-08 18:39:56 +0200

    timeSeries.filter.filterStd

--- matlab/astro/+timeSeries/+peaks/findPeaks.m
+++ matlab/astro/+timeSeries/+peaks/findPeaks.m
@@ -1,4 +1,4 @@
-function [Result] = findLines(Data, Args)
+function [Result] = findPeaks(Data, Args)
     % One line description
     %     Optional detailed description
     % Input  : - 
@@ -10,7 +10,7 @@ function [Result] = findLines(Data, Args)
     %          
     %          AS=AstroSpec.getSkyArcsSpecLines;
     %          Data = AS(1).Flux;
-    %          imUtil.spec.lines.findLines(Data)
+    %          timeSeries.peaks.findPeaks(Data)
 
     arguments
         Data
@@ -21,9 +21,7 @@ function [Result] = findLines(Data, Args)
         Args.StdFilterGap      = [10 20];
         Args.Threshold         = 10;
     end
-    
-    error('not working')
-    
+        
     % make sure that the wavelengh is in the first dim
     if Args.DimWave==2
         Data = Data.';
@@ -35,24 +33,18 @@ function [Result] = findLines(Data, Args)
     else
         Filter = Args.Filter(:);
     end
+    Filter = Filter./sum(Filter);
+    Norm   = sqrt(sum(Filter.^2));
     
     Nwave = size(Data,1);
     Filter = fftshift(Filter);  % move to begining to avoid shift
     FiltData = (ifft(fft(Data,[],1).*conj(fft(Filter,Nwave,1))));
 
-    % Std filter around each poistion
-    HalfSizeFilterStd = max(Args.StdFilterGap);
-    Xstd              = (-HalfSizeFilterStd:1:HalfSizeFilterStd).';
-    StdFilt           = zeros(size(Xstd));
-    Flag1             = abs(Xstd)>min(Args.StdFilterGap) & abs(Xstd)<=max(Args.StdFilterGap);
-    StdFilt(Flag1)    = 1;
-    StdFilt           = StdFilt./sum(StdFilt);
-    StdFilt           = fftshift(StdFilt);
+    StdF = timeSeries.filter.filterStd(Data);
     
-    FiltStdX2 = (ifft(fft(Data.^2,[],1).*conj(fft(StdFilt,Nwave,1))));
-    FiltStdX1 = (ifft(fft(Data,[],1).*conj(fft(StdFilt,Nwave,1))));
-    FiltStd   = FiltStdX2 - FiltStdX1.^2;
-    Stat      = FiltData./sqrt(FiltStd);
+    
+    Stat      = FiltData./StdF;
+    error('not working')
     
     Result.Max  = find(Stat>Args.Threshold);
     Result.MaxS = Stat(Result.Max);
