49f23209a8a9c23b686f3c67cdd6068649eaee26
EranOfek <ofek.eran@gmail.com>
2023-12-20 22:17:38 +0200

    moonlight help update

--- matlab/astro/+celestial/+SolarSys/moonlight.m
+++ matlab/astro/+celestial/+SolarSys/moonlight.m
@@ -20,26 +20,35 @@ RAD = 180./pi;
 
 Illum = zeros(size(Alt));
 
+Nalt  = numel(Alt);
+Nhp   = numel(HP);
+Nelon = numel(Elon);
 
-for J=1:1:length(Alt(:,1))
-   X = Alt(J).*RAD./90;
+N = max(max(Nalt, Nhp), Nelon);
+
+for J=1:1:N
+    Ialt = min(J, Nalt);
+    Ihp  = min(J, Nhp);
+    Ielon = min(J, Nelon);
+    
+    X = Alt(Ialt).*RAD./90;
  
    % Handle Altitude
-   if (Alt(J).*RAD>20)
+   if (Alt(Ialt).*RAD>20)
       L0 = -1.95;
       L1 = 4.06;
       L2 = -4.24;
       L3 = 1.56;
       Error = 0.02;
       LI1 = L0 + L1.*X + L2.*X.*X + L3.*X.*X.*X;
-   elseif (Alt(J).*RAD<=20 && Alt(J).*RAD>5)
+   elseif (Alt(Ialt).*RAD<=20 && Alt(J).*RAD>5)
       L0 = -2.58;
       L1 = 12.58;
       L2 = -42.58;
       L3 = 59.06;
       Error = 0.03;
       LI1 = L0 + L1.*X + L2.*X.*X + L3.*X.*X.*X;
-   elseif (Alt(J).*RAD<=5 && Alt(J).*RAD>-0.8)
+   elseif (Alt(Ialt).*RAD<=5 && Alt(J).*RAD>-0.8)
       L0 = -2.79;
       L1 = 24.27;
       L2 = -252.95;
@@ -52,11 +61,11 @@ for J=1:1:length(Alt(:,1))
    end
 
    % Handle Elongation
-   f = 180 - Elon(J).*RAD;
+   f = 180 - Elon(Ielon).*RAD;
    LI2 = -8.68e-3.*f - 2.2e-9.*f.^4;
 
    % Handle Parallax
-   LI3 = 2.*log10(HP(J).*RAD./0.951);
+   LI3 = 2.*log10(HP(Ihp).*RAD./0.951);
 
    Illum(J) = 10.^(LI1+LI2+LI3);
 end
