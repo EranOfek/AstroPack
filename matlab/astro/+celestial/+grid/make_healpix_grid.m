2e36af22829a08db95f7d10ca8b19ff247338072
A.M. Krassilchtchikov <agioffe@gmail.com>
2023-12-21 13:08:20 +0200

    *.m

--- matlab/astro/+celestial/+grid/make_healpix_grid.m
+++ matlab/astro/+celestial/+grid/make_healpix_grid.m
@@ -1,6 +1,11 @@
 function make_healpix_grid(Npix, Args)
-    %
-    
+    %  make a grid of HEALPix positions covering the whole sky
+    % Input: - Npix parameter of the HEALPix algorythm (effective map resolution)
+    %      * ...,key,val,...
+    %      'Python' - system command for the python3 executable
+    % Output: - a text file containing the grid points in Lon, Lat
+    % Author: A.M. Krassilchtchikov (Dec 2023)
+    % Example: celestial.grid.make_healpix_grid(4)
     arguments
         Npix = 4;
         Args.Python = 'python3';
@@ -15,7 +20,10 @@ function make_healpix_grid(Npix, Args)
     try
         system([Args.Python ' ' Script ' ' N]);
     catch
-        error('Some of the required python libraries may be missing'); 
+        fprintf('Some of the required python libraries may be missing.\n'); 
+        fprintf('You can install them in the following way:\n'); 
+        fprintf('> pip install healpy \n'); 
+        return
     end
 
 end
