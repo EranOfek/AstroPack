ee68657ec64430f52f3628e280ef3664d1d04aa6
EranOfek <ofek.eran@gmail.com>
2024-01-11 13:30:04 +0200

    invRefraction ...

---
+++ matlab/astro/+celestial/+coo/invRefraction.m
@@ -0,0 +1,34 @@
+function [AltGeometrical] = invRefraction(AltRefracted, Args)
+    % One line description
+    %     Optional detailed description
+    % Input  : - 
+    %          - 
+    %          * ...,key,val,... 
+    % Output : - 
+    % Author : Eran Ofek (2024 Jan) 
+    % Example: [AltGeometrical] = celestial.coo.invRefraction(30./RAD)
+
+    arguments
+        AltRefracted
+        Args.InUnits           = 'rad';
+        Args.OutUnits          = 'rad';
+        
+        Args.Lam               = 5000;
+        Args.T                 = 15;
+        Args.P                 = 760;
+        Args.Pw                = 8;
+    end
+
+    AltRefractedRad = convert.angular(Args.InUnits, 'rad', AltRefracted);
+    
+    [RefAng] = celestial.coo.refraction_wave(AltRefractedRad, Args.Lam, Args.T, Args.P, Args.Pw);
+    
+    for I=1:1:10
+        AltRefractedRad1 =  AltRefractedRad + RefAng;
+        [RefAng] = celestial.coo.refraction_wave(AltRefractedRad1, Args.Lam, Args.T, Args.P, Args.Pw);
+        AltRefractedRad2 = AltRefractedRad1 -RefAng;
+        [AltRefractedRad2 - AltRefracted]
+        
+    end
+    AltGeometrical = 1;
+end
