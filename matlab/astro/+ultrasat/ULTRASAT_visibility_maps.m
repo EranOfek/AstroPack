e4b1ee581f596c5c3bd1068718fd69a99578d455
A.M. Krassilchtchikov <agioffe@gmail.com>
2024-01-16 14:44:45 +0200

    improved figures

--- matlab/astro/+ultrasat/ULTRASAT_visibility_maps.m
+++ matlab/astro/+ultrasat/ULTRASAT_visibility_maps.m
@@ -1,14 +1,28 @@
 function ULTRASAT_visibility_maps(Args)
     % calculate and plot uninterrupted ULTRASAT visibilities for the whole sky
-    
+    % Input: -
+    %         * ...,key,val,...
+    %        'GridFile' - the primary sky grid as [RA, Dec] (equatorial) to used for all the maps
+    %        'AllSky'   - a grid of all-sky ULTRSAT pointings as [RA, Dec] (equatorial) to overlay on the maps
+    %        'StartDate' - the start time of the period of interest
+    %        'NumDays'   - the number of days in the period of interest
+    %        'TimeBin'   - time bin [days]: the function checks uninterrupted visibility for consequent bins of this size
+    %        'SaveMat'   - whether the visibilities are saved in a .mat object
+    %        'LimitingAlambda' - the limiting upper value of A_lambda to cut the maps  
+    % Output: - the function produces multiple plots of ULTRASAT visibility
+    %           maps with various sets of limits applied (see the LimitType
+    %           list + extinction limits)
+    % Author: A.M. Krassilchtchikov (Jan 2024)
+    % Example: ULTRASAT_visibility_maps('LimitingAlambda',0.5,'SaveMat',1);   
     arguments
         Args.GridFile = '~/matlab/data/ULTRASAT/healpix_grid_nside_64_npix_49152_pixarea_0.839_deg.txt' 
         % 'healpix_grid_nside_32_npix_12288_pixarea_3.357_deg.txt'; 'healpix_grid_nside_64_npix_49152_pixarea_0.839_deg.txt';
         Args.AllSky   = '~/matlab/data/ULTRASAT/charged_particles_350_rep1.txt'; 
         Args.StartDate = '2027-01-01 00:00:00';
-        Args.NumDays   = 540;  % [days]
+        Args.NumDays   = 1080; % [days]
         Args.TimeBin   = 0.01; % [days] 0.01 day = 864 s ~ 3 x 300 s  
         Args.SaveMat logical = false; 
+        Args.LimitingAlambda = 1; % the limiting value of A_lambda (for the ULTRASAT band)
     end
     
     RAD  = 180/pi;
@@ -101,10 +115,10 @@ function ULTRASAT_visibility_maps(Args)
     Averaged_extinction = celestial.grid.statSkyGrid('SkyPos',[lambda beta]);
     
     % exclude points where the extinction is above 1 
-    ExtinctionFlag = ones(Np,1); ExtinctionFlag( Alam > 1 ) = Tiny;  
+    ExtinctionFlag = ones(Np,1); ExtinctionFlag( Alam > Args.LimitingAlambda ) = Tiny;  
     MaxLen.CombinedLocalExtinct = MaxLen.Combined .* ExtinctionFlag;
     
-    ExtinctionFlag = ones(Np,1); ExtinctionFlag( Averaged_extinction > 1 ) = Tiny;  
+    ExtinctionFlag = ones(Np,1); ExtinctionFlag( Averaged_extinction > Args.LimitingAlambda ) = Tiny;  
     MaxLen.CombinedAverExtinct = MaxLen.Combined .* ExtinctionFlag;
     
                 %%%%%%% PLOTTING BLOCK
@@ -125,21 +139,39 @@ function ULTRASAT_visibility_maps(Args)
                 hold on; plot(lam0*RAD,bet0*RAD,'ro', 'MarkerSize', 10); hold off
                 xlabel '\lambda, deg'; ylabel '\beta, deg'; title 'Combined + Extinction: max(uninterrupted days)'; 
                 saveas(gcf, 'CombinedLimitsAverExtinctionEcl.jpg');
+                
+                plot.ungridded_image(RA, Dec, Alam); caxis([0, 1]);
+                hold on; plot(RA0*RAD,Dec0*RAD,'ro', 'MarkerSize', 10); hold off
+                xlabel 'RA, deg'; ylabel 'Dec, deg'; title 'Extinction (A_\lambda)'; 
+                saveas(gcf, 'AlamJ2000.jpg');
+                plot.ungridded_image(lambda, beta, Alam); caxis([0, 1]);
+                hold on; plot(lam0*RAD,bet0*RAD,'ro', 'MarkerSize', 10); hold off
+                xlabel '\lambda, deg'; ylabel '\beta, deg'; title 'Extinction (A_\lambda)'; 
+                saveas(gcf, 'AlamEcl.jpg');
+                
+                plot.ungridded_image(RA, Dec, Averaged_extinction); caxis([0, 1]);
+                hold on; plot(RA0*RAD,Dec0*RAD,'ro', 'MarkerSize', 10); hold off
+                xlabel 'RA, deg'; ylabel 'Dec, deg'; title 'Averaged Extinction (A_\lambda)'; 
+                saveas(gcf, 'Averaged_extinctionJ2000.jpg');
+                plot.ungridded_image(lambda, beta, Averaged_extinction); caxis([0, 1]);
+                hold on; plot(lam0*RAD,bet0*RAD,'ro', 'MarkerSize', 10); hold off
+                xlabel '\lambda, deg'; ylabel '\beta, deg'; title 'Averaged Extinction (A_\lambda)'; 
+                saveas(gcf, 'Averaged_extinctionEcl.jpg');
     
     % make a table of HCS-approved positions and read in the all-sky pointing map:
     AllSky = readtable(Args.AllSky);
     
     Tab = table(Grid(:,1),Grid(:,2),MaxLen.CombinedAverExtinct,'VariableNames', {'RA', 'Dec', 'MaxLen'});
-    Tab100 = Tab(Tab.MaxLen > 100,:);
+    Tab180 = Tab(Tab.MaxLen > 180,:);
     Tab360 = Tab(Tab.MaxLen > 360,:);
     
                 figure(1); clf; hold on
-                plot(Tab100.RA,Tab100.Dec,'*','Color','blue')
+                plot(Tab180.RA,Tab180.Dec,'*','Color','blue')
                 plot(Tab360.RA,Tab360.Dec,'o','Color','green')        
                 plot(AllSky.Var1,AllSky.Var2,'*','Color','red')
                 hold off    
                 xlabel 'RA, deg'; ylabel 'Dec, deg';
-                title 'HCS constraints, Blue > 100 days, Green > 365 days';
+                title 'HCS constraints, Blue > 180 days, Green > 360 days';
                 
     % save the MaxLen structure and the equatorial grid in a matlab object
     if Args.SaveMat
