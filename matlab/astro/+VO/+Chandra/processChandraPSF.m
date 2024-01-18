c9377c6809be2a79ef9a9d01dbc43ec2c9b7504f
A.M. Krassilchtchikov <agioffe@gmail.com>
2024-01-18 12:56:09 +0200

    matching to the actual format of MARX simulation by MS

--- matlab/astro/+VO/+Chandra/processChandraPSF.m
+++ matlab/astro/+VO/+Chandra/processChandraPSF.m
@@ -4,9 +4,9 @@ function processChandraPSF(Args)
     arguments
         Args.WDir   = '~/ChandraPSF/';
         Args.ChipID = 0;
-        Args.Energy = [0.2 0.36986 1.2649 4.3259 8.0];     
-        Args.ChipX  = linspace(1,1024,16);
-        Args.ChipY  = linspace(1,1024,16);
+        Args.Energy = [0.4, 1.3, 4.3]; % [0.2 0.36986 1.2649 4.3259 8.0];     
+        Args.ChipX  = linspace(32,992,16);
+        Args.ChipY  = linspace(32,992,16);
         Args.StampSize = [196 196];
     end
     
@@ -26,20 +26,15 @@ function processChandraPSF(Args)
     
     MPSF = zeros(StampNx, StampNx, Ne, Nx, Ny, 'single'); % 196 * 196 * 5 * 16 * 16 * 4 / 1024^2 = 188 Mb
     
-    % read the data
-    Dir = sprintf('ccd%d',Args.ChipID);
-    cd(Dir)
-    
+    % read the data    
     for Ien = 1:Ne
         for Ix = 1:Nx
             for Iy = 1:Ny
-                if Args.ChipID < 4 % ACIS-I
-%                     FileName = sprintf('18073_%d%de%d_ccd%d.psf',ChipX(Ix),ChipY(Iy),Energy(Ien),Args.ChipID); 
-                else % ACIS-S
-%                     FileName = sprintf('13659_%d%de%d_ccd%d.psf',ChipX(Ix),ChipY(Iy),Energy(Ien),Args.ChipID); 
-                end
-                FileName = '18073_ra149.1587_dec69.5214_e1.3_ccd0.0.psf';
-                Stamp = FITS.read1(FileName);
+                
+                FileName = getSimulatedFileName(Args.WDir,TabX(Ix),TabY(Iy),TabEn(Ien),Args.ChipID);                
+                
+                FN = sprintf('%s/ccd%d/%s',Args.WDir,Args.ChipID,FileName);
+                Stamp = FITS.read1(FN);
                 % pad if needed:
                 if size(Stamp,1) < StampNx || size(Stamp,2) < StampNy
                     Stamp = imUtil.psf.pad_psf(Stamp,[StampNx StampNy]);
@@ -48,10 +43,22 @@ function processChandraPSF(Args)
             end
         end
     end
-     
-    cd(Args.WDir);
-     
-    Matname = sprintf('ChandraACISchip%dPSF.mat',Args.ChipID);
-    save(Matname,'MPSF','StampX','StampY','TabEn','TabX','TabY');
     
+    % save the stamps in a .mat object
+    Matname = sprintf('ChandraACISchip%dPSF.mat',Args.ChipID);
+    save(Matname,'MPSF','StampX','StampY','TabEn','TabX','TabY');    
+end
+
+function FileName = getSimulatedFileName(WDir,ChipX,ChipY,Energy,ChipID)
+    % read the name conversion tables and construct the data file name
+ 
+    if ChipID < 4 % ACIS-I
+        T = readtable(strcat(WDir,'/Table_coord_acis_I_wChunk.txt'));  
+        ObsId = 18073;
+    else          % ACIS-S
+        T = readtable(strcat(WDir,'/Table_coord_acis_S_wChunk.txt'));
+        ObsId = 13659;
+    end    
+    Line = T(T.ccd_ID == ChipID & T.chipx == ChipX & T.chipy == ChipY, :);
+    FileName = sprintf('%d_ra%.4f_dec%.4f_e%.1f_ccd%d.0.psf',ObsId,Line.ra_deg,Line.dec_deg,Energy,ChipID);
 end
