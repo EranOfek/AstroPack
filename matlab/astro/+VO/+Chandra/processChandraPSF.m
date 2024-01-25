6b9efead7f2bc61652625f13c82e2a1109fdaf34
A.M. Krassilchtchikov <agioffe@gmail.com>
2024-01-25 14:24:40 +0200

    FileName templated changes

--- matlab/astro/+VO/+Chandra/processChandraPSF.m
+++ matlab/astro/+VO/+Chandra/processChandraPSF.m
@@ -2,7 +2,7 @@ function processChandraPSF(Args)
     % process the PSF fits files produced with MARX and make a .mat object
     % with all the PSFs for the given chip
     arguments
-        Args.WDir   = '~/ChandraPSF/';
+        Args.WDir   = '~/ChandraPSF/PSF_library_cubicshift/';
         Args.ChipID = 0;
         Args.Energy = [0.4, 1.3, 4.3]; % [0.2 0.36986 1.2649 4.3259 8.0];     
         Args.ChipX  = linspace(32,992,16);
@@ -31,10 +31,18 @@ function processChandraPSF(Args)
         for Ix = 1:Nx
             for Iy = 1:Ny
                 
+                fprintf('E, X, Y: %d %d %d \n', Ien, Ix, Iy);
+                  
                 FileName = getSimulatedFileName(Args.WDir,TabX(Ix),TabY(Iy),TabEn(Ien),Args.ChipID);                
                 
                 FN = sprintf('%s/ccd%d/%s',Args.WDir,Args.ChipID,FileName);
-                Stamp = FITS.read1(FN);
+              
+                try
+%                     Stamp = FITS.read1(FN);                    
+                    Stamp = readmatrix(FN);
+                catch
+                    fprintf('%s cannot be read\n', FN);
+                end
                 % pad if needed:
                 if size(Stamp,1) < StampNx || size(Stamp,2) < StampNy
                     Stamp = imUtil.psf.pad_psf(Stamp,[StampNx StampNy]);
@@ -60,5 +68,15 @@ function FileName = getSimulatedFileName(WDir,ChipX,ChipY,Energy,ChipID)
         ObsId = 13659;
     end    
     Line = T(T.ccd_ID == ChipID & T.chipx == ChipX & T.chipy == ChipY, :);
-    FileName = sprintf('%d_ra%.4f_dec%.4f_e%.1f_ccd%d.0.psf',ObsId,Line.ra_deg,Line.dec_deg,Energy,ChipID);
+%     FileName = sprintf('%d_ra%.4f_dec%.4f_e%.1f_ccd%d.0.psf',ObsId,Line.ra_deg,Line.dec_deg,Energy,ChipID);
+    % improve the number of digits in RA, Dec:
+    if rem(round(Line.ra_deg*1e4),10) == 0 && rem(round(Line.dec_deg*1e4),10) == 0
+        FileName = sprintf('%d_ra%.3f_dec%.3f_e%.1f_ccd%d.0_95.txt',ObsId,Line.ra_deg,Line.dec_deg,Energy,ChipID);
+    elseif ~ (rem(round(Line.ra_deg*1e4),10) == 0 ) && rem(round(Line.dec_deg*1e4),10) == 0
+        FileName = sprintf('%d_ra%.4f_dec%.3f_e%.1f_ccd%d.0_95.txt',ObsId,Line.ra_deg,Line.dec_deg,Energy,ChipID);
+    elseif rem(round(Line.ra_deg*1e4),10) == 0 && ~ ( rem(round(Line.dec_deg*1e4),10) == 0 )
+        FileName = sprintf('%d_ra%.3f_dec%.4f_e%.1f_ccd%d.0_95.txt',ObsId,Line.ra_deg,Line.dec_deg,Energy,ChipID);
+    else
+        FileName = sprintf('%d_ra%.4f_dec%.4f_e%.1f_ccd%d.0_95.txt',ObsId,Line.ra_deg,Line.dec_deg,Energy,ChipID);
+    end
 end
