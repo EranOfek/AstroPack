53025bd4452371b211fc0b503462f9c6f6cb1685
EranOfek <ofek.eran@gmail.com>
2024-01-28 16:05:00 +0200

    Add FLATFIELD to synonim confi list

--- matlab/image/@AstroDiff/unitTest.m
+++ matlab/image/@AstroDiff/unitTest.m
@@ -11,6 +11,12 @@ function Result=unitTest()
     %AD.New = AstroImage.readFileNamesObj('LAST.01.02.01_20230828.014110.723_clear_358+34_002_001_010_sci_proc_Image_1.fits');
     AD.New = AstroImage.readFileNamesObj('LAST.01.02.01_20230828.014710.841_clear_358+34_020_001_010_sci_proc_Image_1.fits');
 
+    cd /marvin/LAST.01.05.01/2023/12/14/proc/000545v0
+    AD.Ref = AstroImage.readFileNamesObj('LAST.01.05.01_20231215.000535.997_clear_TXS0506+056_000_001_010_sci_coadd_Image_1.fits');
+    
+
+
+
     % Register the Ref image into the New image (New won't change)
     AD.register
     AD.subtractionD
