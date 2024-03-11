b7eef1b6191a7cd5ce4101853045b3ee43b0367d
A.M. Krassilchtchikov <agioffe@gmail.com>
2024-03-11 11:37:41 +0200

    bug fixes

--- matlab/astro/+celestial/+grid/get_healpix_numbers.m
+++ matlab/astro/+celestial/+grid/get_healpix_numbers.m
@@ -1,28 +1,39 @@
 function get_healpix_numbers(NSide, RA, Dec, Args)
     % make a grid of HEALPix positions covering the whole sky
     % Input: - Npix parameter of the HEALPix algorythm (effective map resolution)
+    %        - an array of RA [deg] OR a filename with RA Dec in two columns
+    %        - an array of Dec [deg] (may skip it if RA is a filename)
     %      * ...,key,val,...
     %      'Python' - system command for the python3 executable
-    % Output: - a text file containing the grid points in Lon, Lat
+    %      'Nested' - 'False' (ring structure) or 'True' (nested structure)
+    %      'Lonlat' - 'True' (degrees) or 'False' (radians)
+    % Output: - a text file containing the HEALPIX pixel numbers in a row
     % Author: A.M. Krassilchtchikov (Mar 2024)
     % Example: celestial.grid.make_healpix_grid(4)
     arguments    
         NSide
         RA
-        Dec
+        Dec         =  0;
         Args.Python = 'python3';
+        Args.Nested = 'False';
+        Args.Lonlat = 'True'; 
     end
-    
-    N = sprintf('%d',NSide);
-    RA_array = sprintf('%d',RA);
-    Dec_array = sprintf('%d',Dec);
     
     current_function_path = mfilename('fullpath');
     [current_function_directory, ~, ~] = fileparts(current_function_path);
-    Script = sprintf('%s/make_healpix_grid.py',current_function_directory);
+    Script = sprintf('%s/get_healpix_numbers.py',current_function_directory);
+    
+    Nside = sprintf('%d',NSide);
+    
+    if isnumeric(RA)
+        RA  = sprintf('%f',RA);
+        Dec = sprintf('%f',Dec);
+    else
+        Dec = sprintf('%f',Dec);
+    end
     
     try
-        system([Args.Python ' ' Script ' ' N RA_array Dec_array]);
+        system([Args.Python ' ' Script ' ' Nside ' ' RA ' ' Dec ' ' Args.Nested ' ' Args.Lonlat]);
     catch
         fprintf('Some of the required python libraries may be missing.\n'); 
         fprintf('You can install them in the following way:\n'); 
