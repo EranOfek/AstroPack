d0fbd5f6e353b2942761c02ae91bcecf4baaf9c8
A.M. Krassilchtchikov <agioffe@gmail.com>
2024-03-11 09:30:44 +0200

    *.py

--- matlab/astro/+celestial/+grid/get_healpix_numbers.py
+++ matlab/astro/+celestial/+grid/get_healpix_numbers.py
@@ -1,6 +1,12 @@
 import sys
 import healpy as hp
 
-pixel_numbers = hp.pixelfunc.ang2pix(sys.argv[1], sys.argv[2], sys.argv[3], nest = False, lonlat = True)
+nside = int(sys.argv[1])
+RA    = float(sys.argv[2])
+Dec   = float(sys.argv[3])
+
+# print("Input:", nside, RA, Dec)
+
+pixel_numbers = hp.pixelfunc.ang2pix(nside, RA, Dec, nest = False, lonlat = True)
 print("Nearest pixel numbers:", pixel_numbers)
 
