e4a7af26f675deb49ae84d5e6affbe7007d10d56
Chen Tishler <chen@tishler.dev>
2024-04-26 11:44:07 +0300

    mex update_matrix - still under testing

---
+++ matlab/util/+tools/+array/+mex/mex_update_matrix_inplace_double.cpp
@@ -0,0 +1,7 @@
+// $Auto generated file
+
+typedef double __Type;
+#define MEX_TYPE  mxDOUBLE_CLASS
+#define MEX_UTYPE mxDOUBLE_CLASS
+
+#include "mex_update_matrix_inplace_include.cpp"                            
