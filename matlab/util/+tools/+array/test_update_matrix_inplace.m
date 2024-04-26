b55eb1a512fdeebd7170765933488520990acefe
Chen Tishler <chen@tishler.dev>
2024-04-26 12:01:53 +0300

    testing

--- matlab/util/+tools/+array/test_update_matrix_inplace.m
+++ matlab/util/+tools/+array/test_update_matrix_inplace.m
@@ -1,11 +1,78 @@
 function test_update_matrix_inplace()
     % Test & benchmark
 
-    unittest_update_matrix_inplace();
+    unittest_update_matrix_inplace1();
 
     for i = 1:100
         %unittest_update_matrix_inplace2();    
     end;
+
+    test_update_matrix_inplace3();
+    test_update_matrix_inplace4();    
+end
+
+
+function test_update_matrix_inplace3()
+
+    iterations = 100000;
+	
+	% Constants for matrix dimensions
+    numRowsA = 1703; 
+	numColsA = 1705; 
+	sizeB = 25;
+
+    % Initialize matrix A with zeros and matrix B with random values
+	A = zeros(numRowsA, numColsA, 'double');
+	B = rand(sizeB, 'double'); 
+
+	averageMatlabTime = 0;
+	averageMexTime = 0;
+    averageAvxTime = 0;
+
+    % Loop to perform matrix updates
+    for i = 1:iterations        
+        % Calculate random dimensions for the submatrix
+        deltaRows = sizeB-1;
+        deltaCols = sizeB-1;
+        
+        % Select a random starting point for submatrix in A
+        startRowA = ceil(numRowsA/2 * rand); 
+		endRowA = startRowA + deltaRows;
+        startColA = ceil(numColsA/2 * rand); 
+		endColA = startColA + deltaCols;
+
+        % Select a random starting point for submatrix in B
+        startRowB = 1;
+		endRowB = startRowB + deltaRows;
+        startColB = 1;
+		endColB = startColB + deltaCols;
+        
+        % 
+		tic;
+        A(startRowA:endRowA, startColA:endColA) = ...
+            A(startRowA:endRowA, startColA:endColA) + ...
+            B(startRowB:endRowB, startColB:endColB);
+			
+		averageMatlabTime = averageMatlabTime + toc;
+	
+		tic;
+		tools.array.mex.mex_update_matrix_inplace_double(A, B, int32(startColA), int32(startRowA), int32(startColB), int32(startRowB), int32(1+deltaCols), int32(1+deltaRows));
+		averageMexTime = averageMexTime + toc;		
+
+		tic;
+		tools.array.mex.mex_update_matrix_inplace_double_avx2(A, B, int32(startColA), int32(startRowA), int32(startColB), int32(startRowB), int32(1+deltaCols), int32(1+deltaRows));
+		averageAvxTime = averageAvxTime + toc;		        
+    end
+
+    % Calculate and display the average writing time
+    averageMatlabTime = averageMatlabTime / iterations;
+	averageMexTime = averageMexTime / iterations;
+    averageAvxTime = averageAvxTime / iterations;
+	fprintf('test_update_matrix_inplace3: Average time: MATLAB: %f, MEX: %f: AVX2: %f\n', averageMatlabTime, averageMexTime, averageAvxTime);
+end
+
+
+function test_update_matrix_inplace4()
 
     iterations = 100000;
 	
@@ -61,12 +128,12 @@ function test_update_matrix_inplace()
     averageMatlabTime = averageMatlabTime / iterations;
 	averageMexTime = averageMexTime / iterations;
     averageAvxTime = averageAvxTime / iterations;
-	fprintf('Average time: MATLAB: %f, MEX: %f: AVX2: %f\n', averageMatlabTime, averageMexTime, averageAvxTime);
+	fprintf('test_update_matrix_inplace4: Average time: MATLAB: %f, MEX: %f: AVX2: %f\n', averageMatlabTime, averageMexTime, averageAvxTime);
 end
 
 
 
-function unittest_update_matrix_inplace()
+function unittest_update_matrix_inplace1()
 
     % Initialize matrix A with zeros and matrix B with random values
 	A = zeros(5, 5, 'double');
@@ -135,3 +202,4 @@ function unittest_update_matrix_inplace2()
     %disp(E);
     assert(isequal(C, E));
 end
+
