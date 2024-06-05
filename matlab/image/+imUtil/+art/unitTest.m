f046331b073bdd481357baa2503254726d5aab4b
A.M. Krassilchtchikov <agioffe@gmail.com>
2024-06-05 16:59:08 +0300

    *.m

--- matlab/image/+imUtil/+art/unitTest.m
+++ matlab/image/+imUtil/+art/unitTest.m
@@ -38,16 +38,30 @@ function Result = unitTest()
     AI = AstroImage('~/matlab/data/TestImages/unitTest/LAST_subimage.fits');
     AI = imProc.sources.findMeasureSources(AI,'Threshold', 5);
     AI = imProc.psf.populatePSF(AI);
-    [AI, Res] = imProc.sources.psfFitPhot(AI);
+    [AI, Res] = imProc.sources.psfFitPhot(AI);  % produces PSFs shifted to RoundX, RoundY, so there is no need to Recenter 
     X1Y1 = [Res.Y Res.X]; % note the order! 
     
-    [CubePSF, XY] = imUtil.art.createSourceCube(Res.ShiftedPSF, X1Y1, Res.Flux, 'Recenter', false, ...
-        'RecenterMethod','fft', 'PositivePSF', true);
+    [CubePSF, XY] = imUtil.art.createSourceCube(Res.ShiftedPSF, X1Y1, Res.Flux, 'Recenter', false);
     
     Image0 = repmat(0,size(AI.Image));
     ImageSrc = imUtil.art.injectSources(Image0,CubePSF,XY,'Oversample',[],'Subtract',false);
     ImageSrcBack = imUtil.art.addBackground(ImageSrc, AI.Back, 'Subtract', false);
     DiffImage    = AI.Image - ImageSrcBack;
+    DiffImageMasked = DiffImage .* (AI.Mask == 0);
+    
+    % trying another iteration with the difference image    
+    AI2 = AstroImage({DiffImageMasked});
+    AI2 = imProc.sources.findMeasureSources(AI2,'Threshold', 3,'BackPar',...
+        {'BackFun',@median,'BackFunPar',{'all'},'VarFun',@imUtil.background.rvar,'SubSizeXY','full'}); 
+    AI2 = imProc.psf.populatePSF(AI2);
+    [AI2, Res2] = imProc.sources.psfFitPhot(AI2); 
+    
+    X1Y1 = [Res2.Y Res2.X];
+    [CubePSF, XY] = imUtil.art.createSourceCube(Res2.ShiftedPSF, X1Y1, Res2.Flux, 'Recenter', false);
+    ImageSrc2 = imUtil.art.injectSources(Image0,CubePSF,XY,'Oversample',[],'Subtract',false);
+    ImageSrcBack2 = imUtil.art.addBackground(ImageSrc2, AI2.Back, 'Subtract', false);
+    DiffImage2    = AI2.Image - ImageSrcBack2;
+    DiffImageMasked2 = DiffImage2 .* (AI2.Mask == 0);
     
     io.msgLog(LogLevel.Test, 'imUtil.art.unitTest test passed');
     Result = true;
