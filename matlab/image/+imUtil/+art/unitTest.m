fcff1aedb46604adca0e0cb5edd1f1a29e6d679b
A.M. Krassilchtchikov <agioffe@gmail.com>
2024-06-17 11:59:44 +0300

    *.m

--- matlab/image/+imUtil/+art/unitTest.m
+++ matlab/image/+imUtil/+art/unitTest.m
@@ -3,40 +3,52 @@ function Result = unitTest()
     % Example: imUtil.art.unitTest
     io.msgLog(LogLevel.Test, 'imUtil.art.unitTest started');
     
-    %%% create an artificial sky image from object coordinates and PSFs
+    %%% create an artificial sky image 
+    if false %%%% TEMPRORARILY SWITCH OFF
+        
+    Nx = 1700; Ny = Nx;    
     
-    % set the number of sources, the image size and the flux range 
-    Nsrc = 20000; Nx = 1700; Ny = Nx; MaxFlux = 10000;
-    % define the source fluxes and their PSFs
-    Flux = repmat(0,1,Nsrc);
-    Mag  = repmat(0,1,Nsrc);
     MinMag = 6; MaxMag = 22; MagZP = 25;
-    for Isrc = 1:Nsrc
-        Mag(Isrc)  = max(MinMag,MaxMag*(Isrc/Nsrc).^0.2);
-        Flux(Isrc) = 10.^(0.4.*(MagZP-Mag(Isrc))); 
+    MagBins = MinMag:0.1:MaxMag;    
+    
+    Nsrc = 0;
+    for Imag = 1:numel(MagBins)        
+        Nstars = round(10^(-12).*MagBins(Imag).^11.76);  % the empiric dependence has been measured from a LAST image 
+%         fprintf('%.1f %.0f\n', MagBins(Imag), Nstars);
+        for Istar = 1:Nstars
+            Nsrc = Nsrc + 1;
+            Mag(Nsrc)  = MagBins(Imag);
+            Flux(Nsrc) = 10^(0.4.*(MagZP-Mag(Nsrc)));
+        end
     end
-    Flux = Flux .* (MaxFlux/max(Flux,[],'all')); % check data type conversion! 
+    
+    fprintf('%d objects in the FOV\n', Nsrc);
     
     X1Y1 = Nx.* rand(Nsrc,2);
-    Oversample = 1;
+    Oversample = 3;
     for i = 1:Nsrc; PSF(:,:,i) = imUtil.kernel2.gauss([2 2 0],[25 25]) + 1e-2*rand(25,25); end
     
     % create a list of shifted and resampled fluxed PSF stamps
     [CubePSF, XY] = imUtil.art.createSourceCube(PSF, X1Y1, Flux, 'Recenter', true, ...
         'RecenterMethod','fft','Oversample', Oversample, 'PositivePSF', true);
     
-    % create an empty image with some small noise (relative to MaxSrcFlux) 
+    % create an empty image 
     Image0 = rand(Nx,Ny);
     
     % fill the image with sources
-    ImageSrc = imUtil.art.addSources(Image0,CubePSF,XY,'Oversample',[],'Subtract',false);
+    ImageSrc = imUtil.art.addSources(Image0,CubePSF,XY,'Oversample',[],'Subtract',false);    
+    if sum(ImageSrc < 0) > 0
+        warning('Negative pixels');
+    end
     
     % add background with some spatial variations
-    Back = 0.02 .* MaxFlux .* (1 + 0.1*rand(Nx,Ny));    
+    Back = 200 .* (1 + 0.1*rand(Nx,Ny));    
     ImageSrcBack = imUtil.art.addBackground(ImageSrc, Back, 'Subtract', false);
     
     % add noise
     Image = imUtil.art.addNoise(ImageSrcBack,'normal');
+    
+    end
     
 %     Res = imUtil.sources.findSources(Image);
 
@@ -49,17 +61,21 @@ function Result = unitTest()
     
     Res1(1) = FitRestoreSubtract(AI1(1), 'VarMethod', 'LogHist', 'Threshold', 30, 'MomRadius', 4,...
         'RemoveMasked', false, 'RemovePSFCore', false, ...
-        'BackPar',{'SubSizeXY','full'}); 
-    AC1(1)  = Res1(1).Cat;        
+        'BackPar',{'SubSizeXY',[128 128]}); % {'SubSizeXY','full'}
+    AC1(1)    = Res1(1).Cat;
+    SrcImage1 = Res1(1).Src;
     
     for It = 2:5
         AI1(It)  = AstroImage({Res1(It-1).Diff}); 
         AI1(It).Back = AI1(It-1).Back; AI1(It).Var = AI1(It-1).Var; AI1(It).CatData.JD = AI1(1).CatData.JD;
         Res1(It) = FitRestoreSubtract(AI1(It), 'PSF', Res1(It-1).PSF, 'ReCalcPSF', false, ...
-            'ReCalcBack', true, 'VarMethod', 'LogHist', 'Threshold', 5, 'MomRadius', 6, 'Iteration',It, ...
+            'ReCalcBack', true, 'VarMethod', 'LogHist', ...
+            'PreviousVar', SrcImage1,...
+            'Threshold', 5, 'MomRadius', 6, 'Iteration',It, ...
             'RemoveMasked', false, 'RemovePSFCore', false, ...
             'BackPar',{'SubSizeXY',[128 128]});
-        AC1(It) = Res1(It).Cat;        
+        AC1(It)  = Res1(It).Cat;        
+        SrcImage1 = SrcImage1 + Res1(It).Src;
     end
     
     AI1(1).CatData = merge(AC1); % NB: AC1(1).Table is not updated 
@@ -70,19 +86,22 @@ function Result = unitTest()
     AI2(1)  = AstroImage('~/matlab/data/TestImages/unitTest/LAST_275-16_crop22.fits');
     Res2(1) = FitRestoreSubtract(AI2(1),'VarMethod','LogHist','Threshold', 30, 'MomRadius', 4,...
         'RemoveMasked', false, 'RemovePSFCore', false,...
-        'BackPar',{'SubSizeXY','full'});
-    AC2(1)  = Res2(1).Cat;     
+        'BackPar',{'SubSizeXY',[128 128]}); % {'SubSizeXY','full'}
+    AC2(1)    = Res2(1).Cat; 
+    SrcImage2 = Res2(1).Src;
         
     for It = 2:5
         Thresh = 5; % 50/It^1.4;
         AI2(It)  = AstroImage({Res2(It-1).Diff}); 
         AI2(It).Back = AI2(It-1).Back; AI2(It).Var = AI2(It-1).Var; AI2(It).CatData.JD = AI2(1).CatData.JD;        
         Res2(It) = FitRestoreSubtract(AI2(It), 'PSF', Res2(It-1).PSF, 'ReCalcPSF', true, ...
-            'ReCalcBack', true, ...
-            'VarMethod', 'LogHist', 'Threshold', Thresh, 'MomRadius', 6,'Iteration',It, ...
+            'ReCalcBack', true, 'VarMethod', 'LogHist', ...
+            'PreviousVar', SrcImage2,...
+            'Threshold', Thresh, 'MomRadius', 6,'Iteration',It, ...
             'RemoveMasked', false, 'RemovePSFCore', false,...
             'BackPar',{'SubSizeXY',[128 128]});
-        AC2(It) = Res2(It).Cat;        
+        AC2(It) = Res2(It).Cat; 
+        SrcImage2 = SrcImage2 + Res2(It).Src;
     end
     
     AI2(1).CatData = merge(AC2); % NB: AC2(1).Table is not updated 
@@ -102,13 +121,15 @@ function Result = unitTest()
     DS9_new.regionWrite([AI1(1).CatData.Catalog(:,29) AI1(1).CatData.Catalog(:,30)],'FileName','~/LAST_346+79_lastit.reg','Color','cyan','Marker','o','Size',1,'Width',4,'Precision','%.2f','PrintIndividualProp',0);
     DS9_new.regionWrite([AC1(1).Catalog(:,29) AC1(1).Catalog(:,30)],'FileName','~/LAST_346+79_it1.reg','Color','blue','Marker','o','Size',1,'Width',4,'Precision','%.2f','PrintIndividualProp',0);
     DS9_new.regionWrite([AC1(2).Catalog(:,29) AC1(2).Catalog(:,30)],'FileName','~/LAST_346+79_it2.reg','Color','red','Marker','o','Size',1,'Width',4,'Precision','%.2f','PrintIndividualProp',0);
+    DS9_new.regionWrite([AC1(3).Catalog(:,29) AC1(3).Catalog(:,30)],'FileName','~/LAST_346+79_it3.reg','Color','green','Marker','o','Size',1,'Width',4,'Precision','%.2f','PrintIndividualProp',0);
     
     DS9_new.regionWrite([AI2(1).CatData.Catalog(:,29) AI2(1).CatData.Catalog(:,30)],'FileName','~/LAST_275_16_lastit.reg','Color','cyan','Marker','o','Size',1,'Width',4,'Precision','%.2f','PrintIndividualProp',0);
     DS9_new.regionWrite([AC2(1).Catalog(:,29) AC2(1).Catalog(:,30)],'FileName','~/LAST_275_16_it1.reg','Color','blue','Marker','o','Size',1,'Width',4,'Precision','%.2f','PrintIndividualProp',0);
     DS9_new.regionWrite([AC2(2).Catalog(:,29) AC2(2).Catalog(:,30)],'FileName','~/LAST_275_16_it2.reg','Color','red','Marker','o','Size',1,'Width',4,'Precision','%.2f','PrintIndividualProp',0);
- 
-     S = readtable('~/LAST_275_16_sextractor_v2.cat','FileType','text','NumHeaderLines',14);
-     DS9_new.regionWrite([S.Var2 S.Var3],'FileName','~/LAST_275_16_sextractor_v2.reg','Color','yellow','Marker','b','Size',1,'Width',4,'Precision','%.2f','PrintIndividualProp',0);
+    DS9_new.regionWrite([AC2(3).Catalog(:,29) AC2(3).Catalog(:,30)],'FileName','~/LAST_275_16_it3.reg','Color','green','Marker','o','Size',1,'Width',4,'Precision','%.2f','PrintIndividualProp',0);
+
+%      S = readtable('~/LAST_275_16_sextractor_v2.cat','FileType','text','NumHeaderLines',14);
+%      DS9_new.regionWrite([S.Var2 S.Var3],'FileName','~/LAST_275_16_sextractor_v2.reg','Color','yellow','Marker','b','Size',1,'Width',4,'Precision','%.2f','PrintIndividualProp',0);
 
 end
 
@@ -122,6 +143,7 @@ function Result = FitRestoreSubtract(AI, Args)
        Args.PSF         = [];
        
        Args.VarMethod   = 'LogHist';
+       Args.PreviousVar = [];
        
        Args.MomRadius   = 4; % recommended MomRadius = 1.7 * FWHM ~ 3.8
        
@@ -140,6 +162,13 @@ function Result = FitRestoreSubtract(AI, Args)
     end
     % measure background and variance
     imProc.background.background(AI, 'ReCalcBack', Args.ReCalcBack, Args.BackPar{:});
+    
+%     Result.Var = AI.Var;            % save the measured local variance for the next iteration(s) 
+    if ~isempty(Args.PreviousVar)   % add the variance form the local sources from the previous iteration(s)
+        AI.Var = AI.Var + Args.PreviousVar;
+%         AI.Var = Args.PreviousVar;
+%         AI.Var = max(AI.Var,Args.PreviousVar);
+    end
     
 %     Ind = AI.Var > 1.3 .* AI.Back;
 %     AI.Var = AI.Var .* (1-Ind) + AI.Back .* Ind; % experimental
@@ -163,13 +192,13 @@ function Result = FitRestoreSubtract(AI, Args)
     else
         AI.PSF = Args.PSF;        
     end
-    % find sources once more with the measured PSF? 
+    % find sources once more with the measured PSF instead of a gaussian? 
 %     AI = imProc.sources.findMeasureSources(AI,'Threshold', Args.Threshold,'ReCalcBack',false,...
 %         'MomPar',{'MomRadius',Args.MomRadius},'Psf',AI.PSF,'FlagCR',false); 
-%     % make PSF photometry
+%   % make PSF photometry
     [AI, Res] = imProc.sources.psfFitPhot(AI);  % produces PSFs shifted to RoundX, RoundY, so there is no need to Recenter     
     % construct and inject sources
-    [CubePSF, XY] = imUtil.art.createSourceCube(Res.ShiftedPSF, [Res.RoundY Res.RoundX], Res.Flux, 'Recenter', false);
+    [CubePSF, XY] = imUtil.art.createSourceCube(Res.ShiftedPSF, [Res.RoundY Res.RoundX], Res.Flux, 'Recenter', false,'PositivePSF',true);
     ImageSrc = imUtil.art.addSources(repmat(0,size(AI.Image)),CubePSF,XY,'Oversample',[],'Subtract',false);
 %     ImageSrcBack = imUtil.art.addBackground(ImageSrc, AI.Back, 'Subtract', false);    % for testing, do not use it further 
     % make a difference image    
