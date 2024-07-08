82d056faff0e1ef6e7656aa5504fc2cfed8be162
EranOfek <ofek.eran@gmail.com>
2024-07-08 14:04:57 +0300

    tools.timeStamp

---
+++ matlab/util/+tools/+timeStamp/getTimeStamp.m
@@ -0,0 +1,18 @@
+function [Result] = getTimeStamp(Time)
+    % Get time stamp for now.
+    %     Optional detailed description
+    % Input  : - datenum time. Default is now.
+    % Output : - ISO time stamp.
+    % Author : Eran Ofek (2024 Jul) 
+    % Example: tools.timeStamp.getTimeStamp
+
+    arguments
+        Time = [];
+    end
+    if isempty(Time)
+        Time = now;
+    end
+
+    Result = sprintf('%s',datestr(Time,'yyyy-mm-ddTHH:MM:SS'));
+
+end
---
+++ matlab/util/+tools/+timeStamp/readMsgFile.m
@@ -0,0 +1,36 @@
+function [Result] = readMsgFile(FileName, Path)
+    % Read msessages and time stamps from file.
+    % Input  : - File name.
+    %          - Path. Default is ''.
+    % Output : - Structure array with all the messages.
+    %            Following fields are available:
+    %            .TimeStamp - ISO time stamp.
+    %            .Msg - Message.
+    %            .JD - Julian day.
+    % Author : Eran Ofek (2024 Jul) 
+    % Example:
+    % R=tools.timeStamp.readMsgFile('.status','/marvin/LAST.01.01.01/2024/07/05/proc/233146v0')
+
+    arguments
+        FileName
+        Path = '';
+    end
+
+    FullFileName = fullfile(Path, FileName);
+    FID = fopen(FullFileName);
+    I = 0;
+    while ~feof(FID)
+        I = I + 1;
+        L{I}=fgetl(FID);
+        if numel(L{I})>19
+            % context exist
+            Result(I).TimeStamp = L{I}(1:19);
+            Result(I).Msg       = L{I}(21:end);
+            Result(I).JD        = celestial.time.julday(Result(I).TimeStamp);
+        end
+    end
+    fclose(FID);
+    
+
+
+end
