96af565f85a1df66d3ef1064d9586306cb8f4dfc
A.M. Krassilchtchikov <agioffe@gmail.com>
2024-08-21 16:56:16 +0300

    *.m

--- matlab/astro/+ultrasat/usim.m
+++ matlab/astro/+ultrasat/usim.m
@@ -232,8 +232,11 @@ function [usimImage, AP, ImageSrcNoiseADU] =  usim ( Args )
 %     Back.Tot = ( Back.Zody  + Back.Cher + Back.Stray + Back.Dark + ...
 %                  Back.Cross + Back.Gain ) * sqrt(Exposure/300.) + Back.Readout * Args.Exposure(1); % NOT CORRECT
              
-    Back.Tot = ( Back.Zody  + Back.Cher + Back.Stray + Back.Dark + ...
-                 Back.Cross + Back.Gain + Back.Readout ) * Args.Exposure(1); 
+%     Back.Tot = ( Back.Zody  + Back.Cher + Back.Stray + Back.Dark + ...
+%                  Back.Cross + Back.Gain + Back.Readout ) * Args.Exposure(1);  % NOT CORRECT for small exposures
+             
+    Back.Tot = ( Back.Zody  + Back.Cher + Back.Stray + Back.Dark) * (Exposure/300.) ...
+                       + ( Back.Readout + Back.Cross + Back.Gain) * Args.Exposure(1); 
     
     %%%%%%%%%%%%%%%%%%%% load the matlab object with the ULTRASAT properties:
     I = Installer;
