f7681edf627ff9eb61e2707e705d380b39ef1103
EranOfek <ofek.eran@gmail.com>
2024-10-08 09:31:22 +0300

    period_fitfourier_getH

---
+++ matlab/astro/+timeSeries/+period/period_fitfourier_getH.m
@@ -0,0 +1,18 @@
+function [Result] = period_fitfourier_getH(X, Y, Args)
+    % One line description
+    %     Optional detailed description
+    % Input  : - 
+    %          - 
+    %          * ...,key,val,... 
+    % Output : - 
+    % Author : Eran Ofek (2024 Oct) 
+    % Example: 
+
+    arguments
+        X
+        Y
+        Args.A                 = [];
+        Args.B                 = [];
+    end
+
+end
