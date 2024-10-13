4a3b55ae2bb9f16f9390e0fd5dd213b02660da5b
EranOfek <ofek.eran@gmail.com>
2024-10-13 17:01:01 +0300

    imProc.db.generateImageID

--- matlab/image/+imProc/+db/generateImageID.m
+++ matlab/image/+imProc/+db/generateImageID.m
@@ -1,35 +1,69 @@
-function [Result] = generateImageID(Obj, Args)
-    % Generate Image 'unique' ID and store it in header
-    % Input  : - 
-    %          - 
+function [Obj, ID] = generateImageID(Obj, Args)
+    % Generate image 'unique' ID and store it in the header.
+    %       The ID is constructed from multiple pats each containing a pre
+    %       defined number of bits and information from a specific header keyword.
+    %       The value of each of these header keywords is encoded in the
+    %       sub array of bits.
+    % Input  : - An AstroImage object.
     %          * ...,key,val,... 
-    % Output : - 
+    %            'FormatSt' - A structure array containing information on how
+    %                   to construct the ID. The following fields should be
+    %                   provided:
+    %                   .Key - Header keyword name from which to retrieve
+    %                       the ID that will be stored in a sub array of
+    %                       bits corresponding to this key.
+    %                   .BitNum - The number of bits in the sub array.
+    %                   .Fun - A function handle to apply to the keyword
+    %                       value in order to get the value to enode in the
+    %                       sub array of bits.
+    %               Default: see code.
+    %            'KeyID' - If not empty, then this is the header keyword
+    %                   name in which the generated ID will be written into.
+    %                   Default is 'ID_PROC'.
+    % Output : - An AstroImage object with the updated header.
+    %          - A vector of IDs (one per image).
     % Author : Eran Ofek (2024 Oct) 
-    % Example: imProc.db.generateImageID
+    % Example: [A,ID]=imProc.db.generateImageID(A)
 
     arguments
         Obj
-        Args.BitNum            = [5 4  5 5 3 6  36]
-        Args.TypeCol           = "IMTYPE";
-        Args.LevelCol          = "LEVEL";
-        Args.InstCol           = ["NODENUB", "MOUNTNUM", "CAMNUM", "CROPID"];
-        Args.TimeCol           = "MIDJD";
-        
 
-        Args.TimeFun           = @(jd) uint64((jd-2451545.5).*86400.*10);  % number of 0.1s since J2000
-        Args.LevelOptions      = AstroFileName.ListLevel;
-        Args.TypeOptions       = AstroFileName.ListType;
+        Args.FormatSt          = struct("Key",{'IMTYPE','LEVEL','NODENUMB','MOUNTNUM','CAMNUM','CROPID','JD'},...
+                                        "BitNum", {4, 5, 5, 5, 3, 6, 36},...
+                                        "Fun", {@(x) find(strcmp(x, AstroFileName.ListType)),...
+                                                @(x) find(strcmp(x, AstroFileName.ListLevel)),...
+                                                @(x) x,...
+                                                @(x) x,...
+                                                @(x) x,...
+                                                @(x) x,...
+                                                @(jd) uint64((jd-2451545.5).*86400.*10)});
+
+        Args.KeyID             = 'ID_PROC';
+
     end
 
     Nobj = numel(Obj);
+    Nsub = numel(Args.FormatSt);
+    ID   = zeros(Nobj,1, "uint64");
     for Iobj=1:1:Nobj
-        if isempty(Args.TypeCol)
-            Type = 0;
-            BitType = 
-        else
-            Type = Obj(Iobj).getKey(Args.TypeCol)
+        BitNum = zeros(1,Nsub);
+        BitVal = zeros(1,Nsub);
+        for Isub=1:1:Nsub
+            BitNum(Isub) = Args.FormatSt(Isub).BitNum;
+            if BitNum(Isub)>0
+                % include in keyword value in ID
+                TmpVal       = Obj(Iobj).HeaderData.getVal( Args.FormatSt(Isub).Key );
+                BitVal(Isub) = Args.FormatSt(Isub).Fun(TmpVal);
+                if isnan(BitVal(Isub))
+                    error('Keyword %s value is NaN', Args.FormatSt(Isub).Key);
+                end
+            end
+        end
 
-        St = Obj(Iobj).getStructKey([Args.TypeCol, Args.LevelCol, Args.TimeCol]);
-        St.(Args.TypeCol)
+        ID(Iobj) = tools.bit.bitEncode(BitNum, BitVal);
+        if ~isempty(Args.KeyID)
+            Obj(Iobj).HeaderData.replaceVal(char(Args.KeyID), ID(Iobj));
+        end
+    end
 
 end
