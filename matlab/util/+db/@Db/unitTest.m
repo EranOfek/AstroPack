3022e4d2269e9fd21ac160fb19f7786893861e02
A.M. Krassilchtchikov <agioffe@gmail.com>
2024-11-18 15:33:06 +0200

    *.m

--- matlab/util/+db/@Db/unitTest.m
+++ matlab/util/+db/@Db/unitTest.m
@@ -29,48 +29,42 @@ function Result = unitTest()
     
     for Idir = 1:1 % Ndirs  % loop over the directories 
         
-        DataDir = ProcDirs(Idir);
+%         DataDir = ProcDirs(Idir);
         
         DataDir = '/mnt/marvin/LAST.01.01.01/2024/11/01/proc/001225v0/';
         
         cd(DataDir);
         
-        if ~contains(fileread('.status'), "injected into the DB")
+        if ~contains(fileread('.status'), "injected into the visit image DB")
             Coadd=AstroImage('LAST*coadd_Image_1.fits'); % read the data
             cd(Dir);
             fprintf('Injecting from %s ..',DataDir);
             
             A = AstroFileName;
             A.ProjName = Coadd(1).getStructKey('PROJNAME').PROJNAME;
-            A.SubDir = Coadd(1).getStructKey('SUBDIR').SUBDIR;
-            A.Level = Coadd(1).getStructKey('LEVEL').LEVEL;
-            A.FieldID = Coadd(1).getStructKey('FIELDID').FIELDID;
+            A.SubDir   = Coadd(1).getStructKey('SUBDIR').SUBDIR;
+            A.Level    = Coadd(1).getStructKey('LEVEL').LEVEL;
+            A.FieldID  = Coadd(1).getStructKey('FIELDID').FIELDID;
             % A.Time = Coadd(1).getStructKey('DATEOBS').DATEOBS;
             A.CCDID = 1; A.Counter = 0;
             A.FileType = "csv";
             CsvFN = sprintf("%s.csv",A.genFile);
             
             Columns = db.util.read_xls2tableFormat('~/matlab/data/db/Design-Database-Pipeline-ClickHouse.xlsx',...
-                'Sheet','Images','TableName','visit_images');
-            
-            % Coadd.setKeyVal('CAMNAME',int16(999)); % column type mismatch
-            % Coadd.setKeyVal('ID_DARK',int16(999)); % dealing with NaNs is in db.Db.table2csv
-            
-            % convert char representations of ID_DARK and ID_FLAT back to int64:
-            Coadd.setKeyVal('ID_DARK',int64([Coadd.getStructKey('ID_DARK').ID_DARK]));
-            Coadd.setKeyVal('ID_FLAT',int64([Coadd.getStructKey('ID_FLAT').ID_FLAT]));
-            
+                'Sheet','Images','TableName','visit_images');            
+
             T=imProc.db.insertImages(Coadd,'ColNameDic',Columns,'Db',DB,'DbName','last','DbTable',...
                 CoaddImageTable,'CreateCsv',true,'FileName',CsvFN, 'ColNameID',ColNameID);
             
             % copy the CSV file into the proc catalog and edit the .status file
             CopyCSV = sprintf('su - samar -c "cp ~sasha/%s %s"',CsvFN,DataDir);
-            [~, Err1] = system(CopyCSV);
-            Stamp = datetime('now', 'Format', 'yyyy-MM-dd''T''HH:mm:ss', 'TimeZone', 'UTC');
-            UpdateStatus = sprintf('su - samar -c "echo -n ''%s injected into the DB'' >> %s/.status"',Stamp,DataDir);
-            [~, Err2] = system(UpdateStatus);
+            [~, Err1] = system(CopyCSV);            
+            UpdateStatus = sprintf('su - samar -c "echo ''%s injected into the visit image DB'' >> %s/.status"',tools.timeStamp.getTimeStamp,DataDir);
+            [~, Err2] = system(UpdateStatus); 
             % system(rm fprintf('%s',CsvFN));
-            fprintf(' ..done\n');
+            fprintf(' ..done\n');  
+        else
+            cd(Dir); 
         end                        
     end
                                          
@@ -79,3 +73,11 @@ function Result = unitTest()
     % disconnect the DB     
     DB.disconnectCH_Java;    
 end
+
+            % Coadd.setKeyVal('CAMNAME',int16(999)); % column type mismatch
+            % Coadd.setKeyVal('ID_DARK',int16(999)); % dealing with NaNs is in db.Db.table2csv
+            
+            % convert char representations of ID_DARK and ID_FLAT back to int64:
+%             Coadd.setKeyVal('ID_DARK',int64([Coadd.getStructKey('ID_DARK').ID_DARK])); % converter moved to the Excel template;
+%             Coadd.setKeyVal('ID_FLAT',int64([Coadd.getStructKey('ID_FLAT').ID_FLAT]));
+            
