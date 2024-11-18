d7019e9c94db58c3cefc968c47b078c74cd24901
A.M. Krassilchtchikov <agioffe@gmail.com>
2024-11-18 15:33:00 +0200

    a new function to converts int64 numbers written as char back to int64 format

---
+++ matlab/util/+tools/+class/char2int64.m
@@ -0,0 +1,23 @@
+function Result = char2int64(X)
+    % convert an array of numerical values written as char to int64
+    %     Optional detailed description
+    % Input  : - an array of numerical values written as char
+    %          * ...,key,val,... 
+    % Output : - an array of int64 values 
+    % Author : A.M. Krassilchtchikov (2024 Nov) 
+    % Example: A = '12345687696786'; A = tools.class.char2int64(A);
+    %
+    arguments
+        X        
+    end
+    %
+    if strcmpi(class(X),'int64') || prod(isnan(X)) > 0
+        Result = X; 
+    elseif ischar(X)
+        Result = int64(str2num(X)); 
+    elseif isnumeric(X)
+        Result = int64(X); 
+    else
+        error('Invalid input class');
+    end
+end
