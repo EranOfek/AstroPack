39a3f520e9b84841781637c34f055104f42cff13
EranOfek <ofek.eran@gmail.com>
2024-12-14 17:54:03 +0200

    imUtil.spec.trace.trace

--- matlab/image/+imUtil/+spec/+trace/trace.m
+++ matlab/image/+imUtil/+spec/+trace/trace.m
@@ -1,5 +1,88 @@
-function Result = trace(Array, Args)
+function [Result, SN, SN1, ResCollapse, PeakDet]  = trace(Array, Args)
+    % Find and fit taces from spectra in an image
+    %   This function is performing the following steps:
+    %   1. Measure global background and variance
+    %   2. Filter the 2D images with a short-line kernel for trace.
+    %      This include two kernels one with the instrumental PSF and the
+    %      second which is a delta function (for CR removal).
+    %   3. Optionally mask bad positions in the detector
+    %   4. Detect peaks in 2D filtered image.
+    %   5. Collapse filtered image and find locak maxima (initial trace
+    %      positions).
+    %   6. For each possible trace, calculate moments along the trace and
+    %      fit it with smooth polynomial.
+    %   Comments: The user can choose if to find all traces or only
+    %   significant traces near predefined/predicted positions.
+    %   The trace is calculated using several methods (see information in
+    %   output section).
     %
+    % Input  : - A 2D array.
+    %          * ...,key,val,...
+    %            'Dim' - Dim of spatial coordinate. Default is 1.
+    %            'TraceLineKernel' - 
+    %            'PSFsigma' - 
+    %            'Back' - An optional background image (or scalar).
+    %                   If empty, then calculate a global background using:
+    %                   tools.math.stat.rmean
+    %                   Back and Var are used for the filtering step.
+    %                   Default is [].
+    %            'Var' - An optional variance image (or scalar).
+    %                   If empty, then assume equal to the background image
+    %                   (i.e., assuming the image noise followin Poisson
+    %                   distribution).
+    %                   Default is [].
+    %            'Threshold' - Threshold used in:
+    %                   imUtil.spec.trace.peakDetectionFilter1
+    %            'ThresholdSum' - ThresholdSum used in:
+    %                   imUtil.spec.trace.peakDetectionFilter1
+    %                   This is the threshold in units of noise. For each
+    %                   local maxima above this threshold the S/N along the
+    %                   other dimension will be added sqrt(sum(SN^2)).
+    %
+    %            'GlobalStd' - ThresholdSum used in:
+    %                   imUtil.spec.trace.peakDetectionFilter1
+    %                   A logical indicating if to use the local
+    %                   std, or the std of the colums/rows. The std is used
+    %                   for the noise estimation. Default is false.
+    %            'GoodMask' - An optional array of logicals indicating if
+    %                   to use the pixel (true) or not (false) in the trace
+    %                   finding. If empty, will use all pixels.
+    %            'IgnoreWaveRangePos' - A two column matrix of
+    %                   [Xstart Xend]. Pixels in these ranges will be set
+    %                   with GoodMask values of false (will not be used).
+    %                   Default is [].
+    %
+    %            'ExpectedPos' - A vector of spatial positions of expected
+    %                   traces. If given then only peaks found near the
+    %                   expected position (within +/- ExpectedPosErr), will
+    %                   be selected. If empty, will returm all tarces
+    %                   found. Default is [].
+    %            'ExpectedPosErr' - Error of the positions listed in
+    %                   ExpectedPos. Default is 3 pix.
+    %            'Moments1dArgs' - Additional arguments to pass to:
+    %                   imUtil.spec.trace.moment1d
+    %                   Default is {}.
+    %            'UseWeightedMom' - Return and fit the Weighted moments
+    %                   (rather than the moments).
+    %                   Default is true.
+    %
+    % Output : - A structure array with element per trace, and the
+    %            following fields:
+    %            .Pos
+    %            .ExpectedPos
+    %            .SN
+    %            .ResMomFilt
+    %            .ResMomUnFilt
+    %            .FitMomFilt
+    %            .FitMomUnFilt
+    %          - S/N image.
+    %          - S/N image with delta function kernel.
+    %          - Collapseed S/N.
+    %          - Detected peaks information (output of:
+    %            imUtil.spec.trace.collapse).
+    %
+    % Author : Eran Ofek (Dec 2024)
+    % Example: RR=imUtil.spec.trace.trace(Array);
     
     arguments
         Array
@@ -8,21 +91,17 @@ function Result = trace(Array, Args)
         Args.PSFsigma          = 3;
         Args.Back              = [];
         Args.Var               = [];
-        Args.BackArgs cell     = {}; %'VarFun',@var, 'VarFunPar',{[],'all','omitnan'}};
+        %Args.BackArgs cell     = {}; %'VarFun',@var, 'VarFunPar',{[],'all','omitnan'}};
 
         
         Args.Threshold          = 10;  % integrated 
         Args.ThresholdSum       = 3;
         Args.GlobalStd logical  = false;
-        
-        Args.CollapseOption     = 'sn';
-        Args.MinSN              = 8;
-        Args.ApproxPos          = 332;  % [];
-        
+                
         Args.GoodMask           = [];
-        Args.IgnoreWaveRangePos = [0 30];
+        Args.IgnoreWaveRangePos = []; %[0 30];
         
-        Args.ExpectedPos        = [333, 500]; %[];
+        Args.ExpectedPos        = []; %[333, 500]; %[];
         Args.ExpectedPosErr     = 3;
         
         Args.Moments1dArgs      = {};
@@ -40,17 +119,6 @@ function Result = trace(Array, Args)
     end
     Dim = 1;
     
-    % Build trace kenel
-    StampSize       = Args.TraceLineKernel(1);
-    if (StampSize.*0.5)==floor(StampSize.*0.5)
-        % even number
-        StampSize = StampSize + 1;
-    end
-    StampSize = [StampSize StampSize];
-    
-    LineKernel      = imUtil.kernel2.line([Args.TraceLineKernel, Args.PSFsigma], StampSize);
-    LineDeltaKernel = imUtil.kernel2.line([Args.TraceLineKernel, 0.1],           StampSize);
-    
     % Estimate background and variance
     if isempty(Args.Back) || isempty(Args.Var)
         % this is problematic - a different back sub approac is needed
@@ -63,6 +131,20 @@ function Result = trace(Array, Args)
         Back = Args.Back;
         Var  = Args.Var;
     end
+    
+    
+    % Build trace kenel
+    StampSize       = Args.TraceLineKernel(1);
+    if (StampSize.*0.5)==floor(StampSize.*0.5)
+        % even number
+        StampSize = StampSize + 1;
+    end
+    StampSize = [StampSize StampSize];
+    
+    LineKernel      = imUtil.kernel2.line([Args.TraceLineKernel, Args.PSFsigma], StampSize);
+    LineDeltaKernel = imUtil.kernel2.line([Args.TraceLineKernel, 0.1],           StampSize);
+    
+    
     
     % filter image with trace kernel
     [SN,Flux,FiltImage,FiltImageVar,Info] = imUtil.filter.filter2_sn(Array, Back, Var, LineKernel);
@@ -95,54 +177,46 @@ function Result = trace(Array, Args)
                                                                       'GoodMask',GoodMask);
 
     
-    
-    
-    
        
     SNp(~GoodMask) = NaN;
-    [ResCollapse,PeakDet] = imUtil.spec.trace.collapse(SNp);
+    [ResCollapse,PeakDet] = imUtil.spec.trace.collapse(SNp, 'Threshold',Args.Threshold);
     
-    
-    
-    
-    
-    PeakDet.PeakPos
-    PeakDet.PeakSN
     
     if isempty(Args.ExpectedPos)
         % return all possible traces
+        Npos = numel(PeakDet.PeakPos);
+        Args.ExpectedPos = PeakDet.PeakPos(:).';
         
     else
         % return only traces consistent with ExpectedPos
         Npos = numel(Args.ExpectedPos);
-        
-        Diff = PeakDet.PeakPos - Args.ExpectedPos(:).';
-        [MinDist,MinInd] = min(abs(Diff),[],1);
-        FlagFound = MinDist<Args.ExpectedPosErr;
-        
-        Result = struct('Pos',cell(Npos,1), 'ExpectedPos',cell(Npos,1), 'SN',cell(Npos,1),...
-                        'ResMomFilt',cell(Npos,1), 'ResMomUnFilt',cell(Npos,1),...
-                        'FitMomFilt',cell(Npos,1), 'FitMomUnFilt',cell(Npos,1));
-        
-        for Ipos=1:1:Npos
-            Result(Ipos).ExpectedPos = Args.ExpectedPos(Ipos);
-            if FlagFound(Ipos)
-                IposMin  = MinInd(Ipos);
-                Result(Ipos).Pos         = PeakDet.PeakPos(IposMin);
-                Result(Ipos).SN          = PeakDet.PeakSN(IposMin);
-                
-                % fit the traces:
-                Result(Ipos).ResMomFilt   = imUtil.spec.trace.moment1d(SN, PeakDet.PeakPos(IposMin), 'Dim',1, Args.Moments1dArgs{:});
-                Result(Ipos).ResMomUnFilt = imUtil.spec.trace.moment1d(Array, PeakDet.PeakPos(IposMin), 'Dim',1, Args.Moments1dArgs{:});
-                
-                Result(Ipos).FitMomFilt   = imUtil.spec.trace.fitTrace([],Result(Ipos).ResMomFilt.(MomField)(:));
-                Result(Ipos).FitMomUnFilt = imUtil.spec.trace.fitTrace([],Result(Ipos).ResMomUnFilt.(MomField)(:));
-                
-            end
-        end
-        
     end
-    'a'
+    
+    Diff = PeakDet.PeakPos - Args.ExpectedPos(:).';
+    [MinDist,MinInd] = min(abs(Diff),[],1);
+    FlagFound = MinDist<Args.ExpectedPosErr;
+
+    Result = struct('Pos',cell(Npos,1), 'ExpectedPos',cell(Npos,1), 'SN',cell(Npos,1),...
+                    'ResMomFilt',cell(Npos,1), 'ResMomUnFilt',cell(Npos,1),...
+                    'FitMomFilt',cell(Npos,1), 'FitMomUnFilt',cell(Npos,1));
+
+    for Ipos=1:1:Npos
+        Result(Ipos).ExpectedPos = Args.ExpectedPos(Ipos);
+        if FlagFound(Ipos)
+            IposMin  = MinInd(Ipos);
+            Result(Ipos).Pos         = PeakDet.PeakPos(IposMin);
+            Result(Ipos).SN          = PeakDet.PeakSN(IposMin);
+
+            % fit the traces:
+            Result(Ipos).ResMomFilt   = imUtil.spec.trace.moment1d(SN, PeakDet.PeakPos(IposMin), 'Dim',1, Args.Moments1dArgs{:});
+            Result(Ipos).ResMomUnFilt = imUtil.spec.trace.moment1d(Array, PeakDet.PeakPos(IposMin), 'Dim',1, Args.Moments1dArgs{:});
+
+            Result(Ipos).FitMomFilt   = imUtil.spec.trace.fitTrace([],Result(Ipos).ResMomFilt.(MomField)(:));
+            Result(Ipos).FitMomUnFilt = imUtil.spec.trace.fitTrace([],Result(Ipos).ResMomUnFilt.(MomField)(:));
+
+        end
+    end
+        
     
     
     % extract the trace and background
