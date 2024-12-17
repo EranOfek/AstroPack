63e6321eeed3cbcecef3c2ed095c25ba49a7c1ab
EranOfek <ofek.eran@gmail.com>
2024-12-17 22:10:27 +0200

    SpecTrace class

---
+++ matlab/image/@SpecTrace/SpecTrace.m
@@ -0,0 +1,50 @@
+% SpecTrace - A container class for 2d spectral traces
+%
+
+
+classdef SpecTrace < Component
+    
+    properties (SetAccess = public)
+        ExpectedPos(1,1)       % user specified expected spatial position of the trace
+        MeasuredPos(1,1)       % measured spatial mean position of the trace
+        Traces      = struct('X',[], 'FiltX1',[], 'FiltX1W',[], 'UnfiltX1',[], 'UnfiltX1W',[], 'FiltFit',[], 'UnfiltFit',[]);
+        TracesWidth = struct('X',[], 'X2',[], 'FWHM',[]);
+        TraceMethod = 'FiltFit';
+                
+    end
+    
+    methods % Constructor
+        
+    end
+    
+    methods %
+        function Obj=searchTrace(AI, Args)
+            % Start with image - find all traces b
+            
+        end
+        
+        function Obj=traceKalman(AI, Args)
+            %
+            
+        end
+        
+        function Obj=traceByFilt(AI, Args)
+            %
+            
+        end
+        
+        function LinAI=linTrace(Obj, AI, Args)
+            %
+            
+        end
+        
+    end
+    
+    methods (Static) % UnitTest
+        Result = unitTest()
+            % unitTest for AstroPSF
+    end
+    
+end
+
+           
