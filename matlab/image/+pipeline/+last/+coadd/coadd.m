83434fc948275f8fe64fd6ba01896b093de25ba4
EranOfek <ofek.eran@gmail.com>
2024-12-20 12:49:12 +0200

    pipeline.last.coadd.coadd help

--- matlab/image/+pipeline/+last/+coadd/coadd.m
+++ matlab/image/+pipeline/+last/+coadd/coadd.m
@@ -1,9 +1,50 @@
 function [CI, DB, AI, T] = coadd(RA, Dec, Args)
     % Coadd LAST images by coordinates or fieldid/camnum/cropid
-    % Input  : - 
-    %          - 
+    % Input  : - Either:
+    %            J2000 RA [deg|rad|sexagesimal string]
+    %            or: Object name string (in this case Dec (2nd argument) must be
+    %            empty.
+    %            or: A cell array of {fieldid, camnum, cropid}
+    %            where cropid can be a vector of several elements.
+    %            or: a table which is the output of a visit_images table
+    %            (all the images in the table will be coadd with no
+    %            post spelection).
+    %          - J2000.0 Dec, or empty Default is [].
     %          * ...,key,val,... 
-    % Output : - 
+    %            'HalfWidth' -The [RA, Dec] half width pof the crop image
+    %                   size. This is used in case images are searched by
+    %                   coordinates. Default is [0.55 0.55]./2
+    %            'MinNim' - Minimum number of images to coadd.
+    %                   Default is 5.
+    %            'MaxNim' - Maximum number of images to coadd.
+    %                   Default is 100.
+    %            'SortBy' - if number of images found exceeds 'MaxNim',
+    %                   then will sort them by this column and choose the
+    %                   top MaxNim. Default is 'fwhm'.
+    %            'StackMethod' - Coadd method. Default is 'sigmaclip'.
+    %            'StackArgs' - A cell array of additional arguments to pass
+    %                   to the coadd function: imProc.stack.coaddW
+    %                   Default is {'MeanFun',@tools.math.stat.nanmean, 'StdFun', @tools.math.stat.std_mad, 'Nsigma',[2 2]}
+    %            'FindSrc' - A logical indicating if to populate the
+    %                   CatData (source catalog) in the coadd image.
+    %                   Default is true.
+    %            'InUnits' - Units of RA/Dec coordinates. Default is 'deg'.
+    %            'Constraints' - A two column cell array of constraints to
+    %                   apply to the image search (by coordinates or
+    %                   fieldid).
+    %                   Default is: {'fwhm',[1.0 4.0]; 'airmass',[1 1.5]; 'ph_rms',[0 0.03]; 'limmag',[20 23]};
+    %            'DB' - A db.Db object. If empty, will be created and
+    %                   opened, and returned. Default is [].
+    %
+    %            See code, for additional (hidden) arguments.
+    %
+    % Output : - An AstroImage (array) of coadd images (image per cropid).
+    %          - The db.Db object.
+    %          - An AstroImage array of the coadd images. If multiple
+    %            coaddition are performed, this contains the images for the
+    %            last coaddition (i.e., CI(end)).
+    %          - A cell array of tables of the selected images used for the coaddition.
+    %            Cell element per coadd image.
     % Author : Eran Ofek (2024 Dec) 
     % Example: CI=pipeline.last.coadd.coadd({1325 2 20});
     %          CI=pipeline.last.coadd.coadd(100,10);
@@ -12,7 +53,7 @@ function [CI, DB, AI, T] = coadd(RA, Dec, Args)
         RA                             % J2000 RA [deg|rad|sexagesimal|{FieldID#, CamNum, CropID}|table]
         Dec                    = [];
 
-        Args.HalfWidth         = [0.55 0.55];
+        Args.HalfWidth         = [0.55 0.55]./2;
         Args.MinNim            = 5;    % minimum number of images to add
         Args.MaxNim            = 100;  % maximum number of images to add
         Args.SortBy            = 'fwhm';
@@ -21,11 +62,13 @@ function [CI, DB, AI, T] = coadd(RA, Dec, Args)
         Args.StackArgs         = {'MeanFun',@tools.math.stat.nanmean, 'StdFun', @tools.math.stat.std_mad, 'Nsigma',[2 2]};
 
         Args.FindSrc logical   = true;
-
+        Args.Constraints       = {'fwhm',[1.0 4.0]; 'airmass',[1 1.5]; 'ph_rms',[0 0.03]; 'limmag',[20 23]};
 
         Args.InUnits           = 'deg';
-        Args.Server            = @VO.name.server_simbad;
         Args.DB                = [];
+        
+        Args.Server            = @VO.name.server_simbad;
+        
         Args.TableName         = "last.visit_images"
 
         Args.SelectFields      = ["ra", "dec", "m_ra", "m_dec", "airmass", "exptime", "jd_start", "midjd", "filter", "fieldid", "counter", "nodenumb", "mountnum", "camnum", "ccdid", "cropid", "subdir", "server",...
@@ -33,7 +76,6 @@ function [CI, DB, AI, T] = coadd(RA, Dec, Args)
                                   "meanbck", "medbck", "stdbck", "meanvar", "medvar", "fwhm", "med_a", "med_b", "med_th", "nsrc",...
                                   "ph_zp", "ph_col1", "ph_medc", "ph_rms", "ph_nsrc", "limmag", "backmag", "ncoadd",...
                                   "ra1", "ra2", "ra3", "ra4", "dec1", "dec2", "dec3", "dec4", "optics_cln"];
-        Args.Constraints       = {'fwhm',[1.0 4.0]; 'airmass',[1 1.5]; 'ph_rms',[0 0.03]; 'limmag',[20 22]};
 
         Args.CoaddLevel        = 'coadd';
         Args.CoaddProduct      = 'Image+';
