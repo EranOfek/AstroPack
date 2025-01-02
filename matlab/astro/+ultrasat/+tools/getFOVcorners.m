4719052ceb635f0ce0a8da328acf568973a677b4
A.M. Krassilchtchikov <agioffe@gmail.com>
2025-01-02 14:39:26 +0200

    *.m

--- matlab/astro/+ultrasat/+tools/getFOVcorners.m
+++ matlab/astro/+ultrasat/+tools/getFOVcorners.m
@@ -1,45 +1,59 @@
-function Corners = getFOVcorners(RA, Dec, Args)
+function [FOV, Tiles] = getFOVcorners(RA, Dec, Args)
     % get coordinates of the ULTRASAT FOV corners given its center and roll angle
     %
     % Input : - RA [deg]
     %         - Dec [deg]
     %       * ...,key,val,... 
-    %       'Roll' - roll angle [deg]
-    %       'HalfFOVRad' - half FOV radius (takes the gap into account) [deg]
+    %       'Roll'        - roll angle [deg]
+    %       'HalfFOVSize' - angular size of half FOV (takes the gap into account) [deg]
+    %       'TileSide'    - angular size of the tile side [deg]
     % Output : - a matrix of corner coordinates [deg]
     % Author: A.M. Krassilchtchikov (2024 Nov)
-    % Example: RA = 215; Dec = 60; Corners = ultrasat.tools.getFOVcorners(RA, Dec, 'Roll', 30)
+    % Example: RA = 215; Dec = 60; Corners = ultrasat.tools.getFOVcorners(RA, Dec, 'Roll', 30, 'Tiles',true)
     arguments
         RA
         Dec      
-        Args.Roll       = 0;      % roll angle [deg]      
-        Args.HalfFOVRad = 7.3557; % half-size of the square FOV w/account of the gap [deg] 
-        Args.Plot       = false
-        Args.Color      = 'blue';
+        Args.Roll       = 0;      % roll angle [deg]     
+        Args.Tiles      = false;  % calculate the tile corners 
+        Args.HalfFOV    = 7.3557; % half-size of the square FOV w/account of the gap [deg] 
+        Args.TileSide   = 7.1637; % tile side [deg]         
+        Args.Plot       = false; 
+        Args.Color      = 'blue'; 
     end
     %
     RAD = 180/pi;    
-%     [OutRA,OutDec] = celestial.coo.center2corners(RA/RAD,Dec/RAD,Args.HalfFOVRad/RAD,Args.HalfFOVRad/RAD);            
-    [OutRA, OutDec] = sky_square_corners(RA/RAD, Dec/RAD, 2.*Args.HalfFOVRad/RAD, Args.Roll/RAD);
-    OutRA(OutRA<0)  = OutRA(OutRA<0) + 2*pi;    % need to have only positive RA
-    Corners(:,1)    = OutRA .* RAD; Corners(:,2) = OutDec .*RAD;   
+    HalfGap = Args.HalfFOV-Args.TileSide;
+    [FOV.RA, FOV.Dec] = sky_square_corners(RA, Dec, 2.*Args.HalfFOV, Args.Roll);    
+    %
+    if Args.Tiles                
+        for Itile = 1:4
+            [Arclen, Az] = distance(Dec,RA,FOV.Dec(Itile),FOV.RA(Itile));  % from the FOV center to the new corner
+            Dist2TileCenter = Arclen/2 + sqrt(2) * HalfGap / 2;
+            [Center.Dec, Center.RA] = reckon(Dec, RA, Dist2TileCenter, Az,'degrees');
+            Center.RA(Center.RA<0)  = Center.RA(Center.RA<0) + 360;
+            [Tiles(Itile).RA, Tiles(Itile).Dec] = sky_square_corners(Center.RA, Center.Dec, Args.TileSide, Args.Roll);
+        end
+    else
+        Tiles = [];
+    end
     % 
     if Args.Plot
         figure; clf
         axesm('MapProjection', 'aitoff', 'AngleUnits', 'radians', 'LabelUnits', 'radians', 'Grid', 'on');
-        plotm(RA/RAD,Dec/RAD,'.','Color','red');
-        plotm(Corners(:,2)/RAD,Corners(:,1)/RAD,'.','Color',Args.Color);
+        plotm(Dec/RAD,RA/RAD,'.','Color','red');
+        plotm(FOV.Dec/RAD,FOV.RA/RAD,'.','Color',Args.Color);
     end
 end
 %
 function [RA_C, Dec_C] = sky_square_corners(RA, Dec, Side, Rot)
     % Calculate the coordinates of the four corners of a sky square
     % centered at RA, Dec with the side size Side and clockwise rotation angle Rot 
-    % (NB: all the input and output values are in RAD!)
-    
+    % (NB: all the input and output values are in degrees!)
+    RAD = 180/pi;
     % half-diagonal angular distance and azimuths
     HalfDiagonal = sqrt(2) * Side / 2;
-    Az = Rot + [pi/4, 3*pi/4, 5*pi/4, 7*pi/4];
+    Az = Rot + [pi/4, 3*pi/4, 5*pi/4, 7*pi/4].*RAD;
     % project the HalfDiagonal size at these azimuths 
-    [Dec_C, RA_C] = arrayfun(@(az) reckon(Dec, RA, HalfDiagonal, az, 'radians'), Az);    
+    [Dec_C, RA_C] = arrayfun(@(az) reckon(Dec, RA, HalfDiagonal, az, 'degrees'), Az);    
+    RA_C(RA_C<0) = RA_C(RA_C<0) + 360; % the RA range is [0, 360] 
 end
