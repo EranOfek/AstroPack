895b578fe6e941a46b82b1a1c48912e434a099c6
A.M. Krassilchtchikov <agioffe@gmail.com>
2025-01-06 15:48:04 +0200

    *.m

--- matlab/astro/+ultrasat/+tools/getFOVcircle.m
+++ matlab/astro/+ultrasat/+tools/getFOVcircle.m
@@ -17,13 +17,12 @@ function Circle = getFOVcircle(RA, Dec, Args)
     %
     RAD = 180/pi;    
     [OutRA,OutDec] = celestial.coo.celestial_circ(RA/RAD,Dec/RAD,Args.Radius/RAD,Args.NumPoints);    
-    OutRA(OutRA<0) = OutRA(OutRA<0) + 2*pi;  % need to have only positive RA
     Circle(:,1) = OutRA .* RAD; Circle(:,2) = OutDec .*RAD;    
     % 
     if Args.Plot
         figure; clf
         axesm('MapProjection', 'aitoff', 'AngleUnits', 'radians', 'LabelUnits', 'radians', 'Grid', 'on');
-        plotm(RA./RAD,Dec./RAD,'.','Color','red');
+        plotm(Dec./RAD,RA./RAD,'.','Color','red');
         plotm(Circle(:,2)./RAD,Circle(:,1)./RAD,'.','Color',Args.Color);
     end
 end
