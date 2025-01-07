d13e07608f0cc16febc3c2d5d6de123196853a46
EranOfek <ofek.eran@gmail.com>
2025-01-07 09:38:12 +0200

    tools.array.unique_count

--- matlab/util/+tools/+array/unique_count.m
+++ matlab/util/+tools/+array/unique_count.m
@@ -1,21 +1,32 @@
-function [UnVal,Count]=unique_count(Vec)
-% Unique values and count the number of apperances of each value.
-% Package: Util.array
-% Description: Select unique values in numeric vector and count the
-%              number of apperances of each value.
-% Input  : - Numeric vector.
-% Output : - Unique values
-%          - Count of appearances per unique value.
-% Tested : Matlab R2014a
-%     By : Eran O. Ofek                    Jan 2015
-%    URL : http://weizmann.ac.il/home/eofek/matlab/
-% Example: [UnVal,Count]=tools.array.unique_count(Vec)
-% Reliable: 2
-%--------------------------------------------------------------------------
+function [UnVal,Count]=unique_count(Vec, CmpFun)
+    % Unique values and count the number of apperances of each value.
+    % Description: Select unique values in numeric or char vector and count the
+    %              number of apperances of each value.
+    % Input  : - Numeric, string, or cell of chars vector.
+    %          - Comparison function for strings. Default is @strcmpi
+    % Output : - Unique values
+    %          - Count of appearances per unique value.
+    % Tested : Matlab R2014a
+    %     By : Eran O. Ofek                    Jan 2015
+    %    URL : http://weizmann.ac.il/home/eofek/matlab/
+    % Example: [UnVal,Count]=tools.array.unique_count(Vec)
+    
+    arguments
+        Vec
+        CmpFun   = @strcmpi;
+    end
 
-UnVal = unique(Vec);
-Nun   = numel(UnVal);
-Count = zeros(Nun,1);
-for I=1:1:Nun
-    Count(I) = sum(UnVal(I)==Vec);
+    UnVal = unique(Vec);
+    Nun   = numel(UnVal);
+    Count = zeros(Nun,1);
+
+    if isnumeric(Vec)        
+        for I=1:1:Nun
+            Count(I) = sum(UnVal(I)==Vec);
+        end
+    else
+        for I=1:1:Nun
+            Count(I) = sum(CmpFun(UnVal(I),Vec));
+        end
+    end
 end
