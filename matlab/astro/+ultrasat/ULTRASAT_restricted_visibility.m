e353e221f4bf65425762242e5e72ab1b513c97b5
yossishv <yossishv@gmail.com>
2025-01-14 10:10:26 +0200

    ULTRASAT_restricted_visibility - comments added

--- matlab/astro/+ultrasat/ULTRASAT_restricted_visibility.m
+++ matlab/astro/+ultrasat/ULTRASAT_restricted_visibility.m
@@ -1,13 +1,35 @@
-function ULTRASAT_vis=ULTRASAT_restricted_visibility(JD,Coo,Args)%varargin)
-% SHORT DESCRIPTION HERE
+function ULTRASAT_vis=ULTRASAT_restricted_visibility(JD,Coo,Args)
+% Calcualte the visibilty and compare to the limits of a list of Coo at a vecotr of JD 
 % Package: ultrasat
-% Description: 
-% Input  : - 
+% Input  : - a vector of julian dates
+%          - Atwo column matric of Coordinate [RA,Dec]
 %          * Arbitrary number of pairs of arguments: ...,keyword,value,...
 %            where keyword are one of the followings:
-% Output : - 
+%            'CooUnits' - Units of input coordinates {'g'|'rad'|'deg'}
+%                         'g' - radians or sexagesimal.
+%                         'rad' - radians only.
+%                         'deg' - degrees only.
+%                          Defaults is 'rad'
+%            'MinSunDist' - Minimum distance from the Sun, in [degrees].
+%                           Default is 70.
+%            'MinMoonDist' - Minimum distance from the Moon, in [degrees]
+%                           Default is 34.
+%            'MinEarthDist' - Minimum distance from the Earth, in [degrees]
+%                           Default is 56.
+%            'MinDistOffset' - Offset of the minimum distances from the Sun/Earth/Moon, in [degrees].
+%                              0 for center of FoV (i.e., pointing), 7 to within the 7deg FoV (though not exact - adjust sepratly each limit and do not check together)
+%                              Default is 0.
+%            'Power_MaxSunDist' - Maximum distance from the Sun for positive Power balance (i.e., beyond this it is Hard Obs), in [degrees].
+%                                 Default is 130 (90+45; 45 for the solar panels).
+%            'Comm_MinEarthDist' - Minimum distance from the Earth allowing for real-time dowloand, in [degrees].
+%                                  Default is 0 (i.e. no limit).
+%            'Comm_MaxEarthDist' - Maximum distance from the Earth allowing for real-time dowloand, in [degrees].
+%                                  Default is 180 (i.e. no limit).
+% Output : - Visibility structure with: {'SunAngDist';'EarthAngDist';'MoonAngDist';'JD';'Coo';...
+%                                        'SunRA';'SunDec';'EarthRA';'EarthDec';'MoonRA';'MoonDec';
+%                                        'SunLimits';'EarthLimits';'MoonLimits';'PowerLimits';'CommLimits'}
 % License: GNU general public license version 3
-%     By : Yossi Shvartzvald                    Jan 2021 (updated Jan 2025)
+%     By : Yossi Shvartzvald                    updated Jan 2025
 %    URL : http://weizmann.ac.il/home/eofek/matlab/
 % Example: N1  = [220./RAD, 66./RAD];
 %          S1  = [ 42./RAD,-66./RAD];
@@ -24,8 +46,8 @@ function ULTRASAT_vis=ULTRASAT_restricted_visibility(JD,Coo,Args)%varargin)
         Args.MinSunDist  = 70; % [deg]
         Args.MinMoonDist  = 34; % [deg]
         Args.MinEarthDist  = 56; % [deg]
-        Args.MinDistOffset  = 0; % [deg] 0 for center of FoV (i.e., pointing), 7 to allow for poinitng offset (though not exact)
-        Args.Power_MaxSunDist   = 130; % [deg] 90+45; 45 for the solar panels
+        Args.MinDistOffset  = 0; % [deg] 
+        Args.Power_MaxSunDist   = 130; % [deg] 
         Args.Comm_MinEarthDist   = 0; % [deg] Currently (i.e. 0) means no limit
         Args.Comm_MaxEarthDist = 180; % [deg] Currently (i.e. 180) means no limit
     end
