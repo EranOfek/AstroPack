f6630e80e6f5fb881ec2452912d2f39064f917bb
EranOfek <ofek.eran@gmail.com>
2025-01-18 20:50:13 +0200

    interp2_bilinear

--- matlab/util/+tools/+interp/+mex/interp2d_bilinear_double.m
+++ matlab/util/+tools/+interp/+mex/interp2d_bilinear_double.m
@@ -1,11 +1,11 @@
-% 2D bi-linear interpolation - fast MEX
+% 2D bi-linear interpolation - fast MEX for double inputs
 %       (x2.6 times faster than interp2).
 % Input  : - A 2D array which coordinates are the pixel indices.
-%            Single or double.
 %          - A 2D matrix of X interpolated position.
 %          - A 2D matrix of Y interpolated position.
 % Output : - Interpolated matrix
 % Author : Eran Ofek (2025 Jan) 
+% Compilation: mex interp2d_bilinear_double.cpp CXXFLAGS="\$CXXFLAGS -O3 -march=native -fopenmp" LDFLAGS="\$LDFLAGS -fopenmp"
 % Example:  V=rand(1700,1700);                                                             
 %           [MatX,MatY]=meshgrid((1:1700),(1:1700)); MatX1=MatX+0.1; MatY1=MatY+0.2;
 %           tic;for I=1:1:100, Vq=tools.interp.mex.interp2d_bilinear(V,MatX1,MatY1);end,toc
