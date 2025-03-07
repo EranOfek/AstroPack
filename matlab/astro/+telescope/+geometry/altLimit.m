8c0195af84ba3bd54abaa2836a1753b611b6981d
EranOfek <ofek.eran@gmail.com>
2025-03-07 20:19:02 +0200

    help

--- matlab/astro/+telescope/+geometry/altLimit.m
+++ matlab/astro/+telescope/+geometry/altLimit.m
@@ -24,4 +24,15 @@ function [Beta] = altLimit(l,H1,H2,R)
     Beta = Phi + Alpha;
     Beta = Beta.*RAD;
 
+    % Based on a formula I gave Ofer:
+    % d = 135 
+    % distance from unit to wall
+    % CG_height = 120 
+    % wall_height = 220
+    % R=R_mirror
+    % H=wall_height-CG_height
+    % R=50;
+    % obs_angle = real(-log(-(R - (- H^2 + R^2 - d^2)^(1/2))/(H + d*1i))*1i) * RAD
+    
+    
 end
