feb94533d5e2e5c4522c11f0193c917d6771ceb7
EranOfek <ofek.eran@gmail.com>
2025-03-20 19:23:35 +0200

    sudo_system

--- matlab/util/+tools/+os/sudo_system.m
+++ matlab/util/+tools/+os/sudo_system.m
@@ -1,7 +1,8 @@
 function [Status, Result] = sudo_system(Cmd, Pass)
-    % Execute system command with sudo
+    % Execute system command with sudo or without sudo.
     % Input  : - Command to execute.
-    %          - Password.
+    %          - Password. If [], then will execute the command without
+    %            sudo. Default is [].
     % Output : - Status of executed command.
     %          - Result of executed command.
     % Author : Eran Ofek (2025 Mar) 
@@ -9,8 +10,13 @@ function [Status, Result] = sudo_system(Cmd, Pass)
 
     arguments
         Cmd
-        Pass
+        Pass  = [];
     end
-    [Status, Result] = system(sprintf('echo "%s" | sudo -S %s',Pass,Cmd));
+    
+    if isempty(Pass)
+        [Status, Result] = system(Cmd);
+    else
+        [Status, Result] = system(sprintf('echo "%s" | sudo -S %s',Pass,Cmd));
+    end
     
 end
