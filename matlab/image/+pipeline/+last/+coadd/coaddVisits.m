e8238de0e5eb43476d47f1a45b5ef162e2ed6c72
EranOfek <ofek.eran@gmail.com>
2025-04-06 10:33:01 +0300

    issue #625

--- matlab/image/+pipeline/+last/+coadd/coaddVisits.m
+++ matlab/image/+pipeline/+last/+coadd/coaddVisits.m
@@ -133,6 +133,7 @@ function [CI, AI] = coaddVisits(In, Args)
         CI=imProc.sources.findMeasureSources(CI,'AddFlags',false);
         CI=imProc.astrometry.addCoordinates2catalog(CI,'OutUnits','deg');
         CI = imProc.psf.populatePSF(CI);
+        CI = imProc.sources.psfFitPhot(CI);
         CI=imProc.calib.photometricZP(CI); 
         
     end
