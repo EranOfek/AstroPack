f598eb5058a46f61a7e65d1f0cda27743140cc78
A.M. Krassilchtchikov <agioffe@gmail.com>
2025-05-28 14:30:27 +0300

    *.m

--- matlab/util/+db/+search/imagesContainingPoint.m
+++ matlab/util/+db/+search/imagesContainingPoint.m
@@ -3,12 +3,12 @@ function [Result] = imagesContainingPoint(RA, Dec, Args)
     %     a 2-step algorithm: (i) search by the neighboring healpix pixels
     %     (ii) exact search of point in the image among the images selected at the first step
     % Input  : - RA (deg or sexagesimal string)
-    %          - Dec (deg or sexagesimal string)
-    %          
+    %          - Dec (deg or signed sexagesimal string)
     %          * ...,key,val,... 
-    % Output : - a table containing unique indexes of the images
+    % Output : - a table containing unique indexes of the images, exptime, and jd_start 
     % Author : A.M. Krassilchtchikov (2025 May) 
-    % Example: T=db.search.imagesContainingPoint(100,10,'DB',D); % when D is a DB connected before  
+    % Example: T=db.search.imagesContainingPoint("10:23:00","+40:20:00");
+    %          T=db.search.imagesContainingPoint(100,10,'DB',D); % when D is a DB connected before  
     arguments
         RA                     = 83.63;
         Dec                    = 22.01;
@@ -21,11 +21,11 @@ function [Result] = imagesContainingPoint(RA, Dec, Args)
         Args.HP_ColName        = 'upix_low';  
         Args.PrimarySearchNside= 2^8; % this should match the actual HP_ColName         
         Args.PrimarySearchRad  = 1;   % [deg] this radius should include all the neighboring pixels of PrimarySearchNside
-        
+        Args.Server            = @VO.name.server_simbad; 
     end   
     %
-    Ncoo = numel(RA);
-    Result  = cell(Ncoo,1);
+    Ncoo   = numel(RA);
+    Result = cell(Ncoo,1);
     % get a connection
     if isempty(Args.DB)
         DB = db.Db;
@@ -37,9 +37,14 @@ function [Result] = imagesContainingPoint(RA, Dec, Args)
         DB = Args.DB;
     end
     % loop the points 
-    for Icoo = 1:Ncoo        
+    for Icoo = 1:Ncoo      
+        if isnumeric(RA) % RA, Dec are in deg.
+            RA0 = RA(Icoo); Dec0 = Dec(Icoo);
+        else             % RA, Dec are a cell array of sexadecimal strings            
+            [RA0, Dec0]=celestial.convert.cooResolve(RA{Icoo}, Dec{Icoo}, 'InUnits','sex', 'OutUnits','deg', 'Server',Args.Server);
+        end
         % first we search by the neighboring healpix pixels:
-        WhereClause = db.search.queryConeSearch_Healpix(RA(Icoo), Dec(Icoo),...
+        WhereClause = db.search.queryConeSearch_Healpix(RA0, Dec0,...
             Args.PrimarySearchRad,'SearchRadiusUnits','deg','NSide',Args.PrimarySearchNside, ...
             'HP_ColName',Args.HP_ColName);
         Query = db.Db.genQuery(Args.Table, Args.SelectFields, WhereClause);
@@ -49,10 +54,10 @@ function [Result] = imagesContainingPoint(RA, Dec, Args)
         % next we select only those images that indeed contain the point:
         for Irow = 1:N1
             Pol = [T.ra1(Irow),T.dec1(Irow);T.ra2(Irow),T.dec2(Irow);T.ra3(Irow),T.dec3(Irow);T.ra4(Irow),T.dec4(Irow)];
-            F(Irow) = celestial.search.isPointInsidePolygon(RA(Icoo), Dec(Icoo), Pol);
+            F(Irow) = celestial.search.isPointInsidePolygon(RA0, Dec0, Pol);
         end
-        Result{Icoo} = T(F,:);
+        Result{Icoo} = sortrows(T(F,:),'jd_start'); % select and sort by start time 
         N2      = height(Result{Icoo}); 
-        fprintf('second selection: %.3f\n',N2/N1); % diagnostic
+        fprintf('second selection: %.3f\n',N2/N1);  % diagnostic             
     end
 end
