b223601cfe80970cae175dd764c7ed2149a6faab
A.M. Krassilchtchikov <agioffe@gmail.com>
2025-05-31 15:46:14 +0300

    a new function to check intersection and containment of spherical polygons

--- matlab/astro/+celestial/+coo/polygon_boolean_operations.m
+++ matlab/astro/+celestial/+coo/polygon_boolean_operations.m
@@ -1,33 +1,51 @@
-function [Result] = polygon_boolean_operations(P0, P1, Args)
-    % One line description
-    %     Optional detailed description
+function Result = polygon_boolean_operations(P0, P1, Args)
+    % Check intersection and containment of spherical polygons 
+    %     Comparison of too large polygons at high resolution would lead 
+    %     to a crash in the cone search function 
     % Input  : - P0 - a polygon: Nx2 array of [RA, Dec] in degrees 
     %          - P1 - a polygon or a cell array of polygons
     %          * ...,key,val,... 
-    %          'Resolution' - desired raster resolution [arcsec]
-    % Output : - 
+    %          'Resolution' - desired accuracy = raster resolution [arcsec]
+    % Output : - a struct containing boolean data on intersection 
+    %            and mutual containment of the input polygons 
     % Author : A.M. Krassilchtchikov (2025 May) 
-    % Example: 
-
+    % Example: P0 = [10, 70; 10, 70.5; 9.5, 70.5; 9.5, 70];
+    %          P1 = [10, 70; 10, 70.5; 9.5, 70.5; 9.5, 70];
+    %          P2 = [11, 69.5; 11, 70.3; 9.6, 70.3; 9.6, 69.5];
+    %          P3 = [9.9, 70.1; 9.9, 70.3; 9.6, 70.3; 9.6, 70.1];
+    %          P4 = [8 70.55; 8 70.7; 11 70.7; 11 70.53];    
+    %          Res = celestial.coo.polygon_boolean_operations(P0, {P1,P2,P3,P4})
     arguments
         P0
         P1
-        Args.Resolution = 5; % [arcsec]                
+        Args.Resolution = 10; % [arcsec] 
+        % at <5 arcsec the scheme becomes unstable due to 
+        % loss of accuracy of celestial.healpix.coneSearch for large Nside?
     end
-
+    % raster the first polygon:
+    R0 = celestial.healpix.rasterize_polygon(P0,'Resolution',Args.Resolution);
+    %
     if iscell(P1)
         Np = numel(P1); % number of polygons in the cell array
     else
         Np = 1;
     end
-    
+    Result.Intersect   = zeros(1,Np);
+    Result.R0containR1 = zeros(1,Np);
+    Result.R1containR0 = zeros(1,Np);    
+    % raster the second polygon(s) and compare the pixel lists:
     for Ip = 1:Np
         if iscell(P1)
-            P2 = P1{Ip};
+            P = P1{Ip};
         else
-            P2 = P1;
+            P = P1;
+        end    
+        R = celestial.healpix.rasterize_polygon(P,'Resolution',Args.Resolution);
+        %
+        if ~isempty(intersect(R0, R))
+            Result.Intersect(Ip)   = 1;
+            Result.R0containR1(Ip) = all(ismember(R, R0));
+            Result.R1containR0(Ip) = all(ismember(R0, R));        
         end
-        
-        
     end
 end
