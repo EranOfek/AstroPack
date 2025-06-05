a9574186b6ab357cf6d42620fb2ba19efc94d2c1
A.M. Krassilchtchikov <agioffe@gmail.com>
2025-06-05 13:07:06 +0300

    *.m

--- matlab/astro/+ultrasat/+tools/coverProbMap.m
+++ matlab/astro/+ultrasat/+tools/coverProbMap.m
@@ -7,8 +7,6 @@ function [RA, Dec, Stat] = coverProbMap(SkyMap, Args)
     % Author : A.M. Krassilchtchikov (2025 Feb) 
     % Example: Map = '~/ULTRASAT/SkyGrid/LVC/2024/04/01/lvc_2024_04_01_00_40_58_000000.csv';
     %         [RA, Dec, ~] = ultrasat.tools.coverProbMap(Map,'MaxTarg',4); 
-    %         ultrasat.tools.coverProbMap(Map,'Verbosity',0,'Experimental',1,'ProbThresh',0.01,'MaxTarg',10); % experimental
-
     arguments
         SkyMap      
         Args.MaxTarg           = 4;    % maximal number of exposures (unique targets) to use
@@ -71,31 +69,10 @@ function [RA, Dec, Stat] = coverProbMap(SkyMap, Args)
         end
         
     %%%%%%%%%%%%%%% experimental coverage function:
-    if Args.Experimental
-            NsideAreaDeg = [2, 859.4366926962348; ... % area in deg(2)
-                    4, 214.8591731740587; ...
-                    8, 53.714793293514674; ...
-                   16, 13.428698323378669; ...
-                   32, 3.357174580844667; ...
-                   64, 0.8392936452111668; ...
-                  128, 0.2098234113027917; ...
-                  256, 0.052455852825697924; ...
-                  512, 0.013113963206424481; ...
-                 1024, 0.0032784908016061202; ...
-                 2048, 0.0008196227004015301;...
-                 4096, 0.00020490567510038252; ...
-                 8192, 5.122641877509563e-05; ...
-                16384, 1.2806604693773907e-05];     
-            Ind  = floor(log(Map.UNIQ/4)/(2*log(2)));
-            SRAD = (180/pi)^2; % sq. deg. in srad
-            ProbPerDeg = Map.PROBDENSITY ./ SRAD; % probability per deg^2          
-            WeightedProb = NsideAreaDeg(Ind(:,1),2).*ProbPerDeg;          
-            Overlap  = 0.1;% 1 -- absolute overlap, 0 -- no overlap allowed
-            Coverage = 1;  % percent of filtered probability covered
-            [RA_out, Dec_out, centers_idx, coverage_PD, disk_map, stop_k] = place_probability_disks(Map.RA, Map.DEC, WeightedProb,...
-                Args.MaxTarg, Args.FOVradius, 'Overlap',Overlap,'Plot',1,'TargetCoverage',Coverage);
-%              [RA_out, Dec_out, centers_idx, coverage_PD, disk_map, stop_k] = place_probability_disks(Map.RA, Map.DEC, Map.PROBDENSITY,...
-%                 Nmax, Args.FOVradius, 'Overlap',Overlap,'Plot',1,'TargetCoverage',Coverage);
+    if Args.Experimental           
+        [FOVcenters, IdxCenters, CoveredPD, FOVmap, Nstop] = ...
+            telescope.obs.optimalCoverageProbMap([Map.RA Map.DEC], Map.PROBDENSITY, Args.FOVradius,...
+                'Uniq',Map.UNIQ,'Nmax', Args.MaxTarg, 'Plot',1);
     end
     %%%%%%%%%%%%%%%
         
@@ -248,165 +225,6 @@ function [SumProb, SumArea] = sumProbability(Map, Args)
     SumArea = sum(NsideAreaDeg(Ind(:,1),2));       % deg^2
 end
 
-%%% an experimental function to cover the probability density: 
-
-function [RA_out, Dec_out, centers_idx, coverage_PD, disk_map, stop_k] = place_probability_disks(RA, Dec, PD, N, R, Args)
-% PLACE_PROBABILITY_DISKS Greedy placement of N spherical disks (radius R degrees)
-% to cover maximum probability density on the sphere with optional overlap 
-%
-% Inputs:
-%   RA, Dec - vectors of coordinates in degrees (length M)
-%   PD      - vector of probability density values (length M)
-%   N       - maximum number of disks to place
-%   R       - radius of each disk in degrees
-%   Overlap - overlap factor in [0, 1], where:
-%             0 = disjoint disks only (no reuse),
-%             1 = fully overlapping allowed (pure greedy).
-% Optional:
-%   'TargetCoverage' - desired total PD coverage (0 to 1), default = 1
-%   'Plot'           - if true, show Aitoff projection plot
-%
-% Outputs:
-%   RA_out, Dec_out - coordinates (in degrees) of selected disk centers
-%   centers_idx     - indices of selected centers in original list
-%   coverage_PD     - total summed PD covered by all disks
-%   disk_map        - cell array: disk_map{k} = indices of points covered by disk k
-%   stop_k          - index at which coverage target was achieved
-    arguments
-        RA
-        Dec
-        PD
-        N                    
-        R
-        Args.Overlap         = 0;
-        Args.TargetCoverage  = 1;
-        Args.Plot            = false;
-    end
-    % Convert RA/Dec to radians
-    RA_rad = deg2rad(RA(:));
-    Dec_rad = deg2rad(Dec(:));
-    PD = PD(:);
-    M = length(PD);
-
-    % Convert to 3D unit vectors
-    x = cos(Dec_rad) .* cos(RA_rad);
-    y = cos(Dec_rad) .* sin(RA_rad);
-    z = sin(Dec_rad);
-    V = [x, y, z];
-
-    % Build KD-tree for fast 3D neighbor search
-    tree = KDTreeSearcher(V);
-
-    % Convert angular radius to 3D chord distance
-    chord_radius = 2 * sind(R / 2);
-
-    % Initialize
-    coverage_count = zeros(M,1);
-    PD_effective = PD;
-    centers_idx = [];
-    disk_map = {};
-    coverage_PD = 0;
-    total_PD = sum(PD);
-    stop_k = N;
-
-    for k = 1:N
-        best_idx = -1;
-        best_score = -inf;
-        best_neighbors = [];
-
-        for i = 1:M
-            if PD_effective(i) == 0
-                continue
-            end
-            neighbors = rangesearch(tree, V(i,:), chord_radius);
-            neighbors = neighbors{1};
-            score = sum(PD_effective(neighbors));
-            if score > best_score
-                best_score = score;
-                best_idx = i;
-                best_neighbors = neighbors;
-            end
-        end
-
-        if best_idx == -1
-            break
-        end
-
-        centers_idx(end+1) = best_idx;
-        disk_map{end+1} = best_neighbors;
-        coverage_PD = coverage_PD + sum(PD(best_neighbors));
-        coverage_count(best_neighbors) = coverage_count(best_neighbors) + 1;
-    %     PD_effective(best_neighbors) = PD(best_neighbors) .* (1 - Args.Overlap).^coverage_count(best_neighbors);
-        PD_effective(best_neighbors) = PD(best_neighbors) .* Args.Overlap.^coverage_count(best_neighbors);
-
-        if coverage_PD >= Args.TargetCoverage * total_PD
-            stop_k = k;
-            break
-        end
-    end
-
-    RA_out  = RA(centers_idx);
-    Dec_out = Dec(centers_idx);
-
-    % Optional Plotting (Aitoff Projection)
-    if Args.Plot
-        figure; hold on; grid on
-        % Convert RA to [-180,180] for Aitoff
-        RA_plot = mod(RA + 180, 360) - 180;
-        [x_proj, y_proj] = aitoff_projection(RA_plot, Dec);
-        scatter(x_proj, y_proj, 5, PD, 'filled');
-        colorbar; title('Probability Density and Disk Centers')
-        % Plot circles
-        for i = 1:length(centers_idx)
-            [xc, yc] = aitoff_projection(RA_plot(centers_idx(i)), Dec(centers_idx(i)));
-            plot(xc, yc, 'rx', 'MarkerSize', 8, 'LineWidth', 2);
-            draw_aitoff_circle(RA_plot(centers_idx(i)), Dec(centers_idx(i)), R);
-        end
-        xlabel('RA (Aitoff)'); ylabel('Dec'); axis equal
-    end
-end
-
-function [x, y] = aitoff_projection(ra, dec)
-% Convert degrees to radians
-    ra = deg2rad(ra);
-    dec = deg2rad(dec);
-
-    alpha = ra / 2;
-    d = dec;
-    cos_d = cos(d);
-    denom = sqrt(1 + cos(cos_d .* cos(alpha)));
-    x = 2 * cos_d .* sin(alpha) ./ denom;
-    y = sin(d) ./ denom;
-end
-
-function draw_aitoff_circle(RA_center, Dec_center, R)
-% Draws a small circle of radius R (degrees) around (RA_center, Dec_center)
-% in Aitoff projection.
-    steps = 200;
-    angles = linspace(0, 2*pi, steps);
-    RA_center  = deg2rad(RA_center);
-    Dec_center = deg2rad(Dec_center);
-    R = deg2rad(R);
-
-    % Compute circle on the sphere
-    circle_RA  = zeros(steps, 1);
-    circle_Dec = zeros(steps, 1);
-
-    for j = 1:steps
-        angle = angles(j);
-        % Great circle offset using spherical trigonometry
-        circle_Dec(j) = asin(sin(Dec_center) * cos(R) + cos(Dec_center) * sin(R) * cos(angle));
-        dRA = atan2(sin(angle) * sin(R) * cos(Dec_center), cos(R) - sin(Dec_center) * sin(circle_Dec(j)));
-        circle_RA(j) = RA_center + dRA;
-    end
-
-    % Convert back to degrees and project
-    circle_RA = rad2deg(circle_RA);
-    circle_Dec = rad2deg(circle_Dec);
-    circle_RA = mod(circle_RA + 180, 360) - 180;
-    [xc, yc] = aitoff_projection(circle_RA, circle_Dec);
-    plot(xc, yc, 'r-', 'LineWidth', 1);
-end
 
 
 
