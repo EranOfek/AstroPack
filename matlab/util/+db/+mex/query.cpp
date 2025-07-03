4bebf915c51d9f238123b8163d51e4fb3430b446
A.M. Krassilchtchikov <agioffe@gmail.com>
2025-07-03 13:00:19 +0300

    DB query through a CPP library

--- matlab/util/+db/+mex/query.cpp
+++ matlab/util/+db/+mex/query.cpp
@@ -1,16 +1,14 @@
 #include "mex.h"
 #include <clickhouse/client.h>
-#include <future>
 #include <string>
-#include <atomic>
+#include <future>
+#include "block2struct.cpp"  // include the helper here
 
 using namespace clickhouse;
 
-
-void mexFunction(int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[])
-{
+void mexFunction(int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[]) {
     if (nrhs < 1 || !mxIsChar(prhs[0])) {
-        mexErrMsgTxt("First argument must be a SQL query string.");
+        mexErrMsgTxt("First input must be SQL query string.");
     }
 
     char* query = mxArrayToString(prhs[0]);
@@ -23,54 +21,37 @@ void mexFunction(int nlhs, mxArray* plhs[], int nrhs, const mxArray* prhs[])
             .SetPassword("physics")
             .SetDefaultDatabase("last"));
 
-        std::promise<Block> prom;
-        std::future<Block> fut = prom.get_future();
+        // Block result;
+        // std::promise<void> done;
+        // std::mutex mtx;
+        // 
+        // client.Select(query, [&](const Block& b) {
+        //     std::lock_guard<std::mutex> lock(mtx);
+        //     if (b.GetRowCount() > 0)
+        //         result = b;
+        // });
+        // 
+        // // wait to ensure all callbacks complete
+        // std::this_thread::sleep_for(std::chrono::milliseconds(50));
 
-        std::atomic<bool> promise_set(false);
+        // plhs[0] = convert_block_to_struct_array(result);
 
-        client.Select(query,
-            [&prom, &promise_set](const Block& block) {
-                if (!promise_set.exchange(true)) {
-                    prom.set_value(block);  // set block only once
-                }
-            });
+        std::vector<clickhouse::Block> allBlocks;
 
-        // wait synchronously for query result
-        Block block = fut.get();
+        client.Select(query, [&](const clickhouse::Block& block) {
+            if (block.GetRowCount() > 0) {
+                allBlocks.push_back(block);
+            }
+        });      
 
-        if (block.GetRowCount() == 0 || block.GetColumnCount() == 0) {
-            plhs[0] = mxCreateDoubleScalar(NAN);
-        } else {
-            auto col = block[0];
+        plhs[0] = convert_blocks_to_struct_array(allBlocks);
 
-            if (auto col_uint64 = col->As<ColumnUInt64>()) {
-                uint64_t val = col_uint64->At(0);
-                plhs[0] = mxCreateDoubleScalar(static_cast<double>(val));
-            }
-            else if (auto col_uint32 = col->As<ColumnUInt32>()) {
-                uint32_t val = col_uint32->At(0);
-                plhs[0] = mxCreateDoubleScalar(static_cast<double>(val));
-            }
-            else if (auto col_uint8 = col->As<ColumnUInt8>()) {
-                uint8_t val = col_uint8->At(0);
-                plhs[0] = mxCreateDoubleScalar(static_cast<double>(val));
-            }
-            else if (auto col_float64 = col->As<ColumnFloat64>()) {
-                double val = col_float64->At(0);
-                plhs[0] = mxCreateDoubleScalar(val);
-            }
-            else if (auto col_string = col->As<ColumnString>()) {
-                std::string_view sv = col_string->At(0);
-                std::string str(sv);  // explicit conversion
-                plhs[0] = mxCreateString(str.c_str());
-            }
-            else {
-                mexErrMsgTxt("Unsupported column type or empty result.");
-            }
-        }
     } catch (const std::exception& e) {
         mexErrMsgTxt(e.what());
     }
 
     mxFree(query);
 }
+
+
+  
