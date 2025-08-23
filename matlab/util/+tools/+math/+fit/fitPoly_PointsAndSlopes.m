3daf5fc592ba697fceb83e0886b6e82130754528
Eran Ofek <eofek@cf-gf-lt.physics.weizmann.ac.il>
2025-08-23 13:41:27 +0300

    tools.math.fit.fitPoly_PointsAndSlopes

--- matlab/util/+tools/+math/+fit/fitPoly_PointsAndSlopes.m
+++ matlab/util/+tools/+math/+fit/fitPoly_PointsAndSlopes.m
@@ -9,12 +9,20 @@ function [Result] = fitPoly_PointsAndSlopes(T, X, Xdot, Args)
     %                   Default is [0 1 2].
     %            'SubT' - Subtract mid Time from T.
     %                   Default is true.
+    %            'ErrX', 'ErrXdot' - Errors for X and Xdot. If empty, use 
+    %                   backslash without errors. Default is [].
     % Output : - A structure with the following fields:
     %            .Par - Array of fitted parameters. Column per object.
     %            .T - Vector of time (mid subtracted).
     %            .MidT - Reference time.
     %            .X
     %            .Xdot
+    %            .Resid
+    %            .RMS
+    %            .Chi2
+    %            .Nobs
+    %            .Npar
+    %            .Dof
     % Author : Eran Ofek (2025 Jun) 
     % Example: A=1; B=0.01; C=1e-4; T=(1:2).'; X=A+B.*T+C.*T.^2; Xdot=B+2.*C.*T; R=tools.math.fit.fitPoly_PointsAndSlopes(T, X, Xdot);
 
@@ -24,6 +32,8 @@ function [Result] = fitPoly_PointsAndSlopes(T, X, Xdot, Args)
         Xdot
         Args.Orders      = [0 1 2];
         Args.SubT        = true;
+        Args.ErrX        = [];
+        Args.ErrXdot     = [];
     end
 
 
@@ -38,15 +48,51 @@ function [Result] = fitPoly_PointsAndSlopes(T, X, Xdot, Args)
         MidT = 0;
     end
 
-    H1 = T.^Args.Orders;
-    H2 = [zeros(N,1), Args.Orders(2:end).*T.^(Args.Orders(2:end)-1)];
-    H  = [H1; H2];
-
-    Par = H\[X; Xdot];
-    Result.Par  = Par;
     Result.T    = T;
     Result.X    = X;
     Result.Xdot = Xdot;
     Result.RefT = MidT;
+
+    H1 = T.^Args.Orders;
+    H2 = [zeros(N,1), Args.Orders(2:end).*T.^(Args.Orders(2:end)-1)];
+    H  = [H1; H2];
+
+    Ncol = size(X,2);
+    [Nobs, Npar] = size(H);
+
+    Y = [X; Xdot];
+    if ~isempty(Args.ErrX) && ~isempty(Args.ErrXdot)
+        UseErr = true;
+
+        Npar = size(H,2);
+        Result.Par    = zeros(Npar, Ncol);
+        Result.ParErr = zeros(Npar, Ncol);
+        for Icol=1:1:Ncol
+            InvV = 1./([Args.ErrX(:,Icol); Args.ErrXdot(:,Icol)].^2);
+            [Par, ParErr] = lscov(H, Y(:,Icol), InvV);
+            Result.Par(:,Icol)    = Par(:);
+            Result.ParErr(:,Icol) = ParErr(:);
+        end
+       
+    else
+        UseErr = false;
+        
+        Result.Par = H\Y;
+        Result.ParErr = NaN;
+        
+    end
+
+    Ymodel = H*Result.Par;
+    Result.Resid  = Y - Ymodel;
+    Result.RMS  = std(Result.Resid,[],1);
+
+    if UseErr
+        Result.Chi2 = sum((Result.Resid./[Args.ErrX; Args.ErrXdot]).^2, 1, 'omitnan');
+    else
+        Result.Chi2 = [];
+    end
+    Result.Nobs = Nobs;
+    Result.Npar = Npar;
+    Result.Dof  = Nobs - Npar;
 
 end
