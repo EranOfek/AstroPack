28627df0e541d4753670907aadb518082fd93e5d
Eran Ofek <eofek@cf-gf-lt.physics.weizmann.ac.il>
2025-09-06 17:14:10 +0300

    tools.math.stat.median

---
+++ matlab/util/+tools/+math/+stat/+mex/median.m
@@ -0,0 +1,18 @@
+% Fast median function for arrays over a single dim
+% Input  : - An array (single or double).
+%          - Dimension (scalar) over which to calculate the median.
+%          - 'omitnan'|'includenan' option. Default is 'includenan'.
+% Output : - The median.
+% Compilation : mex -R2018a CXXFLAGS="\$CXXFLAGS -O3 -Ofast -march=native -fopenmp -DNDEBUG" LDFLAGS="\$LDFLAGS -fopenmp" median.cpp
+% Author : ChatGPT, Eran Ofek (2024 Aug) 
+% Example: A=rand(1726,1726,20);                                   
+%          tools.math.stat.mex.median(A);
+%          
+%          tic;for i=1:3, tools.math.stat.mex.median(A,1,'omitnan');end,toc
+%          % Elapsed time is 1.051544 seconds.
+%          tic;for i=1:3, median(A,1,'omitnan');end,toc                    
+%          % Elapsed time is 2.486143 seconds.
+%          tic;for i=1:3, tools.math.stat.mex.median(A,3,'omitnan');end,toc
+%          % Elapsed time is 1.615232 seconds.
+%          tic;for i=1:3, median(A,3,'omitnan');end,toc                    
+%          Elapsed time is 4.354529 seconds.
--- matlab/util/+tools/+math/+stat/+mex/median1.m
+++ matlab/util/+tools/+math/+stat/+mex/median1.m
@@ -5,3 +5,10 @@
 % Compilation : mex CXXFLAGS="\$CXXFLAGS -fopenmp" LDFLAGS="\$LDFLAGS -fopenmp" median1.cpp
 % Author : Eran Ofek (2024 Aug) 
 % Example: tools.math.stat.mex.median1(V)
+%          A=rand(1726,1726,20);                                   
+%          tic;for i=1:5, tools.math.stat.mex.median1(A(:));end,toc  
+%          % Elapsed time is 3.429818 seconds.
+%          tic;for i=1:5, fast_median(A(:));end,toc                
+%          % Elapsed time is 4.368703 seconds.
+%          tic;for i=1:5, median(A(:));end,toc                     
+%          % Elapsed time is 5.455333 seconds.
