f2f672a77249e6e91945f0305d75ef5790bbe75b
Eran Ofek <eofek@cf-gf-lt.physics.weizmann.ac.il>
2025-09-20 23:13:09 +0300

    powerspec mex

--- matlab/astro/+timeSeries/+period/+mex/powerspecMatrix_mex.m
+++ matlab/astro/+timeSeries/+period/+mex/powerspecMatrix_mex.m
@@ -1,10 +1,10 @@
-% Fast calculation of the power spectrum using MEX
+% Fast calculation of the power spectrum for matrix, ignoring NaN, using MEX
 %     The result is not normalized
 % Input  : - A vector of times.
-%          - A vector of measurments.
+%          - A matrix of measurments. NaN will be ignored.
 %          - A vector of frequencies for which to calculate the power
 %            sperctrum.
-% Output : - A vector of the power spectrum per frequency.
+% Output : - A matrix of the power spectrum per frequency, and per source.
 % Author : Eran Ofek (2025 Sep) 
 % Compilation: mex -O CXXFLAGS='$CXXFLAGS -std=c++14 -O3 -ffast-math -fno-math-errno -fno-trapping-math -funroll-loops -march=native -mtune=native -fopenmp -fno-exceptions -fno-rtti' LDFLAGS='$LDFLAGS -fopenmp -lm' ps_mex_md.cpp
-% Example: PS=timeSeries.period.mex.powerspec_mex(T,M,FreqVec);
+% Example: PS=timeSeries.period.mex.powerspecMatrix_mex(T,M,FreqVec);
