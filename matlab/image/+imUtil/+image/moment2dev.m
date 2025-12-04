dc42b5735bc7f684bc82554a3cc2eab814fce556
A.M. Krassilchtchikov <agioffe@gmail.com>
2025-12-04 15:32:38 +0200

    *.m

--- matlab/image/+imUtil/+image/moment2dev.m
+++ matlab/image/+imUtil/+image/moment2dev.m
@@ -1,10 +1,18 @@
-function [M1, M2] = moment2dev(I, X0, Y0, Args)
+function [M1, M2] = moment2dev(Image, X0, Y0, Args)
     % moments of an image
     %     Optional detailed description
     % Input  : - a 2D image matrix 
     %          - X0 initial guess of the centroid
     %          - Y0 initial guess of the centroid
     %          * ...,key,val,... 
+    %          'MomRadius'- Radius around position in which to calculate the
+    %                        moments. Recomended ~1.7 FWHM. Default is 8.
+    %          'SigmaG' - Sigma of the 'filetring' Gaussian, can be
+    %                      up to MomRadius/2
+    %          'MaxIter' - maximal number of iterations for M1
+    %          'Precision' - precision of M1.X and M1.Y position
+    %          'SubtractBack' - whether to measure and subtract the background 
+    %                          (Default is true)
     % Output : - first moment structure:
     %          .X
     %          .Y
@@ -14,33 +22,43 @@ function [M1, M2] = moment2dev(I, X0, Y0, Args)
     %          .Y2
     %          .XY 
     % Author : A.M. Krassilchtchikov (2025 Dec) 
-    % Example: 
-
+    % Example: Im1 = imUtil.kernel2.gauss(2, [31 31]);
+    %          [M1,M2]=imUtil.image.moment2dev(Im1,16,16)
+    %          Im2 = 10+Im1*100;
+    %          [M1,M2]=imUtil.image.moment2dev(Im2,16,16)
+    %          Im3 = Im2 + rand(size(Im2));    
+    %          [M1,M2]=imUtil.image.moment2dev(Im3,16,16)
     arguments
-        I
+        Image
         X0
         Y0
-        Args.R                 = 8;
-        Args.SigmaG            = 4;
+        Args.MomRadius         = 8;
+        Args.SigmaG            = 2;
         Args.MaxIter           = 30;
-        Args.Precision         = 1e-6;        
+        Args.Precision         = 1e-4;   
+        Args.SubtractBack      = 'true';
     end
-    
-    [M, N] = size(I);
+    %
+    [M, N] = size(Image);
     [Xg, Yg] = meshgrid(1:N, 1:M);
+    
+    % Measure and subtract a global background if requested
+    if Args.SubtractBack
+        Back  = imUtil.background.background(Image,'SubSizeXY','full');
+        Image = Image - Back;
+    end
 
-    % Define a circular mask and restrict all the operations to the fixed aperture
+    % Define a circular mask and cut the image outside it 
     Dx0 = Xg - X0;
     Dy0 = Yg - Y0;
-    Mask = (Dx0.^2 + Dy0.^2) <= Args.R^2;
-    Iwin = I .* Mask;
+    Mask = (Dx0.^2 + Dy0.^2) <= Args.MomRadius^2;
+    Iwin = Image .* Mask;
 
     % Find the first moment iteratively
     Xc = X0;
     Yc = Y0;
 
     for Iter = 1:Args.MaxIter
-
         Dx = Xg - Xc;
         Dy = Yg - Yc;
 
@@ -64,7 +82,6 @@ function [M1, M2] = moment2dev(I, X0, Y0, Args)
             Yc = NewY;
             break;
         end
-
         Xc = NewX;
         Yc = NewY;
     end
@@ -74,12 +91,13 @@ function [M1, M2] = moment2dev(I, X0, Y0, Args)
     M1.Niter = Iter;
 
     % calculate the second moments relative to the found centroid: 
-    
     Dx = Xg - M1.X;
     Dy = Yg - M1.Y;
 
-    % use the same mask (inside R) and no Gaussian weights for the 2nd moments:
-    J = Iwin;
+    % use the same mask (inside MomRadius) with or w/o weights for the 2nd moments:    
+    W = exp(-(Dx.^2 + Dy.^2) / (2 * Args.SigmaG^2));
+%     J = Iwin; 
+    J = Iwin.*W;
 
     M0 = sum(J(:));
 
