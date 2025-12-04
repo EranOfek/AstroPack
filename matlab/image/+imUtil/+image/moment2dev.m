332d8c8fe7162247ae7b3710fc7af2d49b21f8a5
A.M. Krassilchtchikov <agioffe@gmail.com>
2025-12-04 09:53:38 +0200

    *.m

--- matlab/image/+imUtil/+image/moment2dev.m
+++ matlab/image/+imUtil/+image/moment2dev.m
@@ -1,4 +1,5 @@
-function [X1, Y1, sigma_xx, sigma_yy, sigma_xy] = moment2dev(I, X0, Y0, R, sigma_g, Args)
+% function [X1, Y1, sigma_xx, sigma_yy, sigma_xy] = moment2dev(I, X0, Y0, R, sigma_g, Args)
+function [M1, M2] = moment2dev(I, X0, Y0, Args)
     % moments of an image
     %     Optional detailed description
     % Input  : - a 2D image matrix 
@@ -12,78 +13,70 @@ function [X1, Y1, sigma_xx, sigma_yy, sigma_xy] = moment2dev(I, X0, Y0, R, sigma
         I
         X0
         Y0
-        R
-        sigma_g
+        Args.R                 = 8;
+        Args.SigmaG            = 4;
         Args.MaxIter           = 10;
-        Args.Precision         = [];        
+        Args.Precision         = 1e-6;        
     end
 
-% I        : 2D image (double)
-% X0, Y0   : initial guess for centroid
-% R        : radius for moment computation
-% sigma_g  : Gaussian scale used in iterative refinement (e.g. R/2)
-% maxIter  : max iterations for centroid refinement (e.g. 20)
-% tol      : convergence threshold, e.g. 1e-4
+    [m, n] = size(I);
+    [xg, yg] = meshgrid(1:n, 1:m);
 
-[m, n] = size(I);
-[xg, yg] = meshgrid(1:n, 1:m);
+    % ----- Define circular mask only once -----
+    dx0 = xg - X0;
+    dy0 = yg - Y0;
+    mask = (dx0.^2 + dy0.^2) <= Args.R^2;
 
-% ----- Define circular mask only once -----
-dx0 = xg - X0;
-dy0 = yg - Y0;
-mask = (dx0.^2 + dy0.^2) <= R^2;
+    % Restrict all operations to fixed aperture
+    Iwin = I .* mask;
 
-% Restrict all operations to fixed aperture
-Iwin = I .* mask;
+    % ----- ITERATIVE CENTROID REFINEMENT -----
+    Xc = X0;
+    Yc = Y0;
 
-% ----- ITERATIVE CENTROID REFINEMENT -----
-Xc = X0;
-Yc = Y0;
+    for k = 1:Args.MaxIter
 
-for k = 1:Args.MaxIter
+        dx = xg - Xc;
+        dy = yg - Yc;
 
-    dx = xg - Xc;
-    dy = yg - Yc;
+        % Gaussian weights centered on current estimate
+        W = exp(-(dx.^2 + dy.^2) / (2 * Args.SigmaG^2));
 
-    % Gaussian weights centered on current estimate
-    W = exp(-(dx.^2 + dy.^2) / (2 * sigma_g^2));
+        % Use both image and Gaussian as weights
+        IW = Iwin .* W;
 
-    % Use both image and Gaussian as weights
-    IW = Iwin .* W;
+        M0 = sum(IW(:));
+        if M0 == 0
+            error('Zero total weight (image may be empty inside R).');
+        end
 
-    M0 = sum(IW(:));
-    if M0 == 0
-        error('Zero total weight (image may be empty inside R).');
-    end
+        newX = sum(xg(:) .* IW(:)) / M0;
+        newY = sum(yg(:) .* IW(:)) / M0;
 
-    newX = sum(xg(:) .* IW(:)) / M0;
-    newY = sum(yg(:) .* IW(:)) / M0;
+        % Check convergence
+        if hypot(newX - Xc, newY - Yc) < Args.Precision
+            Xc = newX;
+            Yc = newY;
+            break;
+        end
 
-    % Check convergence
-    if hypot(newX - Xc, newY - Yc) < Args.Precision
         Xc = newX;
         Yc = newY;
-        break;
     end
 
-    Xc = newX;
-    Yc = newY;
-end
+    M1.X = Xc;
+    M1.Y = Yc;
 
-X1 = Xc;
-Y1 = Yc;
+    % ----- SECOND MOMENTS -----
+    dx = xg - M1.X;
+    dy = yg - M1.Y;
 
-% ----- SECOND MOMENTS -----
-dx = xg - X1;
-dy = yg - Y1;
+    % Use the same mask (inside R) and no Gaussian weights for the 2nd moments:
+    J = Iwin;
 
-% Use the same mask (inside R) and no Gaussian weights for 2nd moments:
-J = Iwin;
+    M0 = sum(J(:));
 
-M0 = sum(J(:));
-
-sigma_xx = sum((dx(:).^2) .* J(:)) / M0;
-sigma_yy = sum((dy(:).^2) .* J(:)) / M0;
-sigma_xy = sum((dx(:).*dy(:)) .* J(:)) / M0;
-
+    M2.X2 = sum((dx(:).^2) .* J(:)) / M0;
+    M2.Y2 = sum((dy(:).^2) .* J(:)) / M0;
+    M2.XY = sum((dx(:).*dy(:)) .* J(:)) / M0;
 end
