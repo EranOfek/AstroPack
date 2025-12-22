c69e06a60a43492fdc85b65edad183462113aa1e
A.M. Krassilchtchikov <agioffe@gmail.com>
2025-12-22 18:17:09 +0200

    a simple polygon plot

---
+++ matlab/util/+plot/plot_polygon.m
@@ -0,0 +1,19 @@
+function plot_polygon(P, Args)
+    % plot a closed polygon
+    %     Optional detailed description
+    % Input  : - 2D matrix in the form X, Y
+    %          * ...,key,val,... 
+    %          'Color' - color
+    %          'Symbol'- plotting symbol
+    % Output : - a graph
+    % Author : A.M. Krasilchtchikov (2025 Dec) 
+    % Example: 
+    arguments
+        P
+        Args.Color             = 'black';
+        Args.Symbol            = '-o';
+    end
+    X = P(:,1); Y = P(:,2);
+    X(end+1) = X(1); Y(end+1) = Y(1);
+    plot(X, Y, Args.Symbol, 'Color', Args.Color);    
+end
