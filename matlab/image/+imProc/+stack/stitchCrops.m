3f4d98141dc117972f1425fded458eb4bbab35b6
A.M. Krassilchtchikov <agioffe@gmail.com>
2026-01-11 19:33:35 +0200

    matlab/image/+imProc/+stack/stitchCrops.m

--- matlab/image/+imProc/+stack/stitchCrops.m
+++ matlab/image/+imProc/+stack/stitchCrops.m
@@ -20,13 +20,12 @@ function [Result] = stitchCrops(AI, Args)
     MaxY  = 1;
     MCat   = repmat(AstroCatalog,1,Ncrop);
     
-    for Icrop = 1:Ncrop
-        MCat(Icrop) = AI(Icrop).CatData;
-        
+    for Icrop = 1:Ncrop        
+        MCat(Icrop) = AI(Icrop).CatData;        
         ReadUniq  = AI(Icrop).getStructKey(Args.UNIQSEC).(Args.UNIQSEC);
         ReadOrig  = AI(Icrop).getStructKey(Args.ORIGUSEC).(Args.ORIGUSEC); 
         Uniq(Icrop,:) = sscanf(ReadUniq(2:end-1), '%d').';
-        Orig(Icrop,:) = sscanf(ReadOrig(2:end-1), '%d').';
+        Orig(Icrop,:) = sscanf(ReadOrig(2:end-1), '%d').';                       
         MaxX  = max(MaxX,  Orig(Icrop,2));
         MaxY  = max(MaxY,  Orig(Icrop,4));
     end
@@ -36,10 +35,18 @@ function [Result] = stitchCrops(AI, Args)
     for Icrop = 1:Ncrop
         Result.Image(Orig(Icrop,1):Orig(Icrop,2),Orig(Icrop,3):Orig(Icrop,4)) = ...
             AI(Icrop).Image(1:Uniq(Icrop,2)+Args.Border,1:Uniq(Icrop,4)+Args.Border);
+        ShiftX = Orig(Icrop,1);
+        ShiftY = Orig(Icrop,3);
+        IndX = MCat(Icrop).colname2ind({'XPEAK','X1','X'});
+        IndY = MCat(Icrop).colname2ind({'YPEAK','Y1','Y'});        
+        MCat(Icrop).Catalog(:,IndX) = MCat(Icrop).Catalog(:,IndX) + ShiftX;
+        MCat(Icrop).Catalog(:,IndY) = MCat(Icrop).Catalog(:,IndY) + ShiftY;
     end
     
-    % merge catalogs:
+    % merge the catalogs:
     Result.CatData = merge(MCat);
-    % exclude eduplicates: 
+            
+    % build WCS from the merged catalog 
+    [~, Result.CatData, ~] = imProc.astrometry.astrometryRefine(Result.CatData);
     
 end
