ab874eea4a938c62ace973b870f4ff4c3bd6659f
A.M. Krassilchtchikov <agioffe@gmail.com>
2026-01-18 18:30:13 +0200

    *.m

--- matlab/image/+imUtil/+art/createSourceCube.m
+++ matlab/image/+imUtil/+art/createSourceCube.m
@@ -84,12 +84,10 @@ function [CubePSF, XY] = createSourceCube(PSF0, X1Y1, Flux, Args)
             end
         else
             [M,~,~] = size(PSF); 
-            for Isrc = 1:Nsrc
-                EdgeFunPars = ceil( Args.FunEdgePars .* M / 15);             % empiric, should somehow depend on M
-                SupressedEdges = Args.FunEdge( EdgeFunPars, [M M] ) .* PSF(:,:,Isrc);
-                SupressedEdges = SupressedEdges .* ( SupressedEdges > 0 );
-                PSF(:,:,Isrc) = SupressedEdges ./ sum(SupressedEdges,'all'); % renormalize
-            end
+            EdgeFunPars = ceil( Args.FunEdgePars .* M / 15); % empiric, should somehow depend on M           
+            SupressedEdges = Args.FunEdge( EdgeFunPars, [M M] ) .* PSF;
+            SupressedEdges = SupressedEdges .* ( SupressedEdges > 0 );
+            PSF = SupressedEdges ./ sum(SupressedEdges,'all'); % renormalize
         end
     end
     
