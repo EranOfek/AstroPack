5366b351a05bf3825f2bdcb103192aabb1a46ddc
A.M. Krassilchtchikov <agioffe@gmail.com>
2026-01-22 14:56:32 +0200

    *.m

--- matlab/image/+imProc/+stack/stitchCrops.m
+++ matlab/image/+imProc/+stack/stitchCrops.m
@@ -30,7 +30,11 @@ function [Result] = stitchCrops(AI, Args)
         [Xmin(Icrop), Xmax(Icrop), Ymin(Icrop), Ymax(Icrop)] = deal(Orig(1),Orig(2),Orig(3),Orig(4));
     end
     
-    X0 = min(Xmin); Y0 = min(Ymin); % the corner of the stitch on the whole image 
+    X0 = min(Xmin); % the lower left corner of the stitch on the whole image 
+    Y0 = min(Ymin); 
+    CatShiftX = Xmin-X0; % shift of pixel coordinates in the catalogs        
+    CatShiftY = Ymin-Y0; 
+        
     Nx = max(Xmax)-X0+1;
     Ny = max(Ymax)-Y0+1;
     Result = AstroImage({nan(Nx,Ny)}); % ,'Back',{nan(Nx,Ny)},'Var',{nan(Nx,Ny)});
@@ -50,23 +54,28 @@ function [Result] = stitchCrops(AI, Args)
     hasTop    = any(fromTop,    2);
     
     for Icrop = 1:Ncrop
+                
         if hasLeft(Icrop)
-            XUmin = round((Uniq(Icrop,1)+CCDSEC(Icrop,1))/2);
+            XUmin = ceil((Uniq(Icrop,1)+CCDSEC(Icrop,1))/2);
+            ImaShiftX = CCDSEC(Icrop,2)-XUmin;
         else
             XUmin = CCDSEC(Icrop,1);
+            ImaShiftX = XUmin-1;
         end
         if hasRight(Icrop)
-            XUmax = round((Uniq(Icrop,2)+CCDSEC(Icrop,2))/2); 
+            XUmax = CCDSEC(Icrop,2)-Uniq(Icrop,1)/2;
         else
             XUmax = CCDSEC(Icrop,2);
         end
         if hasBottom(Icrop)
-            YUmin = round((Uniq(Icrop,3)+CCDSEC(Icrop,3))/2); 
+            YUmin = ceil((Uniq(Icrop,3)+CCDSEC(Icrop,3))/2); 
+            ImaShiftY = CCDSEC(Icrop,4)-YUmin;
         else
             YUmin = CCDSEC(Icrop,3);
+            ImaShiftY = YUmin-1;
         end
         if hasTop(Icrop)
-            YUmax = round((Uniq(Icrop,4)+CCDSEC(Icrop,4))/2); 
+            YUmax = CCDSEC(Icrop,4)-Uniq(Icrop,3)/2;
         else
             YUmax = CCDSEC(Icrop,4);
         end
@@ -74,22 +83,19 @@ function [Result] = stitchCrops(AI, Args)
         AIc = crop(AI(Icrop),[XUmin XUmax YUmin YUmax],'UpdateCat',true,'CreateNewObj',true);     
         MCat(Icrop) = AIc.CatData;
         
-        ShiftX = Xmin(Icrop)-X0;
-        ShiftY = Ymin(Icrop)-Y0;
-        
         IndX = MCat(Icrop).colname2ind({'XPEAK','X1','X'});
         IndY = MCat(Icrop).colname2ind({'YPEAK','Y1','Y'});        
-        MCat(Icrop).Catalog(:,IndX) = MCat(Icrop).Catalog(:,IndX) + ShiftX;
-        MCat(Icrop).Catalog(:,IndY) = MCat(Icrop).Catalog(:,IndY) + ShiftY;
+        MCat(Icrop).Catalog(:,IndX) = MCat(Icrop).Catalog(:,IndX) + CatShiftX(Icrop);
+        MCat(Icrop).Catalog(:,IndY) = MCat(Icrop).Catalog(:,IndY) + CatShiftY(Icrop);
         
-        Result.Image(ShiftX+1:ShiftX+XUmax-XUmin+1, ShiftY+1:ShiftY+YUmax-YUmin+1) = AIc.Image';
+        Result.Image(ImaShiftX+1:ImaShiftX+XUmax-XUmin+1, ImaShiftY+1:ImaShiftY+YUmax-YUmin+1) = AIc.Image;
     end
                     
     % merge the catalogs:
     Result.CatData = merge(MCat);
     Result.CatData.JD = MCat(1).julday;  
-    RA0  = mean(Result.Table.RA);
-    Dec0 = mean(Result.Table.Dec);
+    RA0  = mean(Result.CatData.getCol('RA'));
+    Dec0 = mean(Result.CatData.getCol('Dec'));
             
     % build WCS from the merged catalog 
     [FitRes, Result.CatData, ~] = imProc.astrometry.astrometryRefine(Result.CatData,'RA',RA0,'Dec',Dec0);
