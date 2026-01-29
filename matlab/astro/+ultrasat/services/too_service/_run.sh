b736071c97b0a7008f58d0e628b7825a2be37cc8
Chen Tishler <chentishler@gmail.com>
2026-01-29 10:45:51 +0200

    too planner service

--- matlab/astro/+ultrasat/services/too_service/_run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Start Watchdog process to monitor the MATLAB process
-python watchdog_monitor.py snr_matlab_watchdog.txt &
-
-# Run the process in loop, if killed by watchdog or max-run-time, it will be restarted
-while true; do
-    ./soc_snr_matlab
-done
