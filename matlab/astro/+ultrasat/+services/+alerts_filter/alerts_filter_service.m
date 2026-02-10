01198c68b79b33f447d37e5e28516e7121b0d3f6
Chen Tishler <chentishler@gmail.com>
2026-02-10 14:31:15 +0200

    services

--- matlab/astro/+ultrasat/+services/+alerts_filter/alerts_filter_service.m
+++ matlab/astro/+ultrasat/+services/+alerts_filter/alerts_filter_service.m
@@ -1,15 +1,15 @@
 %==========================================================================
 % Project     : ULTRASAT SOC
-% Filename    : ultrasat/+services/+slew_calc/slew_calc_service.m
-% Author      : Chen Tishler
+% Filename    : ultrasat/+services/+alerts_filter/alerts_filter_service.m
+% Author      : Chen Tishler    
 % Created     : 02/11/2025
 % Modified    : 10/02/2026
-% Description : MATLAB service to calculate slew time between targets
+% Description : MATLAB service to process AlertsFilter requests using JsonFileIpc
 %==========================================================================
 
-function slew_calc_service()
-    % Slew calculator service - main function
-	fprintf('slew_calc_service started\n');    
+function alerts_filter_service()
+    % AlertsFilter service - main function
+	fprintf('alert_filter_service started\n');    
 
     % Get the SOC_PATH environment variable
     SOC_PATH = getenv('SOC_PATH');
@@ -19,20 +19,20 @@ function slew_calc_service()
     end
 
     % Set log file name
-    ultrasat.services.common.setLogFile('slew_calc_service', 'matlab_services/slew_calc/');
+    ultrasat.services.common.setLogFile('alerts_filter_service', 'matlab_services/alerts_filter/');
 
     % Set the input path
-    InputPath = fullfile(SOC_PATH, 'runtime', 'exchange', 'slew_calc', 'input');
-    ProcessedPath = fullfile(SOC_PATH, 'runtime', 'exchange', 'slew_calc', 'processed');    
+    InputPath = fullfile(SOC_PATH, 'runtime', 'exchange', 'alerts_filter', 'input');
+    ProcessedPath = fullfile(SOC_PATH, 'runtime', 'exchange', 'alerts_filter', 'processed');    
 
     % Create the JsonFileIpc object
     io.msgLog(LogLevel.Info, 'creating JsonFileIpc');    
     jsonIpc = ultrasat.services.common.JsonFileIpc('InputPath', InputPath, 'ProcessedPath', ProcessedPath, ...
-        'Callback', @ultrasat.services.slew_calc.processRequest);
+        'Callback', @ultrasat.services.alerts_filter.processRequest);
 
     % Blocking loop: process files in the input folder, call processRequest.m for each file
     io.msgLog(LogLevel.Info, 'calling processLoop...');
     jsonIpc.processLoop();
     
-    io.msgLog(LogLevel.Info, 'slew_calc_service terminated');
+    io.msgLog(LogLevel.Info, 'alerts_filter_service terminated');
 end
--- matlab/astro/+ultrasat/+services/+too_planner/processRequest.m
+++ matlab/astro/+ultrasat/+services/+too_planner/processRequest.m
@@ -2,20 +2,23 @@ function Output = processRequest(Input)
     % Process TooPlanner request for ULTRASAT
 
     Output = struct;
+    Output.status    = 'error';    
     Output.message   = 'processTooJson: started';
-    Output.status    = 'error';
     Output.summaryFileName = '';
 
     try
-        if strcmp(Input.action, 'too_planner')
+        if strcmp(Input.action, 'health')
+            Output.status  = 'ok';            
+            Output.message = 'health: OK';        
+        elseif strcmp(Input.action, 'too_planner')
             Output = processTooPlanner(Input);
         else
+            Output.status = 'error';            
             Output.message = 'processRequest: unknown action';
-            Output.status = 'error';
         end
     catch ex
+        Output.status = 'error';        
         Output.message = sprintf("processRequest: error: identifier='%s', message='%s'", ex.identifier, ex.message);
-        Output.status = 'error';
         io.msgLog(LogLevel.Error, Output.message);
     end
 end
@@ -29,10 +32,13 @@ function Output = processTooPlanner(Input)
         % Create helper (if class-based environment, else call directly)
         runner = ultrasat.planner.TooPlannerRunner();
         summaryFileName = runner.runFromJson(Input.filename);
+
+        % Prepare output struct
         Output = struct;
-        Output.summaryFileName = summaryFileName;
         Output.status  = 'ok';
-        Output.message = 'TooPlanner: OK';
+        Output.message = 'TooPlannerRunner: OK';        
+        Output.summaryFileName = summaryFileName;
+
     catch ex
         Output.status  = 'error';
         Output.message = sprintf("processTooPlanner: error: identifier='%s', message='%s'", ex.identifier, ex.message);
