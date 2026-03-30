d72144aac0553a3d8232121c0d9ee9a9852673c3
A.M. Krassilchtchikov <agioffe@gmail.com>
2026-03-30 18:21:10 +0300

    added read header and CCDSEC

--- matlab/image/+fits/+mex/read_image.cpp
+++ matlab/image/+fits/+mex/read_image.cpp
@@ -61,67 +61,6 @@ int find_image_hdu(fitsfile* fptr)
     return -1;
 }
 
-
-// mxArray* readHeaderCell(fitsfile* fptr)
-// {
-//     int status = 0, nkeys = 0;
-// 
-//     fits_get_hdrspace(fptr, &nkeys, NULL, &status);
-//     checkStatus(status);
-// 
-//     mxArray* cell = mxCreateCellMatrix(nkeys, 3);
-// 
-//     char card[FLEN_CARD];
-// 
-//     for (int i = 1; i <= nkeys; i++) {
-//         fits_read_record(fptr, i, card, &status);
-//         checkStatus(status);
-// 
-//         char key[FLEN_KEYWORD], value[FLEN_VALUE], comment[FLEN_COMMENT];
-//         fits_parse_record(card, key, value, comment, &status);
-// 
-//         // Column 0: key
-//         mxSetCell(cell, (i-1) + 0*nkeys, mxCreateString(key));
-// 
-//         // Column 1: value
-//         mxSetCell(cell, (i-1) + 1*nkeys, mxCreateString(value));
-// 
-//         // Column 2: comment
-//         mxSetCell(cell, (i-1) + 2*nkeys, mxCreateString(comment));
-//     }
-// 
-//     return cell;
-// }
-
-// mxArray* readHeaderCell(fitsfile* fptr)
-// {
-//     int status = 0, nkeys = 0;
-// 
-//     fits_get_hdrspace(fptr, &nkeys, NULL, &status);
-//     checkStatus(status);
-// 
-//     mxArray* cell = mxCreateCellMatrix(nkeys, 3);
-// 
-//     char key[FLEN_KEYWORD];
-//     char value[FLEN_VALUE];
-//     char comment[FLEN_COMMENT];
-// 
-//     for (int i = 1; i <= nkeys; i++) {
-// 
-//         fits_read_keyn(fptr, i, key, value, comment, &status);
-//         checkStatus(status);
-// 
-//         // MATLAB is column-major: index = row + col*nrows
-//         int row = i - 1;
-// 
-//         mxSetCell(cell, row + 0*nkeys, mxCreateString(key));
-//         mxSetCell(cell, row + 1*nkeys, mxCreateString(value));
-//         mxSetCell(cell, row + 2*nkeys, mxCreateString(comment));
-//     }
-// 
-//     return cell;
-// }
-
 mxArray* readHeaderCell(fitsfile* fptr)
 {
     int status = 0, nkeys = 0;
@@ -192,8 +131,8 @@ mxArray* readHeaderCell(fitsfile* fptr)
 void mexFunction(int nlhs, mxArray* plhs[],
                  int nrhs, const mxArray* prhs[])
 {
-    if (nrhs != 2)
-        mexErrMsgTxt("Usage: img = fits_read_image(filename, hdu)");
+    if (nrhs < 2)
+        mexErrMsgTxt("Usage: img = fits_read_image(filename, hdu, [CCDSEC])");
 
     char* filename = mxArrayToString(prhs[0]);
     int hdu_req = (int)mxGetScalar(prhs[1]);
@@ -214,7 +153,7 @@ void mexFunction(int nlhs, mxArray* plhs[],
 
     fits_movabs_hdu(fptr, hdu, &hdutype, &status);
     checkStatus(status);
-
+    
     // Get image parameters (robust for .fz)
     int bitpix, naxis;
     long naxes[10];
@@ -225,16 +164,63 @@ void mexFunction(int nlhs, mxArray* plhs[],
 
     if (naxis == 0)
         mexErrMsgTxt("The requested HDU has no image");
-
-    std::vector<mwSize> dims(naxis);
-    long nelements = 1;
-    for (int i = 0; i < naxis; i++) {
-        dims[i] = (mwSize)naxes[i];
-        nelements *= naxes[i];
-    }
         
     int anynul = 0;
-
+    
+    // treat CCDSEC
+    
+    bool use_subset = false;
+    
+    if (nrhs >= 3) {
+        if (!mxIsEmpty(prhs[2])) {
+            if (mxGetNumberOfElements(prhs[2]) != 4)
+                mexErrMsgTxt("CCDSEC must be [] or [X1 X2 Y1 Y2]");
+            use_subset = true;
+        }
+    }
+    
+    std::vector<long> fpixel(naxis, 1);
+    std::vector<long> lpixel(naxis);
+    std::vector<long> inc(naxis, 1);
+    
+    for (int i = 0; i < naxis; i++)
+        lpixel[i] = naxes[i];
+    
+    if (use_subset) {
+        if (naxis < 2)
+            mexErrMsgTxt("CCDSEC [X1 X2 Y1 Y2] requires at least 2D image");
+        
+        double* s = mxGetPr(prhs[2]);
+        
+        long X1 = (long)s[0];
+        long X2 = (long)s[1];
+        long Y1 = (long)s[2];
+        long Y2 = (long)s[3];
+        
+        // Bounds check
+        if (X1 < 1 || X2 > naxes[0] || X1 > X2)
+            mexErrMsgTxt("Invalid X range");
+        
+        if (Y1 < 1 || Y2 > naxes[1] || Y1 > Y2)
+            mexErrMsgTxt("Invalid Y range");
+        
+        fpixel[0] = X1;
+        lpixel[0] = X2;
+        
+        fpixel[1] = Y1;
+        lpixel[1] = Y2;
+        
+        // higher dimensions remain full range
+    }
+    
+    std::vector<mwSize> dims(naxis);
+    long nelements = 1;
+    
+    for (int i = 0; i < naxis; i++) {
+        dims[i] = (mwSize)(lpixel[i] - fpixel[i] + 1);
+        nelements *= dims[i];
+    }
+    
     // -------- Attempt native read --------
     mxArray* out = nullptr;
     void* data = nullptr;
@@ -251,15 +237,21 @@ void mexFunction(int nlhs, mxArray* plhs[],
             // unsigned 16-bit path
             mxArray* out = mxCreateNumericArray(naxis, dims.data(), mxUINT16_CLASS, mxREAL);
             unsigned short* data = (unsigned short*)mxGetData(out);
-            
-            fits_read_img(fptr, TUSHORT, 1, nelements,
-                    NULL, data, NULL, &status);
+                        
+            fits_read_subset(fptr, TUSHORT,
+                    fpixel.data(),
+                    lpixel.data(),
+                    inc.data(),
+                    NULL,
+                    data,
+                    &anynul,
+                    &status);
+            checkStatus(status);
             
             // Output 2: header (optional)
             if (nlhs >= 2) {
                 plhs[1] = readHeaderCell(fptr);
-            }
-            
+            }            
             // Output 3: HDU number used (optional)
             if (nlhs >= 3) {
                 plhs[2] = mxCreateDoubleScalar((double)hdu);
@@ -282,8 +274,15 @@ void mexFunction(int nlhs, mxArray* plhs[],
 
         int status_try = 0;
 
-        fits_read_img(fptr, datatype, 1, nelements,
-                      NULL, data, &anynul, &status_try);
+        fits_read_subset(fptr, datatype,
+                fpixel.data(),
+                lpixel.data(),
+                inc.data(),
+                NULL,
+                data,
+                &anynul,
+                &status);
+        checkStatus(status);
 
         if (status_try != 0) {
             native_ok = false;
@@ -302,16 +301,22 @@ void mexFunction(int nlhs, mxArray* plhs[],
         out = mxCreateNumericArray(naxis, dims.data(), mxDOUBLE_CLASS, mxREAL);
         double* dptr = mxGetPr(out);
 
-        fits_read_img(fptr, TDOUBLE, 1, nelements,
-                      NULL, dptr, &anynul, &status);
+        fits_read_subset(fptr, TDOUBLE,
+                fpixel.data(),
+                lpixel.data(),
+                inc.data(),
+                NULL,
+                data,
+                &anynul,
+                &status);
+
         checkStatus(status);
     }
     
     // Output 2: header (optional)
     if (nlhs >= 2) {
         plhs[1] = readHeaderCell(fptr);
-    }
-    
+    }    
     // Output 3: HDU number used (optional)
     if (nlhs >= 3) {
         plhs[2] = mxCreateDoubleScalar((double)hdu);
