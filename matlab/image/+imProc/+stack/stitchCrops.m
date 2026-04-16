da79d29e4b4e3d154310bb3a8c0ee1abad932893
A.M. Krassilchtchikov <agioffe@gmail.com>
2026-04-16 10:18:27 +0300

    added stitching masks

--- matlab/image/+imProc/+stack/stitchCrops.m
+++ matlab/image/+imProc/+stack/stitchCrops.m
@@ -50,7 +50,8 @@ function [Result] = stitchCrops(AI, Args)
     % make an empty AstroImage    
     Nx = max(Xmax)-X0+1;
     Ny = max(Ymax)-Y0+1;
-    Result = AstroImage({single(nan(Ny,Nx))}); 
+    Result = AstroImage({nan(Ny,Nx)}); 
+%     Result = AstroImage({single(nan(Ny,Nx))});
     
     % fill the new image with chopped crops, shift the catalog pixels    
     for Icrop = 1:Ncrop                
@@ -94,8 +95,12 @@ function [Result] = stitchCrops(AI, Args)
         end
         
         Result.Image(ImaShiftY+1:ImaShiftY+YUmax-YUmin+1, ImaShiftX+1:ImaShiftX+XUmax-XUmin+1) = AIc.Image;
+        Result.Mask(ImaShiftY+1:ImaShiftY+YUmax-YUmin+1, ImaShiftX+1:ImaShiftX+XUmax-XUmin+1)  = AIc.Mask;
     end
-                    
+    
+    % set the right mask type:
+    Result.MaskData.Data = uint32(Result.MaskData.Data);
+    
     % merge the catalogs:
     Result.CatData = merge(MCat);
     Result.CatData.JD = MCat(1).julday;  
