041bc4b66730346c5ffaad60611063e3b663cec6
Enrico Segre <enrico.segre@weizmann.ac.il>
2026-05-10 18:04:54 +0300

    detectStreaksLSD returns AstroStreak object, #987

---
+++ matlab/image/+imUtil/+streaks/@AstroStreak/AstroStreak.m
@@ -0,0 +1,14 @@
+classdef AstroStreak < handle
+    properties
+        X % 2xN % abscissae of the extremal points of the streak(s), pixel coordinates
+        Y % 2xN % ordinatae of the extremal points of the streak, pixel coordinates
+        RA % 2xN
+        Dec % 2xN
+        JD % 2xN
+        IsEdge % 2xN
+        Flux % 1xN % photometric
+        FitPar % 3xN % a,b,c coefficients of the fitted sagittal deviation
+        Curve = struct('X',[],'Y',[],'Flux',[]); % coordinates and fluxes of streak slices
+        ID % Telescope, Epoch, Crop ID.
+    end
+end
