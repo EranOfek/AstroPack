d0bb3876371aefd74e646ea8ba3a15d9d193d284
EranOfek <ofek.eran@gmail.com>
2026-05-24 09:16:20 +0300

    isuue: #1025

---
+++ matlab/astro/+VO/+prep/addZ2cat.m
@@ -0,0 +1,18 @@
+function [Result] = addZ2cat(X, Y, Args)
+    % One line description
+    %     Optional detailed description
+    % Input  : - 
+    %          - 
+    %          * ...,key,val,... 
+    % Output : - 
+    % Author : Eran Ofek (2026 May) 
+    % Example: 
+
+    arguments
+        X
+        Y
+        Args.A                 = [];
+        Args.B                 = [];
+    end
+
+end
