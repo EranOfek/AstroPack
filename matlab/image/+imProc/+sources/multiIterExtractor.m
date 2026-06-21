eebb331daada2e6502ad56ec6ae40f893199386a
EranOfek <ofek.eran@gmail.com>
2026-06-21 09:30:51 +0300

    multiIter

--- matlab/image/+imProc/+sources/multiIterExtractor.m
+++ matlab/image/+imProc/+sources/multiIterExtractor.m
@@ -794,7 +794,7 @@ function [Result, SourceLess, SubtractedImage] = multiIterExtractor(Obj, Args)
                 ReCalcBackIterI = any(Args.ReCalcBackIter==Iiter); % re-calc backgroun in Iiter iteration
                 if ReCalcBackIterI
                     FlagBack         = ReCalcBackIterI | Result.isemptyProperty('Back') | Result.isemptyProperty('Var');
-                    Result(FlagBack) = imProc.background.backVar(Result, Args.backVarArgs{:});
+                    Result(FlagBack) = imProc.background.backVar(Result(FlagBack), Args.backVarArgs{:}, 'ReCalc',true);
                 end
                 
                 % add local variance from the sources revealed at all the previous iterations
