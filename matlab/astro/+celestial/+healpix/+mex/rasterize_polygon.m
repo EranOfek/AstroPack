c7feaa6140010df387e91a366af35f176dcd6a84
A.M. Krassilchtchikov <agioffe@gmail.com>
2026-06-30 21:48:53 +0300

    iss #1118: the second argument is now Nside by default, can be arcsec if the third one is 'arcsec'

--- matlab/astro/+celestial/+healpix/+mex/rasterize_polygon.m
+++ matlab/astro/+celestial/+healpix/+mex/rasterize_polygon.m
@@ -5,13 +5,21 @@
 %                    -lstdc++ CXXFLAGS="\$CXXFLAGS -std=c++11"
 % Important: the Healpix library must be compiled with the -fPIX option
 % Input  : - A sky polygon as a 2 x N matrix of RA, Dec
-%          - Angular resolution in which to return the helpix list inside the
-%            polygon.
-%          - Healpix scheme (optional, def. NESTED)
+%          - A resolution value: either the Healpix Nside (default) or an
+%            angular resolution in arcsec, depending on the mode argument below
+%          - Mode (optional, def. 'Nside'): 'Nside' interprets the second
+%            argument as the Healpix Nside (must be a positive power of 2);
+%            'arcsec' interprets it as an angular resolution in arcsec, from
+%            which the matching Nside is computed
+%          - Healpix scheme (optional, def. NESTED): 'NEST' or 'RING'
 % Output : - vector of indexes of the raster pixels of resolution Nside
-%          - the value of Nside calculated from the input resolution in arcsec
-% Author : A.M. Krassilchtchikov (2026 Mar) 
-% Example: P0 = [10, 70; 10, 70.5; 9.5, 70.5; 9.5, 70]; 
-%          [Ind, Nside] = celestial.healpix.mex.rasterize_polygon(P0, 3);
+%          - the value of Nside used (echoed when given, or computed from the
+%            input resolution in arcsec)
+% Author : A.M. Krassilchtchikov (2026 Mar)
+% Example: P0 = [10, 70; 10, 70.5; 9.5, 70.5; 9.5, 70];
+%          % second argument as Nside (default):
+%          [Ind, Nside] = celestial.healpix.mex.rasterize_polygon(P0, 2^16);
+%          % second argument as resolution in arcsec:
+%          [Ind, Nside] = celestial.healpix.mex.rasterize_polygon(P0, 3, 'arcsec');
 %          log(double(Nside))/log(2)
 %          numel(Ind)
