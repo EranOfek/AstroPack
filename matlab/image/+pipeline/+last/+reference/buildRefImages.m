9f510ccc8c47792e0efb4f44a9b520c30c2edeca
A.M. Krassilchtchikov <agioffe@gmail.com>
2026-07-05 10:42:09 +0300

    iss. #1114: changed the argument names

--- matlab/image/+pipeline/+last/+reference/buildRefImages.m
+++ matlab/image/+pipeline/+last/+reference/buildRefImages.m
@@ -117,7 +117,7 @@ function [Result,Info] = buildRefImages(RefID, Args)
         Args.DbPort             = 9000;
         Args.DbUser             = 'last_user'
         Args.PassFile           = '~/matlab/AstroPack/config/local/Passwords.yml'; % '~/.astropack/Passwords.yml';                
-        Args.PassToken          = 'last_ro'
+        Args.PassToken          = 'LASTDB_user'
         
         Args.AstrometricCatRad     = 1;           % [deg] cone radius for pre-fetching reference catalogs
         Args.AstrometricCatMagRange = [12 19.5];  % magnitude range for the astrometric catalog
