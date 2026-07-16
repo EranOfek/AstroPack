81f445cc95fb9d872b7325f7c15497e5f250f0a3
A.M. Krassilchtchikov <agioffe@gmail.com>
2026-07-16 11:27:59 +0300

    iss. #1173

--- matlab/startup/startBatchLASTPipeline.m
+++ matlab/startup/startBatchLASTPipeline.m
@@ -7,26 +7,35 @@ function startBatchLASTPipeline(Args)
     
     arguments
         Args.Id     % [numeric] Currently either 1 or 2, but maybe more
+        Args.PipelineVersion = 'v0'  % 'v0' - current (production) pipeline.DemonLAST; 'v1' - development pipeline.last.pipes.PipelineDemon
     end
-    
+
     if ~isfield(Args, 'Id') || ~isnumeric(Args.Id)
         error("Must supply 'Id' argument ([numeric] The DataDir Id)");
     end
-    
+
     % Get a daemon object
-    D = pipeline.DemonLAST;
-    
+    switch lower(Args.PipelineVersion)
+        case 'v0'
+            D = pipeline.DemonLAST;
+        case 'v1'
+            D = pipeline.last.pipes.PipelineDemon;
+        otherwise
+            error('Unknown PipelineVersion option');
+    end
+
     % Setup the logging environment
     D.Logger.Console = false;
     D.Logger.Syslog.ProgName = sprintf("last-pipeline%d", Args.Id);
-    
+
     % Tell the daemon which data directory to monitor
     D.DataDir = Args.Id;
-    
+
     % Notify systemd (if SYSTEMD env. var. exists) that the service is
     % running and what is its main process id
     tools.systemd.mex.notify_ready;
-    
-    % Run the actual daemon
-    D.main('StopButton', false);
+
+    % Run the actual daemon - pause (rather than crash) and warn loudly if
+    % the data disk gets critically full, resuming once space frees up
+    D.main('StopButton', false, 'StopDiskFull', 99, 'PauseDiskFull', 300);
 end
