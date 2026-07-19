1c636319a318af97472cbe822ea4576523d940e3
A.M. Krassilchtchikov <agioffe@gmail.com>
2026-07-19 15:09:15 +0300

    iss. #1173: added more accurate treatment to df for the case of disks with similar names

--- matlab/image/+pipeline/+last/+pipes/@PipelineDemon/PipelineDemon.m
+++ matlab/image/+pipeline/+last/+pipes/@PipelineDemon/PipelineDemon.m
@@ -3122,7 +3122,7 @@ classdef PipelineDemon < Component
 
                 % check disk storage state - pause processing while critically full
                 if ~isempty(Args.StopDiskFull)
-                    [~,DiskP] = tools.os.df(sprintf('data%d',Obj.DataDir));
+                    [~,DiskP] = tools.os.df(Obj.BasePath);
                     while DiskP>Args.StopDiskFull
                         WarnMsg = sprintf('pipeline.last.pipes.PipelineDemon: disk data%d is %.1f%% full (threshold %.1f%%) on %s - pausing processing', ...
                             Obj.DataDir, DiskP, Args.StopDiskFull, Args.HostName);
@@ -3142,7 +3142,7 @@ classdef PipelineDemon < Component
                             break;
                         end
                         pause(Args.PauseDiskFull);
-                        [~,DiskP] = tools.os.df(sprintf('data%d',Obj.DataDir));
+                        [~,DiskP] = tools.os.df(Obj.BasePath);
                     end
                     if ~Cont
                         break;
@@ -3403,7 +3403,7 @@ classdef PipelineDemon < Component
             
                             % check disk storage state - pause processing while critically full
                             if ~isempty(Args.StopDiskFull)
-                                [~,DiskP] = tools.os.df(sprintf('data%d',Obj.DataDir));
+                                [~,DiskP] = tools.os.df(Obj.BasePath);
                                 while DiskP>Args.StopDiskFull
                                     WarnMsg = sprintf('pipeline.last.pipes.PipelineDemon: disk data%d is %.1f%% full (threshold %.1f%%) on %s - pausing processing', ...
                                         Obj.DataDir, DiskP, Args.StopDiskFull, Args.HostName);
@@ -3423,7 +3423,7 @@ classdef PipelineDemon < Component
                                         break;
                                     end
                                     pause(Args.PauseDiskFull);
-                                    [~,DiskP] = tools.os.df(sprintf('data%d',Obj.DataDir));
+                                    [~,DiskP] = tools.os.df(Obj.BasePath);
                                 end
                             end
         
--- matlab/util/+tools/+os/df.m
+++ matlab/util/+tools/+os/df.m
@@ -1,18 +1,23 @@
 function [Str, DiskP] = df(Template)
     % Get disk storage status (for UNIX-like systems)
-    % Input  : - An optional disk name (e.g., 'data1')
-    %            If given, then only the disk name data will be retrieved,
-    %            and the second output argument will contain the disk
-    %            percentage.
+    % Input  : - Either a disk name (e.g., 'data1'), matched as a substring
+    %            against df output (ambiguous if several mounts, local or
+    %            cross-mounted from another host, share that name - the
+    %            first match is used and a warning is issued), or a path
+    %            (e.g., '/last02e/data1/archive/LAST.01.02.01'), resolved
+    %            unambiguously to the filesystem that contains it.
+    %            If given, the second output argument will contain the
+    %            disk percentage.
     % Output : - String of df output.
-    %          - Percentage-full of requested disk name.
-    %            NaN if disk was not found
-    % Author : Eran Ofek (2023 Dec) 
+    %          - Percentage-full of the requested disk/path.
+    %            NaN if not found.
+    % Author : Eran Ofek (2023 Dec)
     % Example: [Str, DiskP] = tools.os.df('data1')
+    %          [Str, DiskP] = tools.os.df('/last02e/data1/archive/LAST.01.02.01')
 
     arguments
         Template
-        
+
     end
 
     Str   = '';
@@ -22,11 +27,19 @@ function [Str, DiskP] = df(Template)
         if isempty(Template)
             [R,Str] = system('df');
         else
-            [R,Str] = system(sprintf('df | grep %s',Template));
+            if contains(Template, filesep)
+                % path: resolve unambiguously to its containing filesystem.
+                % -P (POSIX format) prevents long remote device names (e.g.
+                % 'host:/path') from wrapping onto a second line
+                [R,Str] = system(sprintf('df -P "%s" | tail -1',Template));
+            else
+                % disk name: substring match against df output
+                [R,Str] = system(sprintf('df | grep %s',Template));
+            end
             if isempty(Str)
                 DiskP = NaN;
             else
-                Cell = regexp(Str, '\s', 'split');
+                Cell = regexp(strtrim(Str), '\s+', 'split');
                 Ind = find(contains(Cell, '%'));
                 if isempty(Ind)
                     DiskP = NaN;
